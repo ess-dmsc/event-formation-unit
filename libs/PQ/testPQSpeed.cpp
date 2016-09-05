a49d05e020a7e98e82effae18ca81baad2f38c9d
Morten Jagd Christensen <Morten.Christensen@esss.se>
2016-09-05 07:36:34 +0200

    refactored tests

--- libs/PQ/testPQSpeed.cpp
+++ libs/PQ/testPQSpeed.cpp
@@ -5,17 +5,22 @@
 #include <iostream>
 #include <queue>
 
-TEST(PriorityQueue, DoublingTestRandom) {
+class PriorityQueueTest : public ::testing::Test {
+protected:
+  virtual void SetUp() { std::srand(4242); }
+
+  // virtual void TearDown() {}
+
+  std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+  std::vector<int> pqsizes{250000,  500000,  750000,  1000000, 1500000,
+                           2000000, 3000000, 4000000, 5000000};
+  Timer tm;
   int random_variable;
-  int steps = 6;
-  int count = 250000;
+};
 
-  std::srand(4242);
-
-  for (int i = 0; i < steps; i++) {
+TEST_F(PriorityQueueTest, DoublingTestRandom) {
+  for (auto const &count : pqsizes) {
     std::cout << "inserting " << count << " random ints -";
-    std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
-    Timer tm;
 
     tm.Start();
     for (int i = 0; i < count; i++) {
@@ -28,19 +33,12 @@ TEST(PriorityQueue, DoublingTestRandom) {
     }
     tm.Stop();
     std::cout << " time (us): " << tm.ElapsedUS() << "\n";
-    count += count;
   }
 }
 
-TEST(PriorityQueue, RandomSpeed) {
-  int steps = 6;
-  int count = 250000;
-
-  std::srand(4242);
-
-  for (int i = 0; i < steps; i++) {
+TEST_F(PriorityQueueTest, RandomSpeed) {
+  for (auto const &count : pqsizes) {
     std::cout << "generating " << count << " random ints -";
-    Timer tm;
 
     tm.Start();
     for (int i = 0; i < count; i++) {
@@ -48,6 +46,41 @@ TEST(PriorityQueue, RandomSpeed) {
     }
     tm.Stop();
     std::cout << " time (us): " << tm.ElapsedUS() << "\n";
-    count += count;
+  }
+}
+
+TEST_F(PriorityQueueTest, AlreadySortedIncreasing) {
+  for (auto const &count : pqsizes) {
+    std::cout << "generating " << count << " sorted ints (increasing) -";
+
+    tm.Start();
+    for (int i = 0; i < count; i++) {
+      pq.push(i);
+    }
+
+    for (int i = 0; i < count; i++) {
+      pq.pop();
+    }
+    tm.Stop();
+
+    std::cout << " time (us): " << tm.ElapsedUS() << "\n";
+  }
+}
+
+TEST_F(PriorityQueueTest, AlreadySortedDecreasing) {
+  for (auto const &count : pqsizes) {
+    std::cout << "generating " << count << " sorted ints (decreasing)-";
+
+    tm.Start();
+    for (int i = 0; i < count; i++) {
+      pq.push(count - i);
+    }
+
+    for (int i = 0; i < count; i++) {
+      pq.pop();
+    }
+    tm.Stop();
+
+    std::cout << " time (us): " << tm.ElapsedUS() << "\n";
   }
 }
