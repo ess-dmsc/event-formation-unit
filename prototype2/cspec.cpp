7ac68c843493d112e027e2440d7ee59b0dbdc295
Morten Jagd Christensen <Morten.Christensen@esss.se>
2016-10-12 14:46:08 +0200

    added initial data parse for real multi blade data

--- prototype2/cspec.cpp
+++ prototype2/cspec.cpp
@@ -1,7 +1,13 @@
 /** Copyright (C) 2016 European Spallation Source */
 
-#include "Detector.h"
+#include <CSPECData.h>
+#include <Detector.h>
+#include <EFUArgs.h>
+#include <Socket.h>
+#include <Timer.h>
 #include <iostream>
+#include <mutex>
+#include <queue>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -13,25 +19,71 @@ const char *classname = "CSPEC Detector";
 
 class CSPEC : public Detector {
 public:
-  class Data {
-  public:
-    int w0amp, w0pos, g0amp, g0pos;
-    int w1amp, w1pos, g1amp, g1pos;
-  };
-
   CSPEC(){};
 
   ~CSPEC(){};
 
   void input_thread(void *a);
+
+private:
+  std::mutex i2pqm, mcout;
+  std::queue<CSPECData::Data> i2pq;
 };
 
 
-void CSPEC::input_thread(void *a __attribute__((unused))) {
-  static int ctr = 0;
-  while (1) {
-    printf("%s - ctr: %d\n", classname, ctr++);
-    sleep(1);
+void CSPEC::input_thread(void *args) {
+  EFUArgs *opts = (EFUArgs *)args;
+  Socket::Endpoint local("0.0.0.0", opts->port);
+
+  UDPServer cspecdata(local);
+  cspecdata.buflen(opts->buflen);
+  cspecdata.setbuffers(0, opts->rcvbuf);
+  cspecdata.printbuffers();
+
+  char buffer[9000];
+  uint64_t rx = 0;
+  uint64_t rx_total = 0;
+  uint64_t rxp = 0;
+  int rdsize;
+  uint64_t ierrors = 0;
+
+  Timer upd, stop;
+  for (;;) {
+
+    /** this is the processing step */
+    if ((rdsize = cspecdata.receive(buffer, opts->buflen)) > 0) {
+      rxp++;
+
+      CSPECData dat(buffer, rdsize);
+      ierrors += dat.ierror;
+    }
+    rx += rdsize;
+
+    /** This is the periodic reporting*/
+    if (rxp % 100 == 0) {
+      auto usecs = upd.timeus();
+      if (usecs >= opts->updint * 1000000) {
+        rx_total += rx;
+
+        mcout.lock();
+        printf(
+            "input     : %8.2f Mb/s, q1: %3d, rxpkt: %9d, rxbytes: %12" PRIu64
+            ", errors: %" PRIu64 "\n",
+            rx * 8.0 / usecs, 0, (unsigned int)rxp,
+            rx_total, ierrors);
+        fflush(stdout);
+        mcout.unlock();
+
+        rx = 0;
+
+        if (stop.timeus() >= opts->stopafter * 1000000) {
+          std::cout << "stopping input thread " << std::endl;
+          return;
+        }
+
+        upd.now();
+      }
+    }
   }
 }
 
