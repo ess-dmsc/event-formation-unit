846713c6d4a02b3a8df7a67e2d4072871c86bd82
Morten Jagd Christensen <Morten.Christensen@esss.se>
2016-10-25 15:57:19 +0200

    clang format screwed up compiler

--- prototype2/udp/udp.cpp
+++ prototype2/udp/udp.cpp
@@ -49,8 +49,7 @@ void UDPRaw::input_thread(void *args) {
 
     if (usecs >= 1000000) {
       rx_total += rx;
-      printf("Rx rate: %.2f Mbps, rx %" PRIu64 " MB (total: %" PRIu64
-             " MB) %ld usecs\n",
+      printf("Rx rate: %.2f Mbps, rx %" PRIu64 " MB (total: %" PRIu64 " MB) %ld usecs\n",
              rx * 8.0 / usecs, rx / B1M, rx_total / B1M, usecs);
       rx = 0;
       upd.now();
