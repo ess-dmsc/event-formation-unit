d46709580e498ce5f8087e6c483c494cedaafadb
Morten Jagd Christensen <Morten.Christensen@esss.se>
2016-11-05 08:15:42 +0100

    added errorexit in jenkins script, removed verbose from gcovr

--- jenkins/proto2.bash
+++ jenkins/proto2.bash
@@ -29,14 +29,14 @@ function artifacts()
 function libs()
 {
   pushd libs
-  make V=y
+  make V=y || errexit "libs build failed"
   popd
 }
 
 function libs_test()
 {
   pushd libs
-  make V=y $1 GTEST=../artifacts/code/googletest/build/usr/local test
+  make V=y $1 GTEST=../artifacts/code/googletest/build/usr/local test || errexit "unable to build libs tests"
   make V=y runtest
   popd
 }
@@ -44,14 +44,14 @@ function libs_test()
 function prototype()
 {
   pushd prototype2
-  make V=y NOKAFKA=y
+  make V=y NOKAFKA=y || errexit "prototype2 build failed"
   popd
 }
 
 function prototype_test()
 {
   pushd prototype2
-  make V=y $1 GTEST=../artifacts/code/googletest/build/usr/local test
+  make V=y $1 GTEST=../artifacts/code/googletest/build/usr/local test || errexit "unable to build prototype2 tests"
   make V=y runtest
   make doxygen
   popd
@@ -60,8 +60,8 @@ function prototype_test()
 function coverage()
 {
   mkdir -p gcovr
-  gcovr -v -r . -x -e '.*Test.cpp' -e '.*gtest.*.h' -o gcovr/coverage.xml
-  gcovr -v -r . --html --html-details -e '.*Test.cpp' -e '.*gtest.*.h' -o coverage.html
+  gcovr -r . -x -e '.*Test.cpp' -e '.*gtest.*.h' -o gcovr/coverage.xml
+  gcovr -r . --html --html-details -e '.*Test.cpp' -e '.*gtest.*.h' -o coverage.html
 }
 
 
