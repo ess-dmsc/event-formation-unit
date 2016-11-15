6fb20d057241b6f865352c842202283f2e61621d
Morten Jagd Christensen <Morten.Christensen@esss.se>
2016-11-15 14:01:16 +0100

    added command completion shell frontend to API commands

--- utils/SocketDriver.py
+++ utils/SocketDriver.py
@@ -2,35 +2,35 @@ import string
 import socket
 import sys
 import time
+import threading 
 
 class SimpleSocket:
 
-    def __init__(self, hostname, port = 8888, timeout = 2):
-        self.hostname = hostname
-        try:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        except socket.error, msg:
-            sys.stderr.write("socket() [Socket connection error] Cannot connect to %s, error: %s\n" % (hostname, msg[0]))
-            sys.exit(1)
+  def __init__(self, hostname = "localhost", port = 8888, timeout = 2):
+    self.access_semaphor = threading.Semaphore(1)
+    try:
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error, msg:
+        sys.stderr.write("socket() [Socket connection error] Cannot connect to %s, error: %s\n" % (hostname, msg[0]))
+        sys.exit(1)
 
-        self.sock.settimeout(timeout)
+    self.sock.settimeout(timeout)
 
-        try:
-            self.sock.connect((hostname, port))
-        except socket.error, msg:
-            sys.stderr.write("connect() [Socket connection error] Cannot connect to %s:%d, error: %s\n" % (hostname, port, msg[0]))
-            sys.exit(2)
+    try:
+        self.sock.connect((hostname, port))
+    except socket.error, msg:
+        sys.stderr.write("connect() [Socket connection error] Cannot connect to %s:%d, error: %s\n" % (hostname, port, msg[0]))
+        sys.exit(2)
 
 
-    def __del__(self):
-        self.sock.close()
+  def SendCommand(self, cmd):
+    self.access_semaphor.acquire()
+    self.sock.send(cmd + '\n')
+    self.access_semaphor.release()
 
-    def SendCommand(self, cmd):
-        self.sock.send(cmd + '\n')
-
-    def Ask(self, cmd):
-        self.sock.send(cmd + '\n')
-        return self.sock.recv(2048)
-
-    def set_keepalives(self):
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 2)
+  def Ask(self, cmd):
+    self.access_semaphor.acquire()
+    self.sock.send(cmd + '\n')
+    reply = self.sock.recv(2048).strip('\n')
+    self.access_semaphor.release()
+    return reply
---
+++ utils/efushell.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import cmd, sys, os
+from SocketDriver import SimpleSocket
+
+class EFUShell(cmd.Cmd):
+
+   intro = "Event Formation Unit  Shell"
+   prompt = '(efushell) '
+   ip = "127.0.0.1"
+   port = 8888 
+   driver = SimpleSocket(ip, port)
+
+#
+# EFU Commands 
+#
+
+   def do_stats_clear(self, line):
+      'Clear statistics'
+      res = self.driver.Ask('STAT_RESET')
+      print res
+
+   def do_stats_get(self, line):
+      'Clear statistics'
+      res = self.driver.Ask('STAT_INPUT')
+      print res
+      res = self.driver.Ask('STAT_PROCESSING')
+      print res
+#      res = self.driver.Ask('STAT_OUTPUT')
+#      print res
+
+
+
+#
+# CMD behavior customization
+#
+   def do_shell(self, line):
+      'Run a shell command'
+      output = os.popen(line).read()
+      print output
+
+   def do_EOF(self, line):
+      return True
+
+   def emptyline(self):
+      print self.prompt
+
+   def do_quit(self, arg):
+      'Quit the shell'
+      sys.exit(0)
+
+if __name__ == '__main__':
+   EFUShell().cmdloop()
