71915ed8731cbb5ffd81c3e1379ba7c50f3604fe
Morten Jagd Christensen <Morten.Christensen@esss.se>
2016-11-29 15:43:42 +0100

    multiplot histograms, + finetuning test case

--- dataformats/cncs2016/histogram.py
+++ dataformats/cncs2016/histogram.py
@@ -8,10 +8,9 @@ import csv
 
 def histogram(text, bins, rng, values):
    n, bins, patches = plt.hist(values, bins, range=rng, normed=0, facecolor='green', alpha=0.75)
-
-   plt.xlabel(text)
-   plt.ylabel('Count')
-   plt.title(text)
+   #plt.xlabel(text)
+   plt.ylabel(text + ' count')
+   #plt.title(text)
    #plt.axis([40, 160, 0, 0.03])
    plt.grid(True)
 
@@ -20,18 +19,23 @@ N=1000000
 max = 1
 dt = []
 w1pos = []
+g1pos = []
 with open('1113.dat', 'rb') as csvfile:
   reader = csv.reader(csvfile, delimiter=',')
   for row in reader:
      dt.append(int(row[1]))
      w1pos.append(int(row[4]))
+     g1pos.append(int(row[8]))
      max = max + 1
      if (max == N):
-        plt.subplot(211)
+        plt.subplot(311)
         histogram("time", 50, None, dt)
-        plt.subplot(212)
+        plt.subplot(312)
         histogram("w1pos", 100, (150, 4000), w1pos)
+        plt.subplot(313)
+        histogram("g1pos", 100, (150, 4000), g1pos)
         plt.show()
         dt = []
         w1pos = []
+        g1pos = []
         max = 1
