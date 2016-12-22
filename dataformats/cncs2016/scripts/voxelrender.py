40d85192c8fa18bea4c1552f86743e2988423079
Morten Jagd Christensen <morten.christensen@esss.se>
2016-12-22 15:37:39 +0100

    polishing voxel visualization script

--- dataformats/cncs2016/scripts/voxelrender.py
+++ dataformats/cncs2016/scripts/voxelrender.py
@@ -4,21 +4,82 @@
 import vtk, sys
 from numpy import *
 
+# A simple function to be called when the user decides to quit the application.
+def exitCheck(obj, event):
+    if obj.GetEventPending() != 0:
+        obj.SetAbortRender(1)
+
+# Alpha channels
+def alphahandmade():
+   af = vtk.vtkPiecewiseFunction()
+   af.AddPoint(0, 0.0)
+   af.AddPoint(50, 0.1)
+   af.AddPoint(100, 0.2)
+   af.AddPoint(150, 0.4)
+   af.AddPoint(200, 0.9)
+   af.AddPoint(255, 1.0)
+   return af
+
+def alphalinear(lowcutoff):
+   af = vtk.vtkPiecewiseFunction()
+   for value in range(255):
+      if value >= lowcutoff:
+         af.AddPoint(value, value/255.0)
+   return af
+
+# Colormaps
+def greyscale():
+   cf = vtk.vtkColorTransferFunction()
+   for value in range(255):
+      cf.AddRGBPoint(  value, value/255.0, value/255.0, value/255.0)
+   return cf
+
+def blackbody():
+   cf = vtk.vtkColorTransferFunction()
+   cf.AddRGBPoint(  0, 0.0, 0.0, 1.0)
+   cf.AddRGBPoint( 64, 0.0, 1.0, 1.0)
+   cf.AddRGBPoint(128, 0.0, 1.0, 0.0)
+   cf.AddRGBPoint(192, 1.0, 1.0, 0.0)
+   cf.AddRGBPoint(255, 1.0, 0.0, 0.0)
+   return cf
+
+def blackbodyII():
+   cf = vtk.vtkColorTransferFunction()
+   cf.AddRGBPoint(  0, 1.0, 1.0, 1.0)
+   cf.AddRGBPoint( 50, 0.0, 0.0, 1.0)
+   cf.AddRGBPoint(100, 0.0, 1.0, 1.0)
+   cf.AddRGBPoint(150, 0.0, 1.0, 0.0)
+   cf.AddRGBPoint(200, 1.0, 1.0, 0.0)
+   cf.AddRGBPoint(250, 1.0, 0.0, 0.0)
+   return cf
+
+
 if len(sys.argv) != 2:
    print "usage: voxelrender.py filename"
    sys.exit(0)
 
 filename = sys.argv[1]
 
-x = 8
+x = 16
 y = 48
-z = 16
+z = 8
 
-data_matrix = fromfile(filename, dtype = int32).reshape([z, y, x]) * 1.0
+# Read linear data from file, interpret as 3D matrix with X,Y,Z dimensions
+# specified above
+data_matrix = fromfile(filename, dtype = int32).reshape([z,y,x])*1.0
+
+# When viewed from the front, the image appears mirrored in the x-direction
+# so we swap elements
+data_matrix = data_matrix[::-1,:,:]
+#data_matrix[0:16] = data_matrix.max() # line in the Z-direction from Voxel 1
+#data_matrix[16:32] = data_matrix.max() # line in z dir. from Voxel 17
+#data_matrix[1*48*16:1*48*16 + 16] = data_matrix.max() # z dir. second column
+
+# Normalize to 255 (for RGB color scales and transparency)
 data_matrix = uint8((data_matrix / data_matrix.max()) * 255.0)
 
+# Import into VTK
 dataImporter = vtk.vtkImageImport()
-
 data_string = data_matrix.tostring()
 dataImporter.CopyImportVoidPointer(data_string, len(data_string))
 
@@ -29,42 +90,26 @@ dataImporter.SetNumberOfScalarComponents(1)
 dataImporter.SetDataExtent( 0, x - 1, 0, y - 1, 0, z - 1)
 dataImporter.SetWholeExtent(0, x - 1, 0, y - 1, 0, z - 1)
 
-# The following class is used to store transparencyv-values for later retrival. In our case, we want the value 0 to be
-# completly opaque whereas the three different cubes are given different transperancy-values to show how it works.
-alphaChannelFunc = vtk.vtkPiecewiseFunction()
-alphaChannelFunc.AddPoint(0, 0.0)
-alphaChannelFunc.AddPoint(1, 1.0)
-alphaChannelFunc.AddPoint(2, 0.0)
-alphaChannelFunc.AddPoint(130, 0.3)
-alphaChannelFunc.AddPoint(180, 0.5)
-alphaChannelFunc.AddPoint(240, 1.0)
-alphaChannelFunc.AddPoint(255, 1.0)
+# Select color and transparency mappings
+#colorFunc = greyscale()
+#colorFunc = blackbody()
+colorFunc = blackbodyII()
+#alphafunc = alphalinear(30)
+alphafunc = alphahandmade()
 
-# This class stores color data and can create color tables from a few color points. For this demo, we want the three cubes
-# to be of the colors red green and blue.
-colorFunc = vtk.vtkColorTransferFunction()
-colorFunc.AddRGBPoint(  0, 1.0, 1.0, 1.0)
-colorFunc.AddRGBPoint(  1, 0.0, 0.0, 0.0)
-colorFunc.AddRGBPoint( 20, 0.0, 0.0, 1.0)
-colorFunc.AddRGBPoint( 50, 0.0, 1.0, 1.0)
-colorFunc.AddRGBPoint(130, 0.0, 1.0, 0.0)
-colorFunc.AddRGBPoint(180, 1.0, 1.0, 0.0)
-colorFunc.AddRGBPoint(255, 1.0, 0.0, 0.0)
-
-# The preavius two classes stored properties. Because we want to apply these properties to the volume we want to render,
-# we have to store them in a class that stores volume prpoperties.
+# Dunno
 volumeProperty = vtk.vtkVolumeProperty()
 volumeProperty.SetColor(colorFunc)
-volumeProperty.SetScalarOpacity(alphaChannelFunc)
+volumeProperty.SetScalarOpacity(alphafunc)
 
-# This class describes how the volume is rendered (through ray tracing).
+# Dunno
 compositeFunction = vtk.vtkVolumeRayCastCompositeFunction()
-# We can finally create our volume. We also have to specify the data for it, as well as how the data will be rendered.
+
 volumeMapper = vtk.vtkVolumeRayCastMapper()
 volumeMapper.SetVolumeRayCastFunction(compositeFunction)
 volumeMapper.SetInputConnection(dataImporter.GetOutputPort())
 
-# The class vtkVolume is used to pair the preaviusly declared volume as well as the properties to be used when rendering that volume.
+
 volume = vtk.vtkVolume()
 volume.SetMapper(volumeMapper)
 volume.SetProperty(volumeProperty)
@@ -74,10 +119,11 @@ renderer = vtk.vtkRenderer()
 renderWin = vtk.vtkRenderWindow()
 renderWin.AddRenderer(renderer)
 renderInteractor = vtk.vtkRenderWindowInteractor()
+#renderInteractor.AddObserver('KeyPressEvent', key_handler)
 renderInteractor.SetRenderWindow(renderWin)
 
+# Display axis
 transform = vtk.vtkTransform()
-#transform.Translate(-1.0, 0.0, 0.0)
 transform.Scale(3.0, 3.0, 3.0)
 
 axes = vtk.vtkAxesActor()
@@ -92,15 +138,15 @@ txt.SetInput(filename)
 txtprop=txt.GetTextProperty()
 txtprop.SetFontFamilyToArial()
 txtprop.SetFontSize(18)
-txtprop.SetColor(0.0, 0.0, 0.0)
+txtprop.SetColor(1.0, 1.0, 1.0)
 txt.SetDisplayPosition(10,1000)
 renderer.AddActor(txt)
 
 # We add the volume to the renderer ...
 renderer.AddVolume(volume)
-# ... set background color to white ...
-renderer.SetBackground(1, 1, 1)
-# ... and set window size.
+
+#renderer.SetBackground(1, 1, 1)  # white
+renderer.SetBackground(0, 0, 0)  # black
 renderWin.SetSize(1024, 1024)
 
 ax2 = vtk.vtkOrientationMarkerWidget()
@@ -110,15 +156,9 @@ ax2.EnabledOn()
 ax2.InteractiveOn()
 renderer.ResetCamera()
 
-# A simple function to be called when the user decides to quit the application.
-def exitCheck(obj, event):
-    if obj.GetEventPending() != 0:
-        obj.SetAbortRender(1)
-
 # Tell the application to use the function as an exit check.
 renderWin.AddObserver("AbortCheckEvent", exitCheck)
 
 renderInteractor.Initialize()
-# Because nothing will be rendered without any input, we order the first render manually before control is handed over to the main-loop.
 renderWin.Render()
 renderInteractor.Start()
