f15dfedd572fb0c926e0d74db5f824eb234b7ce9
Morten Jagd Christensen <morten.christensen@esss.se>
2017-01-06 11:52:27 +0100

    Adding cmake support to dynamic_pipeline, still does not work on both Linux and Mac

--- dynamic_pipelines/main.cpp
+++ dynamic_pipelines/main.cpp
@@ -3,18 +3,16 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  Pipeline nmx("./nmx.so");
-  Pipeline beer("./beer.so");
+  Pipeline nmx("./libnmx.so");
+  Pipeline beer("./libbeer.so");
 
   std::cout << "calling receive function nmx\n";
   for (int i = 0; i < 10; i++) {
     std::cout << i << " " << nmx.Rx(i) << "\n";
-    ;
   }
   std::cout << "calling receive function beer\n";
   for (int i = 0; i < 10; i++) {
     std::cout << i << " " << beer.Rx(i) << "\n";
-    ;
   }
   return 0;
 }
