c94f64860c21e831412119e2c257784da62f9765
Morten Jagd Christensen <mortenchristensen@esss.se>
2017-01-13 16:01:33 +0100

    linkbonding scripts

--- centos/linkbond/install.bash
+++ centos/linkbond/install.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-cp ifcfg-bond0 /etc/sysconfig/network-scripts/
+function errexit() {
+  echo "ERROR: $1"
+  exit 1
+}
+
+test -e $1 || errexit "invalid file, please specify ifcfg-bond.[test, fast, parallel]"
+
+cp $1 /etc/sysconfig/network-scripts/ifcfg-bond0
 cp ifcfg-eno49 /etc/sysconfig/network-scripts/
 cp ifcfg-eno50 /etc/sysconfig/network-scripts/
