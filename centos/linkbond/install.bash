3327963eacf87b09b1b0e6fe1ad4e22e85bcbdc1
Morten Jagd Christensen <mortenchristensen@esss.se>
2017-01-13 16:21:47 +0100

    linkbond scripts

--- centos/linkbond/install.bash
+++ centos/linkbond/install.bash
@@ -10,3 +10,6 @@ test -e $1 || errexit "invalid file, please specify ifcfg-bond.[test, fast, para
 cp $1 /etc/sysconfig/network-scripts/ifcfg-bond0
 cp ifcfg-eno49 /etc/sysconfig/network-scripts/
 cp ifcfg-eno50 /etc/sysconfig/network-scripts/
+
+echo "now execute the command: \n> sudo systemctl restart network"
+echo "probably better to \n> sudo reboot"
