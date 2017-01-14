586e19a95b3df6599ee35a4870d0f51398ef3c9e
Morten Jagd Christensen <mortenchristensen@esss.se>
2017-01-14 11:43:33 +0100

    made UUIDs unique, added checks in install script, changed IP address scheme

--- centos/linkbond/install.bash
+++ centos/linkbond/install.bash
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+if [[ $1 == "" ]]; then
+  echo "usage: install.sh [test|parallel|fast]"
+  echo 
+  echo "installs ifcfg-bond ifcfg-en049 and ifcfg-eno50 files"
+  exit 1
+fi
+
 function errexit() {
   echo "ERROR: $1"
   exit 1
 }
 
-test -e $1 || errexit "invalid file, please specify ifcfg-bond.[test, fast, parallel]"
+SCRIPTPATH=/etc/sysconfig/network-scripts/
+
+test -e ifcfg-bond0.$1 || errexit "$1 is not a valid target. Please specify one of [test, fast, parallel]"
 
 cp $1 /etc/sysconfig/network-scripts/ifcfg-bond0
 cp ifcfg-eno49 /etc/sysconfig/network-scripts/
