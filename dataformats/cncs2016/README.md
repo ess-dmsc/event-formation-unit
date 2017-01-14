db91629e9ddc81251a3120ac9733d108d4c29047
Morten Jagd Christensen <morten.christensen@esss.se>
2017-01-14 13:10:42 +0100

    changed multiplot.py to use getopt for better command line handling

--- dataformats/cncs2016/README.md
+++ dataformats/cncs2016/README.md
@@ -102,13 +102,21 @@ grid0 amplitudes and positions.
 
 ### Multiplot
 Reads a csv data file and generates a plot of one or more columns which is
-saved as a png file. First argument is a plot title, the second arguments are 
-the columns of the datafile that should be plotted simulteneously. Finally 
-the user can specify several csv files for this to be done.
+saved as a png file. Now takes several arguments to specify its behavior
+(see multiplot -h).
 
-    > ./multiplot.py "title" "2 1 7" output.csv [output2.csv ...]
+    > ./multiplot.py -t MyTitle -c "2 1 7" file1.csv
 
 Plots columns 2, 1 and 7 in a single figure.
+
+    > ./multiplot.py -b -o myfile.png -c "2 1 7" file1.csv
+
+Plots columns 2, 1 and 7 in a single figure. Saves to myfile.png but does not pause
+to display the plot (batch mode).
+
+    > ./multiplot.py file1.csv file2.csv file3.csv
+
+Plots columns 1 from each file in a separate graph.
 
 ### Heatmap
 Generates a heatmap of w0 positions as a function of file number.
--- dataformats/cncs2016/scripts/multiplot.py
+++ dataformats/cncs2016/scripts/multiplot.py
@@ -1,15 +1,52 @@
 #!/usr/bin/python
 
 import sys, pylab as pl, numpy as np
+import getopt
 
-if len(sys.argv) != 4:
-    print "usage: multiplot.py title columns files"
-    sys.exit(0)
+def usage():
+    print "usage: multiplot.py [options] files"
+    print "options"
+    print " -b, --batch                  does not pause for plotting"
+    print " -c, --columns columstr       specify the number of columns to simultaneously plot"
+    print " -t, --title textstr          provide a title for the plot"
+    print " -o, --output files           name for saved png file"
+    print " -h, --help                   prints this text"
+    print
+    print " Plots columns of the .csv files."
+    return
 
-title = sys.argv[1]
-cols = sys.argv[2]
-files = sys.argv[3:]
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hbc:t:o:", ["help", "title=", "columns=", "output="])
+except getopt.GetoptError as err:
+    print(err)
+    usage()
+    sys.exit(2)
+
+batch = 0
+title = "Multiplot"
+columns = "1"
+pngfile="multiplot.png"
+
+for opt, arg in opts:
+    if opt in ("-h", "--help"):
+        usage()
+        sys.exit()
+    elif opt in ("-c", "--columns"):
+        columns = arg
+    elif opt in ("-o", "--output"):
+        pngfile = arg
+    elif opt in ("-b", "--batch"):
+        batch = 1
+    elif opt in ("-t", "--title"):
+        title = arg
+
+files = args
 n = len(files)
+
+if n == 0:
+    print "Error: no files specified"
+    usage()
+    sys.exit(2)
 
 nc, nr = [1, n]
 if (n > 2):
@@ -22,10 +59,12 @@ for idx, file in enumerate(files):
    pl.subplot(nr, nc, idx + 1)
    data = np.loadtxt(file, delimiter=',', usecols = (1,2,3,4,5,6,7,8,9,10,11) )
    x = data[:,0]
-   for yi in cols.split():
+   for yi in columns.split():
      y = data[:,int(yi)]
      pl.plot(x,y, lw = 0.5)
 
-pl.legend(cols.split())
-pl.savefig('test.png')
-pl.show()
+pl.legend(columns.split())
+
+pl.savefig(pngfile)
+if not batch:
+    pl.show()
