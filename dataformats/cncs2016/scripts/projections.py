997e3a2f6a17ff2798c69b9c7527a2c4c2831134
Morten Jagd Christensen <morten.christensen@esss.se>
2017-01-15 13:27:31 +0100

    added intensity projection visualization

---
+++ dataformats/cncs2016/scripts/projections.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# Copyright (C) 2016, 2017 European Spallation Source ERIC
+
+import sys
+from numpy import genfromtxt
+import pylab as pl
+import matplotlib.pyplot as plt
+
+if len(sys.argv) != 2:
+   print("Usage ./projections.py runbasename")
+   sys.exit(0)
+
+file = sys.argv[1]
+
+xydata = genfromtxt(file +'.events.xyproj', delimiter=' ')
+zydata = genfromtxt(file +'.events.zyproj', delimiter=' ')
+xzdata = genfromtxt(file +'.events.xzproj', delimiter=' ')
+
+plt.suptitle(file)
+plt.subplot(1,3,1)
+
+plt.imshow(xydata)
+
+plt.subplot(1,3,2)
+plt.imshow(zydata)
+
+plt.subplot(1,3,3)
+plt.imshow(xzdata)
+plt.show()
--- dataformats/cncs2016/src/genpixids.cpp
+++ dataformats/cncs2016/src/genpixids.cpp
@@ -62,18 +62,27 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  int data[10];     /** holds channel data + time */
   assert(CNCS.getmaxpixelid() == 6144);
   int values[6144]; /** size is specific to CNCS object above */
-  memset(values, 0, 6144);
+  int xyproj[8][48];
+  int zyproj[48][16];
+  int xzproj[8][16];
+  memset(values, 0, sizeof(values));
+  memset(xyproj, 0, sizeof(xyproj));
+  memset(zyproj, 0, sizeof(zyproj));
+  memset(xzproj, 0, sizeof(xzproj));
 
   int events = 0;
   int badpixels = 0;
   int goodpixels = 0;
 
   DataSave coords(eventfile + ".coord");
+  DataSave xydata(eventfile + ".xyproj");
+  DataSave zydata(eventfile + ".zyproj");
+  DataSave xzdata(eventfile + ".xzproj");
   coords.tofile(std::string("# time,    wire, grid, pixl,   x,   y,  z\n"));
 
+  int data[10];     /** holds channel data + time */
   while (1) {
     ret = fscanf(ff, "%d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n", &data[0],
                  &data[1], &data[2], &data[3], &data[4], &data[5], &data[6],
@@ -84,33 +93,41 @@ int main(int argc, char *argv[]) {
     }
 
     events++;
-    int w0pos = data[4]; /** fifth field of the .events file */
-    int g0pos = data[8]; /** ninth field of the .events file */
-    int wireid = wcal[w0pos];
-    int gridid = gcal[g0pos];
 
+    int w0pos = data[4]; /** fifth field of the .events file */
+    int wireid = wcal[w0pos];
     if (wireid & 1) {
       wireid++;
     } else {
       wireid--;
     }
 
-    if (gridid <= 48) /** swap modules @todo verify */
+    int g0pos = data[8]; /** ninth field of the .events file */
+    int gridid = gcal[g0pos];
+    if (gridid <= 48) { /** swap modules @todo verify */
       gridid += 48;
-    else
+    } else {
       gridid -= 48;
-
-    int pixid = CNCS.getdetectorpixelid(1, gridid, wireid);
+    }
 
     auto time = data[1];
-    auto x = CNCS.getxcoord(pixid);
-    auto y = CNCS.getycoord(pixid);
-    auto z = CNCS.getzcoord(pixid);
 
+    int pixid = CNCS.getdetectorpixelid(1, gridid, wireid);
     if (pixid != -1) { /**< create intensity volume image */
+      auto x = CNCS.getxcoord(pixid);
+      auto y = CNCS.getycoord(pixid);
+      auto z = CNCS.getzcoord(pixid);
+      assert(x>=1 && x <= 8);
+      assert(y>=1 && y <= 48);
+      assert(z>=1 && z <= 16);
+
       goodpixels++;
-      values[pixid - 1]++;
-      char buf[1000];
+      values[pixid - 1]++; // 3D volume
+      xyproj[x-1][y-1]++;
+      zyproj[z-1][y-1]++;
+      xzproj[x-1][15 - (z-1)]++;
+
+      char buf[1000]; /**< @todo should be done in tofile() method */
       auto len = snprintf(buf, 1000, "%8d, %5d, %4d, %4d, %3d, %3d, %2d\n", time, wireid, gridid, pixid, x, y, z);
       coords.tofile(buf, len);
     } else {
@@ -121,6 +138,25 @@ int main(int argc, char *argv[]) {
   //for (int i = 0; i < 6144; i++) {
   //  printf("voxel: %4d, intensity %6d\n", i + 1, values[i]);
   //}
+
+  for (int y = 0; y < 48; y++) {
+    for (int x = 0; x < 8; x++) {
+      xydata.tofile(std::to_string(xyproj[x][y]) + " ");
+    }
+    for (int z = 0; z < 16; z++) {
+      zydata.tofile(std::to_string(zyproj[z][y]) + " ");
+    }
+    xydata.tofile("\n");
+    zydata.tofile("\n");
+  }
+
+  for (int z = 0; z < 16; z++) {
+    for (int x = 0; x < 8; x++) {
+      xzdata.tofile(std::to_string(xzproj[x][z]) + " ");
+    }
+    xzdata.tofile("\n");
+  }
+
   assert(events == goodpixels + badpixels);
   printf("Events:     %8d\nGoodpixels: %8d\nBadpixels:  %8d (%.2f%s)\n",
          events, goodpixels, badpixels, badpixels*100.0/events, "%");
