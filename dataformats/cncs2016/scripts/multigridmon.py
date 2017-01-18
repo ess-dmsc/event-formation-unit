7b033fbc0737250730cf4659d6c6e1498cdbfcc6
Morten Jagd Christensen <Morten.Christensen@esss.se>
2017-01-18 13:56:54 +0100

    added title and kafka file offset to plot

--- dataformats/cncs2016/scripts/multigridmon.py
+++ dataformats/cncs2016/scripts/multigridmon.py
@@ -35,18 +35,21 @@ class Proj:
         self.zy = numpy.zeros((self.y, self.z))
         self.xz = numpy.zeros((self.z, self.x))
 
-    def plot(self):
+    def plot(self, title):
         pl.ion()
-        plt.suptitle(file)
         pl.clf()
 
+        plt.suptitle(title)
         plt.subplot(1,3,1)
+        plt.title("x-y")
         plt.imshow(self.xy)
 
         plt.subplot(1,3,2)
+        plt.title("z-y")
         plt.imshow(self.zy)
 
         plt.subplot(1,3,3)
+        plt.title("x-z")
         plt.imshow(self.xz)
 
         pl.pause(0.001)
@@ -71,21 +74,29 @@ def main():
 
     print("Starting main loop")
 
-    plotevery =  5
+    plotint = 1
+    plotevery =  plotint
+    maxoffset = 0
+    minoffset = 999999999
+    plotrange = 10000
     while (True):
         try:
             msg = consumer.consume(block = True)
             if (msg != None):
+
                 a = bytearray(msg.value)
-                for i in range(80000):
+                for i in range(plotrange):
                     pixel = a[i*12 +5]*256 + a[i*12 +4]
                     #assert ?
                     proj.addpixel(pixel)
                 plotevery -= 1
+                maxoffset = max(msg.offset, maxoffset)
+                minoffset = min(msg.offset, minoffset)
                 if plotevery == 0:
-                    proj.plot()
+                    proj.plot("events: " + str(plotint * plotrange) + ", offset: " + str(minoffset) + "-" + str(maxoffset))
                     proj.clear()
-                    plotevery = 5
+                    minoffset = 999999999
+                    plotevery = plotint
                 #print("data: %02x %02x %02x %02x, %02x %02x %02x %02x " %
                 #      (a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]))
             else:
