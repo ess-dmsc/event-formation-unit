d36bdcc6a326b082ce09a45de708b73117f73004
Morten Jagd Christensen <Morten.Christensen@esss.se>
2017-02-17 15:01:25 +0100

    missing the last metric due to < instead of <=

--- prototype2/common/StatPublisher.cpp
+++ prototype2/common/StatPublisher.cpp
@@ -10,7 +10,7 @@ void StatPublisher::publish(std::shared_ptr<Detector> detector) {
   char buffer[1000];
   int unixtime = (int)time(NULL);
 
-  for (int i = 1; i < detector->statsize(); i++) {
+  for (int i = 1; i <= detector->statsize(); i++) {
     int len =
         sprintf(buffer, "%s %" PRIi64 " %d\n", detector->statname(i).c_str(),
                 detector->statvalue(i), unixtime);
