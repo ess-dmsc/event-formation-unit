247af9a048b88e77ca617d1b6daca75e0755dba3
Morten Jagd Christensen <morten.christensen@esss.se>
2017-03-03 09:03:00 +0100

    added ExitHandler.cpp and .h

---
+++ prototype2/efu/ExitHandler.cpp
@@ -0,0 +1,27 @@
+/** Copyright (C) 2016, 2017 European Spallation Source ERIC */
+
+
+#include <efu/ExitHandler.h>
+#include <common/Trace.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string>
+
+ExitHandler::ExitHandler() {
+  signal(SIGINT, &ExitHandler::signalhandler);
+  signal(SIGSEGV, &ExitHandler::signalhandler);
+  signal(SIGTERM, &ExitHandler::signalhandler);
+  signal(SIGBUS, &ExitHandler::signalhandler);
+}
+
+void ExitHandler::signalhandler(int sig){
+  XTRACE(MAIN, ALW, "efu2 terminated with signal %d", sig);
+  std::string message = "efu2 terminated with signal " + std::to_string(sig) + "\n";
+  GLOG_CRI(message);
+  exit(1);
+}
+
+void ExitHandler::Exit() {
+  GLOG_INF("efu2 terminated normally");
+  exit(0);
+}
---
+++ prototype2/efu/ExitHandler.h
@@ -0,0 +1,25 @@
+/** Copyright (C) 2016, 2017 European Spallation Source ERIC */
+
+/** @file
+ *
+ *  @brief Class to register exit handlers for program termination
+ */
+
+#pragma once
+#include <common/Trace.h>
+#include <stdlib.h>
+
+class ExitHandler {
+public:
+
+  /** @brief constructor does nothing */
+  ExitHandler();
+
+  /** @brief called to terminate the application, wrapper for exit() */
+  void Exit();
+
+private:
+
+  /** Traces an exit message, also sends to Graylog server */
+  static void signalhandler(int a);
+};
