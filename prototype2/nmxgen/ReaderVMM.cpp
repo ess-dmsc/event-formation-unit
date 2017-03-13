8f11f63f772037ac0904e0cc51bded59eab90ba3
MGS <martukas@gmail.com>
2017-03-13 15:03:08 +0100

    ReaderVMM fixes

--- prototype2/nmxgen/ReaderVMM.cpp
+++ prototype2/nmxgen/ReaderVMM.cpp
@@ -2,6 +2,7 @@
 
 #include <cstring>
 #include <nmxgen/ReaderVMM.h>
+#include <iostream>
 
 ReaderVMM::ReaderVMM(std::string filename) {
   data.resize(max_in_buf_ * 4, 0);
@@ -11,6 +12,7 @@ ReaderVMM::ReaderVMM(std::string filename) {
   if (!file_.has_group("RawVMM") ||
       !file_.open_group("RawVMM").has_dataset("points"))
     return;
+
   dataset_ = file_.open_group("RawVMM").open_dataset("points");
 
   auto shape = dataset_.shape();
@@ -23,9 +25,14 @@ ReaderVMM::ReaderVMM(std::string filename) {
 size_t ReaderVMM::read(char *buf) {
   size_t limit = std::min(current_ + max_in_buf_, total_);
   slabsize[0] = (limit - current_);
-  current_ = limit;
   index[0] = current_;
-  dataset_.read(data, slabsize, index);
-  memcpy(buf, data.data(), psize_ * slabsize[0]);
+
+  if (slabsize[0] > 0)
+  {
+    dataset_.read(data, slabsize, index);
+    memcpy(buf, data.data(), psize_ * slabsize[0]);
+  }
+
+  current_ = limit;
   return psize_ * slabsize[0];
 }
