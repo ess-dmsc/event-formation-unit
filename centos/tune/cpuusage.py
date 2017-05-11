547b3e9d9bb6a14e4017ed9ff60f9ceb9d95990f
Morten Jagd Christensen <morten.christensen@esss.se>
2017-05-11 09:07:23 +0200

    added scripts for cpu usage (thanks Jonas), setting txqueuelen, getting network performance parameters

--- centos/tune/cpuusage.py
+++ centos/tune/cpuusage.py
@@ -3,8 +3,15 @@
 
 import re
 import time
+import os
+import sys
 
 proc_file = open("/proc/stat", "r")
+
+args = sys.argv[1:]
+
+def extract_cpu_nr(cpu_str):
+    return int(cpu_str[3:])
 
 def getStats():
     proc_file.seek(0)
@@ -28,15 +35,19 @@ def main():
     while (True):
         time.sleep(1)
         new_stats = getStats()
-        print("_" * 60)
-        print(("{:7s}" * 8).format("cpu", *names))
-        for cpu in new_stats:
+        cpu_list = list(new_stats.keys())
+        cpu_list.sort(key = extract_cpu_nr)
+        sys.stderr.write("\x1b[2J\x1b[H")
+        print(("{:7s} " * 8).format("cpu", *names))
+        print("–" * 70)
+        for cpu in cpu_list:
+         if cpu in args or len(args) == 0:
             res_list = []
             old_total = float(sum(old_stats[cpu].values()))
             new_total = float(sum(new_stats[cpu].values()))
             for name in names:
                 res_list.append(100.0 * (new_stats[cpu][name] - old_stats[cpu][name]) / (new_total - old_total))
-            print((cpu + "{:7.1f}"*7).format(*res_list))
+            print(("{:7s} " + "{:7.1f} "*7).format(cpu, *res_list))
         old_stats = new_stats
 
 if __name__ == '__main__':
