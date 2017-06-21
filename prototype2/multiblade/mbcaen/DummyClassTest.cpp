53b1c3308ec08bb0e06bd2d23d004de710e664a1
Morten Jagd Christensen <morten.christensen@esss.se>
2017-06-21 12:20:13 +0200

    added directory structure and skeleton for unit tests for multiblade detectors

---
+++ prototype2/multiblade/mbcaen/DummyClassTest.cpp
@@ -0,0 +1,27 @@
+/** Copyright (C) 2017 European Spallation Source ERIC */
+
+#include <multiblade/mbcaen/DummyClass.h>
+
+#include <test/TestBase.h>
+
+using namespace std;
+
+class DummyClassTest : public TestBase {
+protected:
+
+  virtual void SetUp() { }
+  virtual void TearDown() { }
+
+};
+
+/** Test cases below */
+TEST_F(DummyClassTest, Constructor) {
+   ASSERT_TRUE(1);
+   ASSERT_FALSE(0);
+   ASSERT_EQ(42, 42);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
