6d8fa4be4fa31a37826da32d215ac94b2df6c535
Morten Jagd Christensen <Morten.Christensen@esss.se>
2017-07-03 14:33:58 +0200

    fixing some linking problems associated with c++11 abi

--- prototype2/common/ProducerTest.cpp
+++ prototype2/common/ProducerTest.cpp
@@ -11,8 +11,7 @@ int fail = -1; // Dont fail
 typedef RdKafka::Conf *(* pcreate) (RdKafka::Conf::ConfType);
 
 RdKafka::Conf * RdKafka::Conf::create(ConfType type) {
-  pcreate real_create;
-  real_create  = (pcreate)dlsym(RTLD_NEXT, "_ZN7RdKafka4Conf6createENS0_8ConfTypeE"); // nm -C
+  pcreate real_create = (pcreate)dlsym(RTLD_NEXT, "_ZN7RdKafka4Conf6createENS0_8ConfTypeE"); // nm -C
   if (fail != -1 && type == fail) {
     printf("Forcing RdKafka::Conf::create() to fail\n");
     return nullptr;
@@ -21,10 +20,10 @@ RdKafka::Conf * RdKafka::Conf::create(ConfType type) {
   }
 }
 
-typedef RdKafka::Producer *(* pcreateprod) (RdKafka::Conf*, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&);
-RdKafka::Producer * RdKafka::Producer::create(RdKafka::Conf* type, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >& str) {
-  pcreateprod real_create;
-  real_create  = (pcreateprod)dlsym(RTLD_NEXT, "_ZN7RdKafka8Producer6createEPNS_4ConfERNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE"); // nm -C
+typedef RdKafka::Producer *(* pcreateprod) (RdKafka::Conf*, std::string&);
+RdKafka::Producer * RdKafka::Producer::create(RdKafka::Conf* type, std::string& str) {
+  pcreateprod real_create = (pcreateprod)dlsym(RTLD_NEXT,
+            "_ZN7RdKafka8Producer6createEPNS_4ConfERSs"); // nm -C librdkafka.a
   if (fail == 777) {
     printf("Forcing RdKafka::Producer::create() to fail\n");
     return nullptr;
@@ -33,10 +32,10 @@ RdKafka::Producer * RdKafka::Producer::create(RdKafka::Conf* type, std::__cxx11:
   }
 }
 
-typedef RdKafka::Topic *(* pcreatetopic) (RdKafka::Handle*, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&, RdKafka::Conf*, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&);
-RdKafka::Topic * RdKafka::Topic::create(RdKafka::Handle* handle, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const& topic, RdKafka::Conf* conf, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >& str) {
-  pcreatetopic real_create;
-  real_create  = (pcreatetopic)dlsym(RTLD_NEXT, "_ZN7RdKafka5Topic6createEPNS_6HandleERKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEPNS_4ConfERS8_"); // nm -C
+typedef RdKafka::Topic *(* pcreatetopic) (RdKafka::Handle*, std::string const&, RdKafka::Conf*, std::string&);
+RdKafka::Topic * RdKafka::Topic::create(RdKafka::Handle* handle, std::string const& topic, RdKafka::Conf* conf, std::string& str) {
+  pcreatetopic real_create  = (pcreatetopic)dlsym(RTLD_NEXT,
+             "_ZN7RdKafka5Topic6createEPNS_6HandleERKSsPNS_4ConfERSs"); // nm -C librdkafka.a
   if (fail == 888) {
     printf("Forcing RdKafka::Topic::create() to fail\n");
     return nullptr;
@@ -52,7 +51,7 @@ class ProducerTest : public TestBase {
 
 TEST_F(ProducerTest, ConstructorOK) {
   Producer prod{"nobroker", "notopic"};
-  auto ret = prod.produce(0, 0);
+  int ret = prod.produce(0, 0);
   ASSERT_EQ(ret, RdKafka::ERR_NO_ERROR);
 }
 
