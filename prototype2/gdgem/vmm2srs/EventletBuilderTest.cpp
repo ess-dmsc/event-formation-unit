210ec99956dbc2cde315a5172c180cd5285d806d
Morten Jagd Christensen <morten.christensen@esss.se>
2017-08-23 15:53:22 +0200

    removed function calls in unit test as they were operating on uninitialized data

--- prototype2/gdgem/vmm2srs/EventletBuilderTest.cpp
+++ prototype2/gdgem/vmm2srs/EventletBuilderTest.cpp
@@ -5,6 +5,12 @@
 #include <test/TestBase.h>
 #include <unistd.h>
 
+std::vector<uint8_t> data_3_ch0    { 0x00, 0x33, 0x71, 0x37, 0x56, 0x4d, 0x32, 0x00, 0x4c, 0x39, 0x2f, 0x60, // hdr
+                                0xe0, 0x92, 0x24, 0x02, 0xfc, 0x00, 0x00, 0x00, // hit 1
+                                0xe0, 0x92, 0x34, 0x01, 0xf2, 0x00, 0x00, 0x00, // hit 2
+                                0xe0, 0x92, 0x20, 0x22, 0xea, 0x00, 0x00, 0x00, // hit 3
+                              };
+
 class EventletBuilderTest : public TestBase {
 protected:
   EventletBuilder *builder;
@@ -22,12 +28,13 @@ TEST_F(EventletBuilderTest, Process) {
   Clusterer clusterer(30);
   NMXVMM2SRSData data(1125);
 
-  auto num1 = builder->process_readout(data, clusterer);
-  ASSERT_EQ(num1, 0);
+  //auto num1 = builder->process_readout(data, clusterer);
+  //ASSERT_EQ(num1, 0);
 
-  data.elems = 4;
-  auto num2 = builder->process_readout(data, clusterer);
-  ASSERT_EQ(num2, 4);
+  //data.elems = 4;
+  //auto num2 = builder->process_readout(data, clusterer);
+  //ASSERT_EQ(num2, 4);
+  MESSAGE() << "Bad test, working on uninitialized data\n";
 }
 
 int main(int argc, char **argv) {
