651e4402808cdd4f9e2df89c92e8869923c8cc1f
MGS <martukas@gmail.com>
2017-09-06 00:31:16 +0200

    generic Geometry class, with tests, NMX Cmake file reformatted, Geometry for Multigrid renamed files

--- prototype2/multigrid/mgcncs/MultigridGeometryTest.cpp
+++ prototype2/multigrid/mgcncs/MultigridGeometryTest.cpp
@@ -1,7 +1,7 @@
 /** Copyright (C) 2016, 2017 European Spallation Source ERIC */
 
 #include <libs/include/TSCTimer.h>
-#include <multigrid/mgcncs/Geometry.h>
+#include <multigrid/mgcncs/MultigridGeometry.h>
 #include <test/TestBase.h>
 
 using namespace std;
