01780ceebaa242aab32129b2c19c518202f1c44b
Morten Jagd Christensen <Morten.Christensen@esss.se>
2017-10-27 14:56:36 +0200

    basename(__FILE__) does not work in Ubuntu

--- prototype2/common/Trace.h
+++ prototype2/common/Trace.h
@@ -65,7 +65,7 @@
 #if 1
 #define XTRACE(group, level, fmt, ...)                                         \
   (void)(((TRC_L_##level >= TRC_LEVEL) && (TRC_MASK & TRC_G_##group))          \
-             ? printf("%-3s %-20s %5d %-s - " fmt, #level, basename((char*)__FILE__), __LINE__,  \
+             ? printf("%-3s %-80s %5d %-s - " fmt, #level, __FILE__, __LINE__,  \
                       #group, ##__VA_ARGS__)                                   \
              : 0)
 #endif
