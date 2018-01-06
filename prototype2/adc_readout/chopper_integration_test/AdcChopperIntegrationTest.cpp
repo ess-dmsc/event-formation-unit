3febe0d1190e3d993596023f185c672801cf39f0
Jonas Nilsson <jonas.nilsson@esss.se>
2018-01-06 02:01:23 +0100

    Chopper integration test code.

--- prototype2/adc_readout/chopper_integration_test/AdcChopperIntegrationTest.cpp
+++ prototype2/adc_readout/chopper_integration_test/AdcChopperIntegrationTest.cpp
@@ -1,26 +1,87 @@
 #include <iostream>
 #include <fstream>
 #include "UDPServer.h"
+#include "PacketGenerator.h"
+#include "TDCGenerator.h"
+#include <chrono>
+#include <thread>
+#include <csignal>
+#include <iomanip>
+#include "Producer.h"
+#include <random>
+#include <algorithm>
 
-int main(int argc, char **argv) {
-  std::ifstream InStream("DataPacketTemplate.dat", std::ios::binary);
-  if (not InStream.good()) {
-    std::cout << "Stream is not good, exiting." << std::endl;
-    return 0;
-  }
+bool RunLoop = true;
+
+void signalHandler(int signal) {
+  std::cout << "Got exit signal:" << signal << std::endl;
+  RunLoop = false;
+}
+
+struct MonitorSimParams {
+  std::uint16_t ChannelNr;
+  int MeanNrOfPulses;
+  double TimeOffset;
+  double TimeWidth;
+};
+
+struct MonitorPulseInfo {
+  std::uint16_t ChannelNr;
+  double TimeStamp;
+  double Amplitude;
+};
+
+bool PulseSortingFunction(const MonitorPulseInfo &Pulse1, const MonitorPulseInfo &Pulse2) {
+  return Pulse1.TimeStamp < Pulse2.TimeStamp;
+}
+
+int main() {
+  const long double ChopperFrequency = 14;
+  std::chrono::duration<long double> ChopperPeriod(1 / ChopperFrequency);
+  auto PulseTime = std::chrono::system_clock::now() + ChopperPeriod;
+  std::signal(SIGINT, signalHandler);
+  Producer KafkaProducer("localhost", "tdc_data");
   
-  InStream.seekg(0, std::ios::end);
-  std::size_t FileSize = InStream.tellg();
-  InStream.seekg(0, std::ios::beg);
-  std::unique_ptr<std::uint8_t[]> TemplatePacketData(new std::uint8_t[FileSize]);
-  InStream.read(reinterpret_cast<char*>(TemplatePacketData.get()), FileSize);
-  UDPServer server(2048, 9000);
-  std::uint8_t TestData[150];
-  server.TransmitPacket(TestData, 150);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  
+  std::vector<MonitorSimParams> Monitors;
+  Monitors.emplace_back(MonitorSimParams{0, 10, -50e-6, 10e-6}); //Pulses, offset, width
+  Monitors.emplace_back(MonitorSimParams{1, 10, 150e-6, 10e-6}); //Pulses, offset, width
+  
+  ADC::PacketGenerator AdcGenerator;
+  TDC::TDCGenerator TdcGenerator("mini_chopper");
+  
+  UDPServer server(2048, 65535);
   while (not server.IsOk()) {
   }
-  for (int i = 0; i < 1000000; ++i) {
-    server.TransmitPacket(TemplatePacketData.get(), FileSize);
+  
+  while (RunLoop) {
+    PulseTime += ChopperPeriod;
+    std::this_thread::sleep_until(PulseTime);
+    long double FloatTime = std::chrono::duration_cast<std::chrono::duration<long double>>(PulseTime.time_since_epoch()).count();
+    
+    //auto TdcPacket = TdcGenerator.GetTDCPacket(double(FloatTime));
+    //KafkaProducer.produce((char*)TdcPacket.Pointer, TdcPacket.Size);
+    
+    std::vector<MonitorPulseInfo> AllPulses;
+    for (auto &Monitor: Monitors) {
+      std::poisson_distribution<> NrOfPulsesGen(Monitor.MeanNrOfPulses);
+      int NrOfPulses = NrOfPulsesGen(gen);
+      std::normal_distribution<> TimeOffsetGen{Monitor.TimeOffset,Monitor.TimeWidth};
+      for (int i = 0; i < NrOfPulses; i++) {
+        double CurrentTimeOffset = TimeOffsetGen(gen);
+        double CurrentTimeStamp = FloatTime + CurrentTimeOffset;
+        AllPulses.emplace_back(MonitorPulseInfo{Monitor.ChannelNr, CurrentTimeStamp, 1.0});
+      }
+    }
+    std::sort(AllPulses.begin(), AllPulses.end(), PulseSortingFunction);
+    for (auto &Pulse : AllPulses) {
+      std::uint32_t TS_Seconds(Pulse.TimeStamp);
+      auto AdcPacket = AdcGenerator.GeneratePacket(TS_Seconds, Pulse.TimeStamp - TS_Seconds, Pulse.ChannelNr);
+      
+      server.TransmitPacket(AdcPacket.Pointer, AdcPacket.Size);
+    }
   }
   std::cout << "Waiting for transmit thread to exit!" << std::endl;
   return 0;
