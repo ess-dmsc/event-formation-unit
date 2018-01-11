3b9e0b998cfa832f35a2041dff3ee725e2e0d721
Morten Jagd Christensen <morten.christensen@esss.se>
2018-01-11 15:29:57 +0100

    added doxygen comments

--- prototype2/common/ReadoutSerializer.h
+++ prototype2/common/ReadoutSerializer.h
@@ -17,29 +17,38 @@
 
 class ReadoutSerializer {
 public:
-  /** @todo document */
-  ReadoutSerializer(size_t maxarraylength, Producer &prod);
+  /** @brief Create the ReadoutSerializer
+   * @param maxentries the number of readout tuples to buffer before sending to Kafka
+   * @param producer Kafka producer to use for publishing the readout data
+   */
+  ReadoutSerializer(size_t maxentries, Producer &producer);
 
-  /** @todo document */
+  /** @brief empty destructor */
   ~ReadoutSerializer();
 
-  /** @todo document */
+  /** @brief function to add a readout tuple to an array for later publishing to Kafka
+   * @param plane arbitrary data multiplexer (x, y, module, rack, ...)
+   * @param channel datasource identifier (strip, wire, grid, ...)
+   * @param time timestamp
+   * @param adc digitizer adc value (charge, voltage, ...)
+   */
   int addEntry(uint16_t plane, uint16_t channel, uint32_t time, uint16_t adc);
 
-  /** @todo document */
+  /** @brief publish data to Kafka broker and clear internal counters */
   int produce();
 
+  /** @brief return the number of queues samples */
   size_t getNumEntries(){return entries;};
 
 private:
-  size_t maxlen{0};
-  flatbuffers::FlatBufferBuilder builder;
-  Producer &producer;
+  size_t maxlen{0}; /**< maximum number of entries in array */
+  flatbuffers::FlatBufferBuilder builder; /**< google flatbuffer builder */
+  Producer &producer; /**< wrapper for Kafka producer */
 
   // Will be used to create MONHit
-  size_t entries{0};
-  std::vector<uint16_t> planes;
-  std::vector<uint32_t> times;
-  std::vector<uint16_t> channels;
-  std::vector<uint16_t> adcs;
+  size_t entries{0}; /**< current number of queues etries */
+  std::vector<uint16_t> planes; /**< described above  */
+  std::vector<uint32_t> times; /**< described above */
+  std::vector<uint16_t> channels; /**< described above */
+  std::vector<uint16_t> adcs; /**< described above */
 };
