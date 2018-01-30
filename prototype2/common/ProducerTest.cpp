a512db310bff030882ed0bf1aa56692f0f465d7e
Morten Jagd Christensen <morten.christensen@esss.se>
2018-01-30 15:48:08 +0100

    ported producertest from other branhc

--- prototype2/common/ProducerTest.cpp
+++ prototype2/common/ProducerTest.cpp
@@ -8,14 +8,46 @@
 
 int fail = -1; // Dont fail
 
+// Create pointers to the retuned objects
 typedef RdKafka::Conf *(*pcreate)(RdKafka::Conf::ConfType);
+typedef RdKafka::Producer *(*pcreateprod)(RdKafka::Conf *, std::string &);
+typedef RdKafka::Topic *(*pcreatetopic)(RdKafka::Handle *, std::string const &,
+                                        RdKafka::Conf *, std::string &);
 
-RdKafka::Conf *RdKafka::Conf::create(ConfType type) {
+/**Attmpt to load primary and alternatively secondary symbols using dlsym()
+ * Created to cover different compilations of librdkafka (old and new abis)
+ * Used for forcing external functions to fail for unit testing purposes
+ * exit if none of the symbols can be loaded.
+**/
+void * loadsyms(const char * primary, const char * secondary) {
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpedantic"
-  pcreate real_create = (pcreate)dlsym(
-      RTLD_NEXT, "_ZN7RdKafka4Conf6createENS0_8ConfTypeE"); // nm -C
+  void * vpsym = dlsym(RTLD_NEXT, primary);
+
+  if (vpsym == NULL) {
+    printf("Could not load primary symbol: %s\n", primary);
+    printf("Trying secondary...\n");
+
+    dlerror();
+    vpsym = dlsym(RTLD_NEXT, secondary);
+    if (vpsym == NULL) {
+      printf("Could not load secondary symbol: %s\n", secondary);
+      printf("Error stubbing kafka functions\n");
+      exit(1);
+    }
+  }
 #pragma GCC diagnostic pop
+  return vpsym;
+}
+
+
+/** Intercept RdKafka::Conf::create() */
+RdKafka::Conf *RdKafka::Conf::create(ConfType type)
+{
+  pcreate real_create = (pcreate)loadsyms(
+      "_ZN7RdKafka4Conf6createENS0_8ConfTypeE",
+      "_ZN7RdKafka4Conf6createENS0_8ConfTypeE"); // nm -C
+
   if (fail != -1 && type == fail) {
     printf("Forcing RdKafka::Conf::create() to fail\n");
     return nullptr;
@@ -24,15 +56,14 @@ RdKafka::Conf *RdKafka::Conf::create(ConfType type) {
   }
 }
 
-typedef RdKafka::Producer *(*pcreateprod)(RdKafka::Conf *, std::string &);
+
+/** Intercept RdKafka::Producer::create() */
 RdKafka::Producer *RdKafka::Producer::create(RdKafka::Conf *type,
                                              std::string &str) {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wpedantic"
-  pcreateprod real_create = (pcreateprod)dlsym(
-      RTLD_NEXT,
-      "_ZN7RdKafka8Producer6createEPNS_4ConfERSs"); // nm -C librdkafka.a
-#pragma GCC diagnostic pop
+  pcreateprod real_create = (pcreateprod)loadsyms(
+      "_ZN7RdKafka8Producer6createEPNS_4ConfERSs",
+      "_ZN7RdKafka8Producer6createEPNS_4ConfERNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE"); // nm -C librdkafka.a
+
   if (fail == 777) {
     printf("Forcing RdKafka::Producer::create() to fail\n");
     return nullptr;
@@ -41,18 +72,16 @@ RdKafka::Producer *RdKafka::Producer::create(RdKafka::Conf *type,
   }
 }
 
-typedef RdKafka::Topic *(*pcreatetopic)(RdKafka::Handle *, std::string const &,
-                                        RdKafka::Conf *, std::string &);
+
+/** Intercept RdKafka::Topic::create() */
 RdKafka::Topic *RdKafka::Topic::create(RdKafka::Handle *handle,
                                        std::string const &topic,
                                        RdKafka::Conf *conf, std::string &str) {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wpedantic"
-  pcreatetopic real_create = (pcreatetopic)dlsym(
-      RTLD_NEXT,
-      "_ZN7RdKafka5Topic6createEPNS_6HandleERKSsPNS_4ConfERSs"); // nm -C
-                                                                 // librdkafka.a
-#pragma GCC diagnostic pop
+
+  pcreatetopic real_create = (pcreatetopic)loadsyms(
+      "_ZN7RdKafka5Topic6createEPNS_6HandleERKSsPNS_4ConfERSs",
+      "_ZN7RdKafka5Topic6createEPNS_6HandleERKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEPNS_4ConfERS8_");
+
   if (fail == 888) {
     printf("Forcing RdKafka::Topic::create() to fail\n");
     return nullptr;
