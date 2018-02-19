8bc7b80218685e808c570dd60787b89c505705ee
MGS <martin.shetty@esss.se>
2018-02-19 20:05:04 +0100

    qwlerhuaneroiqcur

--- jenkins/cncsdata.bash
+++ jenkins/cncsdata.bash
@@ -15,6 +15,7 @@ pushd build
 /opt/dm_group/virtualenv/conan/bin/conan install --build=outdated ..
 source activate_run.sh
 cmake ../dataformats  || errexit "cmake failed"
+make                  || errexit "make failed"
 make runtest          || errexit "make runtest failed"
 
 popd
