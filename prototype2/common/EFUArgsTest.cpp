dbf38de2a0b64f53d561f4d22b6f7f220d394caf
Jonas Nilsson <jonas.nilsson@esss.se>
2018-02-22 10:03:07 +0100

    Re-introduced unit tests.

--- prototype2/common/EFUArgsTest.cpp
+++ prototype2/common/EFUArgsTest.cpp
@@ -1,5 +1,19 @@
 /** Copyright (C) 2016, 2017 European Spallation Source ERIC */
 
+#ifndef __llvm__
+#if __GNUC__ == 4
+#pragma message("GCC4 does not implement regular expressions. Ignoring some unit tests.")
+#else
+#define RUN_REGEX_UNIT_TESTS
+#endif
+#else
+#define RUN_REGEX_UNIT_TESTS
+#endif
+
+#ifdef RUN_REGEX_UNIT_TESTS
+#pragma message("Unit tests testing regular expressions are enabled.")
+#endif
+
 #include <common/EFUArgs.h>
 #include <test/TestBase.h>
 
@@ -107,6 +121,32 @@ TEST_F(EFUArgsTest, VerifyCommandLineOptions) {
   ASSERT_EQ("10.0.0.1", glsettings.address);
   ASSERT_EQ(8989, settings.CommandServerPort);
 }
+
+#ifdef RUN_REGEX_UNIT_TESTS
+TEST_F(EFUArgsTest, CoreAffinityOption) {
+  int myargc = 5;
+  const char *myargv[] = {"progname", "-d", "dummydetector", "-c", "thread1:5"};
+  
+  EFUArgs efu_args;
+  auto ret = efu_args.parseFirstPass(myargc, (char **)myargv);
+  ASSERT_EQ(ret, EFUArgs::Status::CONTINUE); // has detector
+  
+  ASSERT_EQ(myargc, 5);
+  ASSERT_TRUE(myargv != NULL);
+}
+
+TEST_F(EFUArgsTest, CoreAffinityOptionFailure) {
+  int myargc = 5;
+  const char *myargv[] = {"progname", "-d", "dummydetector", "-c", "thread1:h"};
+  
+  EFUArgs efu_args;
+  auto ret = efu_args.parseSecondPass(myargc, (char **)myargv);
+  ASSERT_EQ(ret, EFUArgs::Status::EXIT); // has detector
+  
+  ASSERT_EQ(myargc, 5);
+  ASSERT_TRUE(myargv != NULL);
+}
+#endif
 
 TEST_F(EFUArgsTest, HelpText) {
   int myargc = 2;
