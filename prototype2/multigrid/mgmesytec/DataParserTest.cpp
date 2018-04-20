1c4fcacc5bf93d3660d18d9d2c25123884ade38b
Morten Jagd Christensen <morten.christensen@esss.se>
2018-04-20 12:27:08 +0200

    making unit tests run, after code changes from review

--- prototype2/multigrid/mgmesytec/DataParserTest.cpp
+++ prototype2/multigrid/mgmesytec/DataParserTest.cpp
@@ -1,9 +1,7 @@
 /** Copyright (C) 2016, 2017 European Spallation Source ERIC */
 
-//#include <algorithm>
-//#include <memory>
 #include <gdgem/nmx/Hists.h> // @fixme
-#include <multigrid/mgmesytec/Data.h>
+#include <multigrid/mgmesytec/DataParser.h>
 #include <multigrid/mgmesytec/TestData.h>
 #include <test/TestBase.h>
 
@@ -28,20 +26,20 @@ protected:
 /** Test cases below */
 TEST_F(MesytecDataTest, ErrUnsupportedCommand) {
   auto res = mesytec.parse((char *)&err_unsupported_cmd[0], err_unsupported_cmd.size(), hists, *fbserializer, *serializer);
-  ASSERT_EQ(res, -MesytecData::error::EUNSUPP);
+  ASSERT_EQ(res, MesytecData::error::EUNSUPP);
 
   res = mesytec.parse((char *)&err_unsupported_cmd_II[0], err_unsupported_cmd_II.size(), hists, *fbserializer, *serializer);
-  ASSERT_EQ(res, -MesytecData::error::EUNSUPP);
+  ASSERT_EQ(res, MesytecData::error::EUNSUPP);
 }
 
 TEST_F(MesytecDataTest, ErrNoSisReadoutHeader) {
   auto res = mesytec.parse((char *)&err_no_sis_readout_header[0], err_no_sis_readout_header.size(), hists, *fbserializer, *serializer);
-  ASSERT_EQ(res, -MesytecData::error::EHEADER);
+  ASSERT_EQ(res, MesytecData::error::EHEADER);
 }
 
 TEST_F(MesytecDataTest, ErrNoSisReadoutTrailer) {
   auto res = mesytec.parse((char *)&err_no_sis_readout_trailer[0], err_no_sis_readout_trailer.size(), hists, *fbserializer, *serializer);
-  ASSERT_EQ(res, -MesytecData::error::EHEADER);
+  ASSERT_EQ(res, MesytecData::error::EHEADER);
 }
 
 TEST_F(MesytecDataTest, ErrNoTimeStamp) {
@@ -52,19 +50,19 @@ TEST_F(MesytecDataTest, ErrNoTimeStamp) {
 
 TEST_F(MesytecDataTest, ErrNoEndDataCookie) {
   auto res = mesytec.parse((char *)&err_no_end_data_cookie[0], err_no_end_data_cookie.size(), hists, *fbserializer, *serializer);
-  ASSERT_EQ(res, -MesytecData::error::EHEADER);
+  ASSERT_EQ(res, MesytecData::error::EHEADER);
 }
 
 TEST_F(MesytecDataTest, ErrPktShort) {
   auto res = mesytec.parse((char *)&err_pkt_too_short[0], err_pkt_too_short.size(), hists, *fbserializer, *serializer);
 
-  ASSERT_EQ(res, -MesytecData::error::ESIZE);
+  ASSERT_EQ(res, MesytecData::error::ESIZE);
 }
 
 
 TEST_F(MesytecDataTest, ParseRecordedWSData) {
   auto res = mesytec.parse((char *)&ws1[0], ws1.size(), hists, *fbserializer, *serializer);
-  ASSERT_EQ(res, -MesytecData::error::OK);
+  ASSERT_EQ(res, MesytecData::error::OK);
   ASSERT_EQ(mesytec.readouts, 128);
   ASSERT_EQ(mesytec.discards, 0);
   ASSERT_EQ(mesytec.triggers, mesytec.geometry_errors + mesytec.events);
@@ -74,7 +72,7 @@ TEST_F(MesytecDataTest, ParseRecordedWSDataDiscardAll) {
   mesytec.setWireThreshold(60000, 65535);
   mesytec.setGridThreshold(60000, 65535);
   auto res = mesytec.parse((char *)&ws1[0], ws1.size(), hists, *fbserializer, *serializer);
-  ASSERT_EQ(res, -MesytecData::error::OK);
+  ASSERT_EQ(res, MesytecData::error::OK);
   ASSERT_EQ(mesytec.readouts, 128);
   ASSERT_EQ(mesytec.discards, 128);
   ASSERT_EQ(mesytec.triggers, mesytec.geometry_errors + mesytec.events);
@@ -82,21 +80,21 @@ TEST_F(MesytecDataTest, ParseRecordedWSDataDiscardAll) {
 
 TEST_F(MesytecDataTest, ParseRecordedWSDataII) {
   auto res = mesytec.parse((char *)&ws2[0], ws2.size(), hists, *fbserializer, *serializer);
-  ASSERT_EQ(res, -MesytecData::error::OK);
+  ASSERT_EQ(res, MesytecData::error::OK);
   ASSERT_EQ(mesytec.readouts, 256);
   ASSERT_EQ(mesytec.triggers, mesytec.geometry_errors + mesytec.events);
 }
 
 TEST_F(MesytecDataTest, ParseRecordedWSDataIII) {
   auto res = mesytec.parse((char *)&ws3[0], ws3.size(), hists, *fbserializer, *serializer);
-  ASSERT_EQ(res, -MesytecData::error::OK);
+  ASSERT_EQ(res, MesytecData::error::OK);
   ASSERT_EQ(mesytec.readouts, 256); // Readout provides more than 92 channels
   ASSERT_EQ(mesytec.triggers, mesytec.geometry_errors + mesytec.events);
 }
 
 TEST_F(MesytecDataTest, ParseRecordedWSDataMultipleTriggers) {
   auto res = mesytec.parse((char *)&ws4[0], ws4.size(), hists, *fbserializer, *serializer);
-  ASSERT_EQ(res, -MesytecData::error::OK);
+  ASSERT_EQ(res, MesytecData::error::OK);
   ASSERT_EQ(mesytec.triggers, 36);
   ASSERT_EQ(mesytec.readouts, 54);
   ASSERT_EQ(mesytec.discards, 0);
