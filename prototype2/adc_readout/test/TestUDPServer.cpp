fe364a65f67f02c3a819e103a0a6227e9442985a
Martin Shetty <martukas@gmail.com>
2018-05-01 06:04:55 +0200

    fixed some warnings for newer compilers; updates #24'

--- prototype2/adc_readout/test/TestUDPServer.cpp
+++ prototype2/adc_readout/test/TestUDPServer.cpp
@@ -12,13 +12,24 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wreorder"
-TestUDPServer::TestUDPServer(std::uint16_t SrcPort, std::uint16_t DstPort, int PacketSize) : Service(), Socket(Service, asio::ip::udp::endpoint(asio::ip::udp::v4(), SourcePort)), Resolver(Service), PacketTimer(Service), SourcePort(SrcPort), DestinationPort(DstPort) {
+TestUDPServer::TestUDPServer(std::uint16_t SrcPort, std::uint16_t DstPort, int PacketSize)
+    : Service()
+    , SourcePort(SrcPort)
+    , DestinationPort(DstPort)
+    , Socket(Service, asio::ip::udp::endpoint(asio::ip::udp::v4(), SourcePort))
+    , Resolver(Service)
+    , PacketTimer(Service) {
   BufferSize = PacketSize;
   SendBuffer = std::unique_ptr<std::uint8_t[]>(new std::uint8_t[PacketSize]);
 }
 
 TestUDPServer::TestUDPServer(std::uint16_t SrcPort, std::uint16_t DstPort, std::uint8_t *DataPtr, size_t DataLength)
-: Service(), Socket(Service, asio::ip::udp::endpoint(asio::ip::udp::v4(), SrcPort)), Resolver(Service), PacketTimer(Service), SourcePort(SrcPort), DestinationPort(DstPort) {
+    : Service()
+    , SourcePort(SrcPort)
+    , DestinationPort(DstPort)
+    , Socket(Service, asio::ip::udp::endpoint(asio::ip::udp::v4(), SrcPort))
+    , Resolver(Service)
+    , PacketTimer(Service) {
   BufferSize = DataLength;
   SendBuffer = std::unique_ptr<std::uint8_t[]>(new std::uint8_t[DataLength]);
   std::memcpy(SendBuffer.get(), DataPtr, DataLength);
@@ -29,7 +40,8 @@ void TestUDPServer::startPacketTransmission(int TotalPackets, int PacketGapNS) {
   NrOfPackets = TotalPackets;
   WaitTimeNSec = PacketGapNS;
   asio::ip::udp::resolver::query Query(asio::ip::udp::v4(), "localhost", std::to_string(DestinationPort));
-  Resolver.async_resolve(Query, std::bind(&TestUDPServer::handleResolve, this, std::placeholders::_1, std::placeholders::_2));
+  Resolver.async_resolve(Query,
+                         std::bind(&TestUDPServer::handleResolve, this, std::placeholders::_1, std::placeholders::_2));
   AsioThread = std::thread(&TestUDPServer::threadFunction, this);
 }
 
@@ -49,7 +61,8 @@ void TestUDPServer::threadFunction() {
   Service.run();
 }
 
-void TestUDPServer::handleResolve(const asio::error_code __attribute__((unused)) &Err, asio::ip::udp::resolver::iterator EndpointIter) {
+void TestUDPServer::handleResolve(const asio::error_code __attribute__((unused)) &Err,
+                                  asio::ip::udp::resolver::iterator EndpointIter) {
   asio::ip::udp::endpoint Endpoint = *EndpointIter;
   Socket.async_connect(Endpoint, std::bind(&TestUDPServer::handleConnect, this, std::placeholders::_1, ++EndpointIter));
 }
@@ -57,25 +70,25 @@ void TestUDPServer::handleResolve(const asio::error_code __attribute__((unused))
 void TestUDPServer::handleConnect(const asio::error_code &Err,
                                   asio::ip::udp::resolver::iterator EndpointIter) {
   if (!Err) {
-    Socket.async_send(asio::buffer(SendBuffer.get(), BufferSize), std::bind(&TestUDPServer::handleWrite, this, std::placeholders::_1, std::placeholders::_2));
+    Socket.async_send(asio::buffer(SendBuffer.get(), BufferSize),
+                      std::bind(&TestUDPServer::handleWrite, this, std::placeholders::_1, std::placeholders::_2));
     --NrOfPackets;
     PacketTimer.expires_from_now(std::chrono::nanoseconds(WaitTimeNSec));
     PacketTimer.async_wait(std::bind(&TestUDPServer::handleNewPacket, this, std::placeholders::_1));
-  }
-  else if (EndpointIter != asio::ip::udp::resolver::iterator())
-  {
+  } else if (EndpointIter != asio::ip::udp::resolver::iterator()) {
     Socket.close();
     asio::ip::udp::endpoint Endpoint = *EndpointIter;
     Socket.async_connect(Endpoint,
-                          std::bind(&TestUDPServer::handleConnect, this,
-                                      std::placeholders::_1, ++EndpointIter));
+                         std::bind(&TestUDPServer::handleConnect, this,
+                                   std::placeholders::_1, ++EndpointIter));
   }
 }
 
 void TestUDPServer::handleNewPacket(const asio::error_code &Err) {
   if (!Err and NrOfPackets > 0 and not GotError) {
     --NrOfPackets;
-    Socket.async_send(asio::buffer(SendBuffer.get(), BufferSize), std::bind(&TestUDPServer::handleWrite, this, std::placeholders::_1, std::placeholders::_2));
+    Socket.async_send(asio::buffer(SendBuffer.get(), BufferSize),
+                      std::bind(&TestUDPServer::handleWrite, this, std::placeholders::_1, std::placeholders::_2));
     PacketTimer.expires_from_now(std::chrono::nanoseconds(WaitTimeNSec));
     PacketTimer.async_wait(std::bind(&TestUDPServer::handleNewPacket, this, std::placeholders::_1));
   }
