11fd9ce1e8bc1af9220025f1dd1aca747df3cd4a
Morten Jagd Christensen <morten.christensen@esss.se>
2018-05-09 11:49:19 +0200

    corrected unit test for grey2bin32, added lua script for wireshark, benchmark test for grey2bin32

--- libs/include/BitMath.h
+++ libs/include/BitMath.h
@@ -12,7 +12,7 @@
 class BitMath {
 public:
   ///
-  static uint32_t reversebits32(register uint32_t x) {
+  inline static uint32_t reversebits32(register uint32_t x) {
     x = (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
     x = (((x & 0xcccccccc) >> 2) | ((x & 0x33333333) << 2));
     x = (((x & 0xf0f0f0f0) >> 4) | ((x & 0x0f0f0f0f) << 4));
@@ -21,12 +21,12 @@ public:
   }
 
   /// @fixme test this
-  static uint16_t reversebits16(register uint16_t x) {
+  inline static uint16_t reversebits16(register uint16_t x) {
     uint32_t temp = reversebits32(x);
     return (temp >> 16);
   };
 
-  static uint32_t gray2bin32(uint32_t num) {
+  inline static uint32_t gray2bin32(uint32_t num) {
     num = num ^ (num >> 16);
     num = num ^ (num >> 8);
     num = num ^ (num >> 4);
--- libs/test/BitMathTest.cpp
+++ libs/test/BitMathTest.cpp
@@ -41,10 +41,10 @@ std::vector<struct pair32> results32 = {
 
 // Is this a correct table?
 std::vector<struct pair16> graycode = {
-  { 0,  0}, { 1,  1}, { 2,  3}, { 3,  2},
-  { 4,  6}, { 5,  7}, { 6,  5}, { 7,  4},
-  { 8, 12}, { 9, 13}, {10, 15}, {11, 14},
-  {12, 10}, {13, 11}, {14,  9}, {15,  8}
+  { 0,  0}, { 1,  1}, { 3,  2}, { 2,  3},
+  { 6,  4}, { 7,  5}, { 5,  6}, { 4,  7},
+  {12,  8}, {13,  9}, {15, 10}, {14, 11},
+  {10, 12}, {11, 13}, { 9, 14}, { 8, 15}
 };
 
 class BitMathTest : public ::testing::Test {
@@ -63,14 +63,14 @@ TEST_F(BitMathTest, BitSawp16) {
   }
 }
 
-
-// TEST_F(BitMathTest, GrayCode) {
-//   for (auto data : graycode) {
-//     auto result = BitMath::gray2bin32(data.input);
-//     printf("i: %d, gray2bin32(i): %d\n", data.input, result);
-//     ASSERT_EQ(result, data.output);
-//   }
-// }
+TEST_F(BitMathTest, GrayCode) {
+  for (auto data : graycode) {
+    auto gray_code = data.input;
+    auto binary = BitMath::gray2bin32(gray_code);
+    printf("i: %d, gray2bin32(i): %d\n", gray_code, binary);
+    ASSERT_EQ(binary, data.output);
+  }
+}
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
