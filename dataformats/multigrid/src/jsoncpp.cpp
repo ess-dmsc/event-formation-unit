bb6041595fb416a55d04abb7544bb1d6fc20a728
Morten Jagd Christensen <morten.christensen@esss.se>
2018-06-01 12:35:11 +0200

    GCOVR_EXCL_START/STOP on json.cpp which is third party sw

--- dataformats/multigrid/src/jsoncpp.cpp
+++ dataformats/multigrid/src/jsoncpp.cpp
@@ -71,7 +71,7 @@ license you like.
 
 
 
-
+/// GCOVR_EXCL_START
 
 
 #include <dataformats/multigrid/inc/json.h>
@@ -5305,3 +5305,4 @@ JSONCPP_OSTREAM& operator<<(JSONCPP_OSTREAM& sout, Value const& root) {
 // //////////////////////////////////////////////////////////////////////
 // End of content of file: src/lib_json/json_writer.cpp
 // //////////////////////////////////////////////////////////////////////
+/// GCOVR_EXCL_STOP
