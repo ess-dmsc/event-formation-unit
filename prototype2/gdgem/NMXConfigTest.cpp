9087e0d8d0539a0fce7815e8fc778d38ee7acfa0
Morten Jagd Christensen <morten.christensen@esss.se>
2018-06-01 13:17:52 +0200

    added NMXConfigTest

---
+++ prototype2/gdgem/NMXConfigTest.cpp
@@ -0,0 +1,32 @@
+/** Copyright (C) 2016, 2017 European Spallation Source ERIC */
+
+#include <algorithm>
+#include <cstring>
+#include <gdgem/NMXConfig.h>
+#include <memory>
+#include <test/TestBase.h>
+
+class NMXConfigTest : public TestBase {
+protected:
+  virtual void SetUp() {
+  }
+
+  virtual void TearDown() { }
+};
+
+/** Test cases below */
+TEST_F(NMXConfigTest, ConstructorDefaults) {
+  NMXConfig nmxconfig;
+  ASSERT_EQ("SRS", nmxconfig.builder_type);
+  ASSERT_EQ(256, nmxconfig.geometry_x);
+  ASSERT_EQ(256, nmxconfig.geometry_y);
+  ASSERT_FALSE(nmxconfig.dump_csv);
+  ASSERT_FALSE(nmxconfig.dump_h5);
+}
+
+
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
