7d436be75f3d6ad1ed5e64b4dba6c5bfe37d88f7
Morten Jagd Christensen <morten.christensen@esss.se>
2018-06-06 15:59:28 +0200

    cleaning up benchmark files

--- prototype2/common/ESSGeometryBenchmarkTest.cpp
+++ prototype2/common/ESSGeometryBenchmarkTest.cpp
@@ -2,7 +2,6 @@
 
 #include <benchmark/benchmark.h>
 #include <logical_geometry/ESSGeometry.h>
-#include <gdgem/nmx/Geometry.h>
 #include <string>
 #include <unistd.h>
 
@@ -19,33 +18,7 @@ static void GetPixelID(benchmark::State &state) {
     }
   }
   state.SetItemsProcessed(items);
-};
+}
 BENCHMARK(GetPixelID);
-
-
-static void GetPixelIDNMX(benchmark::State &state) {
-  uint32_t items = 0;
-  std::vector<uint16_t> coords{0, 0, 0, 0};
-
-  Geometry geom;
-  geom.add_dimension(100);
-  geom.add_dimension(100);
-  geom.add_dimension(100);
-  geom.add_dimension(100);
-
-  for (auto _ : state) {
-    for (size_t i = 10; i < 20; i++) {
-      coords[0] = i;
-      coords[1] = i;
-      coords[2] = i;
-      coords[2] = i;
-      benchmark::DoNotOptimize(geom.to_pixid(coords));
-      items += 1;
-    }
-  }
-  state.SetItemsProcessed(items);
-};
-BENCHMARK(GetPixelIDNMX);
-
 
 BENCHMARK_MAIN();
