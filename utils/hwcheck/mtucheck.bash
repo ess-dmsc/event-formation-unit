8ea55d0f6814a40014f9a63c1bc5d242a1dbf414
Morten Jagd Christensen <mortenchristensen@esss.se>
2018-07-05 12:36:16 +0200

    added mtucheck and pingcheck scripts

--- utils/hwcheck/mtucheck.bash
+++ utils/hwcheck/mtucheck.bash
@@ -1,33 +1,25 @@
 #!/bin/bash
 
-icmpechosize=${1:-8972}
+mtusize=${1:-9000}
 
-# could add -i 0.5 - if it worked
-pingoptions="-M do -c 2 "
+echo mtu size $mtusize
 
 function errexit() {
     echo "Error: "$1
     exit 1
 }
 
-servers=" 172.24.0.201 172.24.0.202 172.24.0.203 172.24.0.204 172.24.0.205 172.24.0.206"
 
-echo "Checking servers for icmp echo with payload size of $icmpechosize"
-failures=0
-for server in $servers
-do
-   echo -n "pinging server $server ... "
-   ping $pingoptions -s $icmpechosize $server &>/dev/null 
-   if [[ "$?" -eq "0" ]]; then
-       echo PASSED
-   else
-       failures=$((failures + 1)) 
-       echo FAILED 
-   fi
-done
+echo "checking MTU size on ethernet interfaces"
 
-echo failures: $failures
+numenos=$(ifconfig | grep eno | wc -l)
+echo number of enoX interfaces $numenos
+mtumatch=$(ifconfig | grep eno | grep "mtu $mtusize" | wc -l)
+echo number of enoX with correct MTU: $mtumatch
 
-if [[ "$failures" -ne "0" ]]; then
-    errexit "unable to ping $failures server[s]  with -c $icmpechosize"
+if [[ $numenos -ne $mtumatch ]]; then
+   errexit "correct MTU on $mtumatch out of $numenos enoX interfaces"
 fi
+
+exit 0
+
