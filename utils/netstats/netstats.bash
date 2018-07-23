a04e3686cd4f83f4f9fed2343de2e530860b9f23
Morten Jagd Christensen <Morten.Christensen@esss.se>
2018-07-23 13:07:29 +0200

    device driver specific counter support

--- utils/netstats/netstats.bash
+++ utils/netstats/netstats.bash
@@ -7,19 +7,43 @@ port=$2
 dev=${3:-enp0s31f6}
 
 function errexit() {
-    echo error: device $1 doesn\'t exist
+    echo error: $1
     exit 1
 }
 
+# Check that device exists
+ethtool -S $dev &>/dev/null || errexit "device: $dev does'nt exist"
 
-ethtool -S $dev || errexit $dev
+
+# Get the driver specific pattern match for counter
+driver=$(ethtool -i $dev | grep driver | awk '{print $2}')
+
+
+case $driver in
+e1000e)
+  # Ubuntu + driver e1000e
+  echo e100e found - OK
+  PATT_NICERR=rx_no_buffer_count
+  PATT_NICRX=rx_packets
+  ;;
+tg3)
+  # Ubuntu + tg3 
+  echo tg3 found - STATS NOT VERIFIED
+  PATT_NICERR=rx_discard
+  PATT_NICRX=rx_ucast_packets
+  ;;
+*)
+  errexit "unknown driver for device $dev - no stats available"
+  ;;
+esac
+PATT_UDPRX=InDatagrams
 
 while [[ 1 ]]
 do
   DATE=$(date +%s)
-  SNMP=$(cat /proc/net/snmp | grep Udp: | grep -v InDatagrams)
-  ETHT=$(ethtool -S $dev | grep rx_no_buffer_count)
-  ETHPKT=$(ethtool -S $dev | grep rx_packets)
+  SNMP=$(cat /proc/net/snmp | grep Udp: | grep -v ${PATT_UDPRX})
+  ETHT=$(ethtool -S $dev | grep ${PATT_NICERR})
+  ETHPKT=$(ethtool -S $dev | grep ${PATT_NICRX})
 
   INDG=$(echo $SNMP | awk '{print $2}')
   INERR=$(echo $SNMP | awk '{print $4}')
@@ -31,9 +55,9 @@ do
   echo "efu.net.nic_rxerr $NICERR $DATE"
   echo "efu.net.nic_rx $NICPKT $DATE"
 
-  echo "efu.net.udp_rx $INDG $DATE" | nc $ip $port
-  echo "efu.net.udp_rxperr $INERR $DATE" | nc $ip $port
-  echo "efu.net.nic_rxerr $NICERR $DATE" | nc $ip $port
-  echo "efu.net.nic_rx $NICPKT $DATE" | nc $ip $port
+  echo "efu.net.udp_rx $INDG $DATE" | nc -q 0 $ip $port
+  echo "efu.net.udp_rxperr $INERR $DATE" | nc -q 0 $ip $port
+  echo "efu.net.nic_rxerr $NICERR $DATE" | nc -q 0 $ip $port
+  echo "efu.net.nic_rx $NICPKT $DATE" | nc -q 0 $ip $port
   sleep 2
 done
