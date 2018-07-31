8b2467d4652b3c611ab71ff1db8a3bd7bb671cc0
Jonas Nilsson <jonas.nilsson@esss.se>
2018-07-31 17:46:44 +0200

    Add new log files.

---
+++ prototype2/common/Log.h
@@ -0,0 +1,19 @@
+
+//#ifdef GRAYLOG
+//#pragma GCC diagnostic push
+//#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+//#include <graylog_logger/GraylogInterface.hpp>
+//#include <graylog_logger/Log.hpp>
+//#pragma GCC diagnostic pop
+//#define GLOG_DEB(x) Log::Msg(Severity::Debug, x)
+//#define GLOG_INF(x) Log::Msg(Severity::Informational, x)
+//#define GLOG_WAR(x) Log::Msg(Severity::Warning, x)
+//#define GLOG_ERR(x) Log::Msg(Severity::Error, x)
+//#define GLOG_CRI(x) Log::Msg(Severity::Critical, x)
+//#else
+//#define GLOG_DEB(x)
+//#define GLOG_INF(x)
+//#define GLOG_WAR(x)
+//#define GLOG_ERR(x)
+//#define GLOG_CRI(x)
+//#endif
