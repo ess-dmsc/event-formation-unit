f531aaf1cb772cdb758b1f5682a5171f7a9f3fb0
Jonas Nilsson <jonas.nilsson@esss.se>
2018-08-01 10:11:56 +0200

    Build issue fix.

--- Utilities.cmake
+++ Utilities.cmake
@@ -84,7 +84,7 @@ function(create_test_executable)
   target_link_libraries(${exec_name}
     ${${exec_name}_LIB}
     ${EFU_COMMON_LIBS}
-    ${GTEST_LIBRARIES})
+    ${GTEST_LIBRARIES} efu_common)
 
   if(${CMAKE_COMPILER_IS_GNUCXX})
     add_linker_flags(${exec_name} "-Wl,--no-as-needed")
@@ -120,5 +120,5 @@ function(create_integration_test_executable exec_name)
   target_link_libraries(${exec_name}
     ${${exec_name}_LIB}
     ${EFU_COMMON_LIBS}
-    eventlib)
+    eventlib efu_common)
 endfunction(create_integration_test_executable)
