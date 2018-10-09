80a5b8ea9dbb471699609e6bc08cbc6c7e9ca547
Morten Jagd Christensen <morten.christensen@esss.se>
2018-10-09 17:21:12 +0200

    added testdata with size less than MacOS default MTU

--- prototype2/multiblade/MBCaenBaseTest.cpp
+++ prototype2/multiblade/MBCaenBaseTest.cpp
@@ -5,7 +5,8 @@
  *  \brief Unit tests.
  */
 
- #include <string>
+#include <string>
+#include <multiblade/mbcaen/DataParserTestData.h>
 
  std::string mb16json = R"(
  {
@@ -57,16 +58,18 @@ TEST_F(MBCAENBaseTest, Constructor) {
 }
 
 
-
 TEST_F(MBCAENBaseTest, DataReceive) {
   MBCAENBaseStandIn Readout(Settings, LocalSettings);
   Readout.startThreads();
-  TestUDPServer Server(43126, Settings.DetectorPort, 1470);
+  //TestUDPServer Server(43126, Settings.DetectorPort, 1470);
+  TestUDPServer Server(43126, Settings.DetectorPort, (unsigned char *)&pkt13_short[0], pkt13_short.size());
   Server.startPacketTransmission(1, 100);
   std::chrono::duration<std::int64_t, std::milli> SleepTime(200);
   std::this_thread::sleep_for(SleepTime);
   Readout.stopThreads();
   EXPECT_EQ(Readout.mystats.rx_packets, 1);
+  EXPECT_EQ(Readout.mystats.rx_bytes, pkt13_short.size());
+  EXPECT_EQ(Readout.mystats.rx_readouts, 66); // number of readouts in pkt13_short
 }
 
 int main(int argc, char **argv) {
