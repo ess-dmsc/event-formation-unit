3b4f9198d84a09222e04885fb747c2bdfb9fe061
Morten Jagd Christensen <morten.christensen@esss.se>
2018-10-19 16:54:46 +0200

    one more (u-test) for the road

--- prototype2/common/HistsTest.cpp
+++ prototype2/common/HistsTest.cpp
@@ -27,6 +27,33 @@ TEST_F(HistsTest, SetBinWidth) {
   // }
 }
 
+TEST_F(HistsTest, BinCluster) {
+  Hists hists(64, 4096);
+  hists.bincluster(0);
+  ASSERT_EQ(hists.cluster_count(), 0);
+
+  for (unsigned int i = 1; i < 4096; i++) {
+    hists.bincluster(i);
+    ASSERT_EQ(hists.cluster_count(), i);
+    ASSERT_EQ(hists.cluster_adc_hist[i], 1);
+  }
+}
+
+TEST_F(HistsTest, BinClusterWDownshift) {
+  Hists hists(64, 4096);
+  hists.set_cluster_adc_downshift(1);
+  for (unsigned int i = 1; i < 4096; i++) {
+    hists.bincluster(i);
+    ASSERT_EQ(hists.cluster_count(), i);
+  }
+  for (unsigned int i = 1; i < 2048; i++) {
+    ASSERT_EQ(hists.cluster_adc_hist[i], 2);
+  }
+  for (unsigned int i = 2048; i < 4096; i++) {
+    ASSERT_EQ(hists.cluster_adc_hist[i], 0);
+  }
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
