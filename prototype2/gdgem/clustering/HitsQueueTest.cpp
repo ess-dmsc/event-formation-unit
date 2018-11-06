f8779bfa1f8ac9c8d3ff173cdafd052f6c3b2ea1
Martin Shetty <martukas@gmail.com>
2018-11-06 04:03:35 +0100

    "more HitsQueue tests with real data; updates #155"

--- prototype2/gdgem/clustering/HitsQueueTest.cpp
+++ prototype2/gdgem/clustering/HitsQueueTest.cpp
@@ -40,6 +40,7 @@ TEST_F(HitsQueueTest, PrintConfig) {
 
 TEST_F(HitsQueueTest, a1_notrigger) {
   ReadoutFile::read(DataPath + "a00001", readouts);
+  EXPECT_EQ(readouts.size(), 144);
 
   for (const auto &r : readouts) {
     auto plane = opts.srs_mappings.get_plane(r);
@@ -59,6 +60,7 @@ TEST_F(HitsQueueTest, a1_notrigger) {
 
 TEST_F(HitsQueueTest, a1_trigger) {
   ReadoutFile::read(DataPath + "a00001", readouts);
+  EXPECT_EQ(readouts.size(), 144);
 
   for (const auto &r : readouts) {
     auto plane = opts.srs_mappings.get_plane(r);
@@ -79,93 +81,258 @@ TEST_F(HitsQueueTest, a1_trigger) {
   EXPECT_EQ(p0->hits().size(), 0);
 }
 
+TEST_F(HitsQueueTest, a1_chronological_no_trigger) {
+  ReadoutFile::read(DataPath + "a00001", readouts);
+  EXPECT_EQ(readouts.size(), 144);
 
-/// \todo some checks disabled, this is not stricly chronological!!!
+  for (const auto &r : readouts) {
+    auto plane = opts.srs_mappings.get_plane(r);
+    EXPECT_EQ(plane, 0);
+    p0->store(plane, opts.srs_mappings.get_strip(r),
+              r.adc, r.chiptime, r.srs_timestamp);
 
-//TEST_F(HitsQueueTest, Run16_chronological_no_trigger) {
-//  for (auto hit : Run16) {
-//    auto chiptime = srstime.chip_time_ns(hit.bcid, hit.tdc, no_offset, unit_slope);
-//    queue->store(0,0,0,chiptime);
-//  }
-//
-//  double prevtime{0};
-//
-//  queue->sort_and_correct();
-//  while(queue->hits().size()) {
-////    EXPECT_GE(queue->hits().front().time, prevtime);
-//    prevtime = queue->hits().front().time;
-//    for (const auto &e : queue->hits()) {
-//      EXPECT_GE(e.time, prevtime);
-//      prevtime = e.time;
-//    }
-//    queue->sort_and_correct();
-//  };
-//}
+  }
 
-//TEST_F(HitsQueueTest, Run16_chronological_with_trigger) {
-//  for (auto hit : Run16) {
-//    auto chiptime = srstime.chip_time_ns(hit.bcid, hit.tdc);
-//    queue->store(0,0,0,chiptime);
-//  }
-//
-//  double prevtime{0};
-//
-//  queue->subsequent_trigger(true);
-//  queue->sort_and_correct();
-//  while(queue->hits().size()) {
-////    EXPECT_GE(queue->hits().front().time, prevtime);
-//    prevtime = queue->hits().front().time;
-//    for (const auto &e : queue->hits()) {
-////      EXPECT_GE(e.time, prevtime);
-//      prevtime = e.time;
-//    }
-//    queue->subsequent_trigger(true);
-//    queue->sort_and_correct();
-//  };
-//}
+  double prevtime{0};
+  p0->sort_and_correct();
+  p0->sort_and_correct();
+  while (p0->hits().size()) {
+    EXPECT_GE(p0->hits().front().time, prevtime);
+    prevtime = p0->hits().front().time;
+    for (const auto &e : p0->hits()) {
+      EXPECT_GE(e.time, prevtime);
+      prevtime = e.time;
+    }
+    p0->sort_and_correct();
+  }
+}
 
-//TEST_F(HitsQueueTest, Long_chronological_no_trigger) {
-//  for (auto hit : long_data) {
-//    auto chiptime = srstime.chip_time_ns(hit.bcid, hit.tdc, no_offset, unit_slope);
-//    queue->store(0,0,0,chiptime);
-//  }
-//
-//  double prevtime{0};
-//
-//  queue->sort_and_correct();
-//  while(queue->hits().size()) {
-////    EXPECT_GE(queue->hits().front().time, prevtime);
-//    prevtime = queue->hits().front().time;
-//    for (const auto &e : queue->hits()) {
-//      EXPECT_GE(e.time, prevtime);
-//      prevtime = e.time;
-//    }
-//    queue->sort_and_correct();
-//  };
-//}
-//
-//TEST_F(HitsQueueTest, Long_chronological_with_trigger) {
-//  for (auto hit : long_data) {
-//    auto chiptime = srstime.chip_time_ns(hit.bcid, hit.tdc, no_offset, unit_slope);
-//    queue->store(0,0,0,chiptime);
-//  }
-//
-//  double prevtime{0};
-//
-//  queue->subsequent_trigger(true);
-//  queue->sort_and_correct();
-//  while(queue->hits().size()) {
-//    EXPECT_GE(queue->hits().front().time, prevtime);
-//    prevtime = queue->hits().front().time;
-//    for (const auto &e : queue->hits()) {
-////      EXPECT_GE(e.time, prevtime);
-//      prevtime = e.time;
-//    }
-//    queue->subsequent_trigger(true);
-//    queue->sort_and_correct();
-//  };
-//}
+TEST_F(HitsQueueTest, a10_notrigger) {
+  ReadoutFile::read(DataPath + "a00010", readouts);
+  EXPECT_EQ(readouts.size(), 920);
 
+  for (const auto &r : readouts) {
+    auto plane = opts.srs_mappings.get_plane(r);
+    EXPECT_LT(plane, 2) << "fec:" << int(r.fec) << " chip:" << int(r.chip_id) << "\n";
+    if (plane == 0)
+      p0->store(plane, opts.srs_mappings.get_strip(r),
+                r.adc, r.chiptime, r.srs_timestamp);
+
+    if (plane == 1)
+      p1->store(plane, opts.srs_mappings.get_strip(r),
+                r.adc, r.chiptime, r.srs_timestamp);
+  }
+  EXPECT_EQ(p0->hits().size(), 0);
+  EXPECT_EQ(p1->hits().size(), 0);
+  p0->sort_and_correct();
+  p1->sort_and_correct();
+  EXPECT_EQ(p0->hits().size(), 0);
+  EXPECT_EQ(p1->hits().size(), 0);
+  p0->sort_and_correct();
+  p1->sort_and_correct();
+  EXPECT_EQ(p0->hits().size(), 558);
+  EXPECT_EQ(p1->hits().size(), 362);
+  p0->sort_and_correct();
+  p1->sort_and_correct();
+  EXPECT_EQ(p0->hits().size(), 0);
+  EXPECT_EQ(p1->hits().size(), 0);
+}
+
+
+TEST_F(HitsQueueTest, a10_trigger) {
+  ReadoutFile::read(DataPath + "a00010", readouts);
+  EXPECT_EQ(readouts.size(), 920);
+
+  for (const auto &r : readouts) {
+    auto plane = opts.srs_mappings.get_plane(r);
+    EXPECT_LT(plane, 2) << "fec:" << int(r.fec) << " chip:" << int(r.chip_id) << "\n";
+    if (plane == 0)
+      p0->store(plane, opts.srs_mappings.get_strip(r),
+                r.adc, r.chiptime, r.srs_timestamp);
+
+    if (plane == 1)
+      p1->store(plane, opts.srs_mappings.get_strip(r),
+                r.adc, r.chiptime, r.srs_timestamp);
+  }
+  EXPECT_EQ(p0->hits().size(), 0);
+  EXPECT_EQ(p1->hits().size(), 0);
+  p0->subsequent_trigger(true);
+  p1->subsequent_trigger(true);
+  p0->sort_and_correct();
+  p1->sort_and_correct();
+  EXPECT_EQ(p0->hits().size(), 0);
+  EXPECT_EQ(p1->hits().size(), 0);
+  p0->subsequent_trigger(true);
+  p1->subsequent_trigger(true);
+  p0->sort_and_correct();
+  p1->sort_and_correct();
+  EXPECT_EQ(p0->hits().size(), 558);
+  EXPECT_EQ(p1->hits().size(), 362);
+  p0->subsequent_trigger(true);
+  p1->subsequent_trigger(true);
+  p0->sort_and_correct();
+  p1->sort_and_correct();
+  EXPECT_EQ(p0->hits().size(), 0);
+  EXPECT_EQ(p1->hits().size(), 0);
+}
+
+TEST_F(HitsQueueTest, a10_chronological_no_trigger) {
+  ReadoutFile::read(DataPath + "a00010", readouts);
+  EXPECT_EQ(readouts.size(), 920);
+
+  for (const auto &r : readouts) {
+    auto plane = opts.srs_mappings.get_plane(r);
+    EXPECT_LT(plane, 2) << "fec:" << int(r.fec) << " chip:" << int(r.chip_id) << "\n";
+    if (plane == 0)
+      p0->store(plane, opts.srs_mappings.get_strip(r),
+                r.adc, r.chiptime, r.srs_timestamp);
+
+    if (plane == 1)
+      p1->store(plane, opts.srs_mappings.get_strip(r),
+                r.adc, r.chiptime, r.srs_timestamp);
+  }
+
+  double prevtime{0};
+  size_t count {0};
+  while(count < 558) {
+    p0->sort_and_correct();
+    if (p0->hits().size()) {
+      EXPECT_GE(p0->hits().front().time, prevtime);
+    }
+    for (const auto &e : p0->hits()) {
+      EXPECT_GE(e.time, prevtime);
+      prevtime = e.time;
+      count++;
+    }
+  }
+
+  prevtime = 0;
+  count = 0;
+  while(count < 362) {
+    p1->sort_and_correct();
+    if (p1->hits().size()) {
+      EXPECT_GE(p1->hits().front().time, prevtime);
+    }
+    for (const auto &e : p1->hits()) {
+      EXPECT_GE(e.time, prevtime);
+      prevtime = e.time;
+      count++;
+    }
+  }
+}
+
+TEST_F(HitsQueueTest, a100_notrigger) {
+  ReadoutFile::read(DataPath + "a00100", readouts);
+  EXPECT_EQ(readouts.size(), 126590);
+
+  for (const auto &r : readouts) {
+    auto plane = opts.srs_mappings.get_plane(r);
+    EXPECT_LT(plane, 2) << "fec:" << int(r.fec) << " chip:" << int(r.chip_id) << "\n";
+    if (plane == 0)
+      p0->store(plane, opts.srs_mappings.get_strip(r),
+                r.adc, r.chiptime, r.srs_timestamp);
+
+    if (plane == 1)
+      p1->store(plane, opts.srs_mappings.get_strip(r),
+                r.adc, r.chiptime, r.srs_timestamp);
+  }
+  EXPECT_EQ(p0->hits().size(), 0);
+  EXPECT_EQ(p1->hits().size(), 0);
+  p0->sort_and_correct();
+  p1->sort_and_correct();
+  EXPECT_EQ(p0->hits().size(), 0);
+  EXPECT_EQ(p1->hits().size(), 0);
+  p0->sort_and_correct();
+  p1->sort_and_correct();
+  EXPECT_EQ(p0->hits().size(), 84162);
+  EXPECT_EQ(p1->hits().size(), 42428);
+  p0->sort_and_correct();
+  p1->sort_and_correct();
+  EXPECT_EQ(p0->hits().size(), 0);
+  EXPECT_EQ(p1->hits().size(), 0);
+}
+
+TEST_F(HitsQueueTest, a100_trigger) {
+  ReadoutFile::read(DataPath + "a00100", readouts);
+  EXPECT_EQ(readouts.size(), 126590);
+
+  for (const auto &r : readouts) {
+    auto plane = opts.srs_mappings.get_plane(r);
+    EXPECT_LT(plane, 2) << "fec:" << int(r.fec) << " chip:" << int(r.chip_id) << "\n";
+    if (plane == 0)
+      p0->store(plane, opts.srs_mappings.get_strip(r),
+                r.adc, r.chiptime, r.srs_timestamp);
+
+    if (plane == 1)
+      p1->store(plane, opts.srs_mappings.get_strip(r),
+                r.adc, r.chiptime, r.srs_timestamp);
+  }
+  EXPECT_EQ(p0->hits().size(), 0);
+  EXPECT_EQ(p1->hits().size(), 0);
+  p0->subsequent_trigger(true);
+  p1->subsequent_trigger(true);
+  p0->sort_and_correct();
+  p1->sort_and_correct();
+  EXPECT_EQ(p0->hits().size(), 0);
+  EXPECT_EQ(p1->hits().size(), 0);
+  p0->subsequent_trigger(true);
+  p1->subsequent_trigger(true);
+  p0->sort_and_correct();
+  p1->sort_and_correct();
+  EXPECT_EQ(p0->hits().size(), 84162);
+  EXPECT_EQ(p1->hits().size(), 42428);
+  p0->subsequent_trigger(true);
+  p1->subsequent_trigger(true);
+  p0->sort_and_correct();
+  p1->sort_and_correct();
+  EXPECT_EQ(p0->hits().size(), 0);
+  EXPECT_EQ(p1->hits().size(), 0);
+}
+
+TEST_F(HitsQueueTest, a100_chronological_no_trigger) {
+  ReadoutFile::read(DataPath + "a00100", readouts);
+  EXPECT_EQ(readouts.size(), 126590);
+
+  for (const auto &r : readouts) {
+    auto plane = opts.srs_mappings.get_plane(r);
+    EXPECT_LT(plane, 2) << "fec:" << int(r.fec) << " chip:" << int(r.chip_id) << "\n";
+    if (plane == 0)
+      p0->store(plane, opts.srs_mappings.get_strip(r),
+                r.adc, r.chiptime, r.srs_timestamp);
+
+    if (plane == 1)
+      p1->store(plane, opts.srs_mappings.get_strip(r),
+                r.adc, r.chiptime, r.srs_timestamp);
+  }
+
+  double prevtime{0};
+  size_t count {0};
+  while(count < 84162) {
+    p0->sort_and_correct();
+    if (p0->hits().size()) {
+      EXPECT_GE(p0->hits().front().time, prevtime);
+    }
+    for (const auto &e : p0->hits()) {
+      EXPECT_GE(e.time, prevtime);
+      prevtime = e.time;
+      count++;
+    }
+  }
+
+  prevtime = 0;
+  count = 0;
+  while(count < 42428) {
+    p1->sort_and_correct();
+    if (p1->hits().size()) {
+      EXPECT_GE(p1->hits().front().time, prevtime);
+    }
+    for (const auto &e : p1->hits()) {
+      EXPECT_GE(e.time, prevtime);
+      prevtime = e.time;
+      count++;
+    }
+  }
+}
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
