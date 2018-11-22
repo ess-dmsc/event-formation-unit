1b6b428f4d5ed6277f66babd2829a93282e6357b
Morten Jagd Christensen <morten.christensen@esss.se>
2018-11-22 16:39:53 +0100

    initial dumb unit test for constructor

--- prototype2/common/clustering/GapMatcherTest.cpp
+++ prototype2/common/clustering/GapMatcherTest.cpp
@@ -8,49 +8,16 @@
 #include <test/TestBase.h>
 #include <functional>
 
-std::vector<Hit> tg126_p0 = {
-  {0,   15, 1000, 0},
-  {126, 15, 1000, 0}
-};
-
-std::vector<Hit> tg126_p1 = {
-  {0,   15, 1000, 1},
-  {126, 15, 1000, 1}
-};
-
 class GapMatcherTest : public TestBase {
 protected:
-    Cluster c0, c1;
-    ClusterContainer cc;
 };
 
-TEST_F(GapMatcherTest, NoTimeOverlap) {
-  GapMatcher matcher(125, 0);
-
-  for (auto & hit  : tg126_p0) {
-    c0.insert(hit);
-  }
-  cc.push_back(c0);
-  matcher.insert(0, cc);
-
+TEST_F(GapMatcherTest, Constructor) {
+  GapMatcher matcher(125, 70);
   ASSERT_EQ(matcher.stats_event_count, 0);
   ASSERT_EQ(matcher.matched_events.size(), 0);
 
   matcher.match(true); // ever called with false?
-  ASSERT_EQ(matcher.stats_event_count, 0);
-  ASSERT_EQ(matcher.matched_events.size(), 0);
-
-  cc.clear();
-  for (auto & hit : tg126_p1) {
-    c1.insert(hit);
-  }
-  cc.push_back(c1);
-  matcher.insert(1, cc);
-
-  matcher.match(true); // ever called with false?
-  ASSERT_EQ(matcher.stats_event_count, 0);
-  ASSERT_EQ(matcher.matched_events.size(), 0);
-
 }
 
 
