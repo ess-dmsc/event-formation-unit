551d2aa16ae33067b93640ccb3951bc3bda41175
Matthew D Jones <matthew.d.jones@tessella.com>
2019-01-28 16:36:43 +0000

    Refactor verifymetrics so it can be used as a module

--- utils/efushell/verifymetrics.py
+++ utils/efushell/verifymetrics.py
@@ -3,20 +3,11 @@
 from EFUMetrics import Metrics
 import argparse, sys
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", metavar='ipaddr', help = "server ip address (default 127.0.0.1)",
-                        type = str, default = "127.0.0.1")
-    parser.add_argument("-p", metavar='port', help = "server tcp port (default 8888)",
-                        type = int, default = 8888)
-    parser.add_argument('metrics', metavar='metrics', nargs='+',
-                        help='grafana metrics to be verified, examples: efu.rx_packets:1234 - strict match. efu.rx_packets:+1234 at least)')
-    args = parser.parse_args()
-
-    metrics = Metrics(args.i, args.p)
+def verify_metrics(ip_address, port, test_metrics):
+    metrics = Metrics(ip_address, port)
     metrics.get_all_metrics(metrics.get_number_of_stats())
 
-    for validation in args.metrics:
+    for validation in test_metrics:
         m = validation.split(":")
         if len(m) != 2:
             print("Illegal metric specification: %s (hint use name:value)" % (validation))
@@ -31,6 +22,20 @@ if __name__ == '__main__':
         res, op = f(retval, value)
 
         if res == False:
-            print("Validation failed for %s: expected %s %d, got %d" % (name, op, value, retval))
-            sys.exit(1)
+            yield name, op, value, retval
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", metavar='ipaddr', help = "server ip address (default 127.0.0.1)",
+                        type = str, default = "127.0.0.1")
+    parser.add_argument("-p", metavar='port', help = "server tcp port (default 8888)",
+                        type = int, default = 8888)
+    parser.add_argument('metrics', metavar='metrics', nargs='+',
+                        help='grafana metrics to be verified, examples: efu.rx_packets:1234 - strict match. efu.rx_packets:+1234 at least)')
+    args = parser.parse_args()
+
+    for name, op, value, retval in verify_metrics(args.i, args.p, args.metrics):
+        print("Validation failed for %s: expected %s %d, got %d" % (name, op, value, retval))
+        sys.exit(1)
     print("OK")
