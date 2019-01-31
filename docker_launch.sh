838522e81fa998e11cc46fffb4d69e4893dd7ac2
Matthew D Jones <matthew.d.jones@tessella.com>
2019-01-31 08:16:39 +0000

    Add comments to docker launch script

--- docker_launch.sh
+++ docker_launch.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Run specified executable, or the efu by default
+# If a config file or optional arguments are provided then pass
+# them to the executable, otherwise print the help message
+
+# if EXECUTABLE is given as "efu" or omitted, then run the efu
 if [ "${EXECUTABLE}" = "efu" ] || [ -z "${EXECUTABLE}" ]; then
   if [ -z "${CONFIG_FILE}" ] && [ -z "${OPT_ARGS}" ]; then
     /efu/bin/efu --help
@@ -8,8 +13,11 @@ if [ "${EXECUTABLE}" = "efu" ] || [ -z "${EXECUTABLE}" ]; then
   else
     /efu/bin/efu ${OPT_ARGS}
   fi
+  # if specified executable doesn't exist then print an error
 elif [ ! -f /efu/bin/${EXECUTABLE} ]; then
-    echo "Could not find executable with name ${EXECUTABLE}"
+  echo "Could not find executable with name ${EXECUTABLE}"
+  exit 1
+  # if specified executable exists then run it
 else
   if [ -z "${OPT_ARGS}" ]; then
     /efu/bin/${EXECUTABLE} --help
