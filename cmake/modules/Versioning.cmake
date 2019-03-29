7161210be3760c978183395ddfa8714d3d1d5bbb
Afonso Mukai <28659574+amues@users.noreply.github.com>
2019-03-29 18:17:55 +0100

    Simplify Versioning.cmake

--- cmake/modules/Versioning.cmake
+++ cmake/modules/Versioning.cmake
@@ -1,21 +1,33 @@
-# Set variables from version string
+# set_version()
 #
-# Usage:
+# Set version string variable to release version or Git branch and commit.
 #
-#   set_version_variables(version)
+# Variables that change the behaviour of this module:
 #
-# Variables defined by this module:
+#   - RELEASE_VERSION: if defined, it must be a string of form "X.Y.Z[-rcW]" and
+#       is be used as the version string. Otherwise, the Git branch and commit
+#       are used to create the version string "<branch>-<commit>[-dirty]", where
+#       "-dirty" is appended if there are non-committed chages in the Git
+#       repository.
 #
-#   VERSION_STRING    The full version string, e.g. 1.2.3-rc4
-#   VERSION_MAJOR     Major version number, e.g. 1 for 1.2.3-rc4
-#   VERSION_MINOR     Minor version number, e.g. 2 for 1.2.3-rc4
-#   VERSION_PATCH     Patch version number, e.g. 3 for 1.2.3-rc4
-#   VERSION_EXTRA     Extra version information, e.g. rc4 for 1.2.3-rc4
+# Variables set by this module:
 #
-find_package(Git)
+#   - VERSION_STRING: the version string defined according to the rules above.
+#
+# External requirements:
+#
+#   - Git (only if RELEASE_VERSION is not defined)
+#
+# Examples:
+#
+#   If RELEASE_VERSION is set to "1.2.3-rc4", VERSION_STRING will be set to
+#   "1.2.3-rc4". If RELEASE_VERSION is not defined and the current Git branch is
+#   "master" on commit "bc5b2c50e1fc8b1ce96607aa2f0d373900634389", with no
+#   non-committed changes to tracked files, VERSION_STRING will be set to
+#   "master-bc5b2c5".
+#
 
-# Set the GIT_BRANCH variable to the current Git branch. In case of error, set
-# the variable to an empty string.
+# Set the GIT_BRANCH variable to the current Git branch.
 function(set_git_branch_variable)
   execute_process(
     COMMAND           "${GIT_EXECUTABLE}" rev-parse --abbrev-ref HEAD
@@ -31,12 +43,14 @@ function(set_git_branch_variable)
     set(GIT_BRANCH "${git_output}" PARENT_SCOPE)
   else()
     set(GIT_BRANCH "" PARENT_SCOPE)
-    message("Failed to get Git branch information")
+    message(
+      FATAL_ERROR
+      "Failed to get Git branch information with error ${git_error}"
+    )
   endif()
 endfunction()
 
-# Set the GIT_SHORT_REF variable to the current Git ref. In case of error, set
-# the variable to an empty string.
+# Set the GIT_SHORT_REF variable to the current Git ref.
 function(set_git_short_ref_variable)
   execute_process(
     COMMAND           "${GIT_EXECUTABLE}" rev-parse --short HEAD
@@ -52,7 +66,10 @@ function(set_git_short_ref_variable)
     set(GIT_SHORT_REF "${git_output}" PARENT_SCOPE)
   else()
     set(GIT_SHORT_REF "" PARENT_SCOPE)
-    message("Failed to get Git ref information")
+    message(
+      FATAL_ERROR
+      "Failed to get Git ref information with error ${git_error}"
+    )
   endif()
 endfunction()
 
@@ -77,95 +94,29 @@ function(set_git_dirty_variable)
   endif()
 endfunction()
 
-# Set the GIT_VERSION_TAG variable if a tag is present and matches the format
-# "vX.Y.Z[-rcW]". In case it does not or an error occurs, set the variable to an
-# empty string.
-function(set_git_version_tag_variable)
-  execute_process(
-    COMMAND           "${GIT_EXECUTABLE}" tag --list --points-at HEAD
-    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-    RESULT_VARIABLE   git_result
-    OUTPUT_VARIABLE   git_output
-    ERROR_VARIABLE    git_error
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    ERROR_STRIP_TRAILING_WHITESPACE
-  )
-  if(git_result EQUAL 0)
-    if(NOT git_output STREQUAL "")
-      # A Git tag has been found; check if it has the format "vX.Y.Z[-rcW]".
-      if(git_output MATCHES "^v(0|[1-9][0-9]*)[.](0|[1-9][0-9]*)[.](0|[1-9][0-9]*)([-]rc[1-9][0-9]*)?$")
-        set(GIT_VERSION_TAG "${git_output}" PARENT_SCOPE)
-        message("Found Git tag ${git_output}")
-      else()
-        set(GIT_VERSION_TAG "" PARENT_SCOPE)
-        message("Git tag ${git_output} does not match versioning format")
-      endif()
-    else()
-      set(GIT_VERSION_TAG "" PARENT_SCOPE)
-      message("No Git tag found")
-    endif()
-  else()
-    set(GIT_VERSION_TAG "" PARENT_SCOPE)
-    message("Failed to get Git tag information")
-  endif()
-endfunction()
-
-# Check format of version string, set VERSION_STRING and break it into
-# components if it has the format "X.Y.Z[-rcW]"; VERSION_EXTRA is set to an
-# empty string if "-rcW" is not present. If the version string does not match
-# that format, VERSION_MAJOR is set to "${version}" and the remaining component
-# variables are set to empty strings.
+# Set VERSION_STRING if the argument has the form "X.Y.Z[-rcW]".
 macro(match_and_set_version_variables version)
-  set(VERSION_STRING ${version})
-  # Check if version has the form "X.Y.Z[-rcW]".
-  if(version MATCHES "^(0|[1-9][0-9]*)[.](0|[1-9][0-9]*)[.](0|[1-9][0-9]*)([-]rc[1-9][0-9]*)?$")
-    # Break the version string components into separate variables.
-    set(VERSION_MAJOR "${CMAKE_MATCH_1}" PARENT_SCOPE)
-    set(VERSION_MINOR "${CMAKE_MATCH_2}" PARENT_SCOPE)
-    set(VERSION_PATCH "${CMAKE_MATCH_3}" PARENT_SCOPE)
-    # Extra version information is optional, so it might not be available.
-    if(DEFINED CMAKE_MATCH_4)
-      string(SUBSTRING "${CMAKE_MATCH_4}" 1 -1 VERSION_EXTRA_LOCAL)
-      set(VERSION_EXTRA "${VERSION_EXTRA_LOCAL}" PARENT_SCOPE)
-    else()
-      set(VERSION_EXTRA "" PARENT_SCOPE)
-    endif()
+  if("${version}" MATCHES "^(0|[1-9][0-9]*)[.](0|[1-9][0-9]*)[.](0|[1-9][0-9]*)([-]rc[1-9][0-9]*)?$")
+    set(VERSION_STRING ${version} PARENT_SCOPE)
   else()
-    set(VERSION_MAJOR "${version}" PARENT_SCOPE)
-    set(VERSION_MINOR "" PARENT_SCOPE)
-    set(VERSION_PATCH "" PARENT_SCOPE)
-    set(VERSION_EXTRA "" PARENT_SCOPE)
+    message(FATAL_ERROR "Version ${version} does not match format X.Y.Z[-rcW]")
   endif()
 endmacro()
 
-# Main module function (see module documentation).
-function(set_version_variables version)
-  if(GIT_FOUND)
-    set_git_version_tag_variable()
-    if(GIT_VERSION_TAG)
-      # The version tag has the form "vX.Y.Z[-rcW]"; skip the initial 'v'.
-      string(SUBSTRING "${GIT_VERSION_TAG}" 1 -1 GIT_VERSION)
-      # If a version has been found in a tag, the version argument must match it.
-      if(version STREQUAL GIT_VERSION)
-        match_and_set_version_variables("${version}")
-      else()
-        message(FATAL_ERROR "${version} does not match version ${GIT_VERSION} from Git tag")
-      endif()
-    else()
-      set_git_branch_variable()
-      set_git_short_ref_variable()
-      if(GIT_BRANCH STREQUAL "" OR GIT_SHORT_REF STREQUAL "")
-        # Missing Git information, use provided version argument.
-        match_and_set_version_variables("${version}")
-      else()
-        # Building from Git repo with no tag with format "vX.Y.Z[-rcW]".
-        set_git_dirty_variable()
-        match_and_set_version_variables("${GIT_BRANCH}-${GIT_SHORT_REF}${GIT_DIRTY}")
-      endif()
-    endif()
+# Set VERSION_STRING to "<branch>-<commit>[-dirty]".
+macro(set_version_variables_from_git_branch_and_commit)
+  find_package(Git REQUIRED)
+  set_git_branch_variable()
+  set_git_short_ref_variable()
+  set_git_dirty_variable()
+  set(VERSION_STRING "${GIT_BRANCH}-${GIT_SHORT_REF}${GIT_DIRTY}" PARENT_SCOPE)
+endmacro()
+
+# The public function to be called in the main CMakeLists.txt file.
+function(set_version)
+  if(DEFINED RELEASE_VERSION)
+    match_and_set_version_variables("${RELEASE_VERSION}")
   else()
-    # Git package not found by CMake, use provided version argument.
-    message("Git not found for checking tags, using ${version}")
-    match_and_set_version_variables("${version}")
+    set_version_variables_from_git_branch_and_commit()
   endif()
 endfunction()
