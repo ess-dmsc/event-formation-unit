4f5b70de69af7a90e3961980c3ec9fd7308d0ddb
Afonso Mukai <28659574+amues@users.noreply.github.com>
2019-05-08 17:20:11 +0200

    Add versioning

--- cmake/Versioning.cmake
+++ cmake/Versioning.cmake
@@ -1,43 +1,33 @@
 # Versioning.cmake
 #
 # set_version()
-#   Set version string variable to release version or Git branch and commit.
+#   Set version string variable to value X.Y.Z defined in the file VERSION or
+#   Git branch and commit if that file does not exist.
 #
 # create_version_header(create_version_header output_version_file)
 #   Replace version string in template file and create output header file. If
 #   you want to generate a new header file with an updated version string, you
-#   need to run CMake again.
-#
-# Variables that change the behaviour of this module:
-#
-#   - RELEASE_VERSION: it must be a string of form "X.Y.Z[-rcW]", a false
-#     constant (e.g. an empty string) or undefined. If true, it is used as the
-#     version string; otherwise, the Git branch and commit are used to create
-#     the version string "<branch>-<commit>[-dirty]", where "-dirty" is appended
-#     if there are non-committed chages in the Git repository. When the git
-#     branch and commit are used, the version component variables are all set
-#     to "0".
+#   must run CMake again.
 #
 # Variables set by this module:
 #
 #   - VERSION_STRING: the version string defined according to the rules above.
-#   - MAJOR_VERSION: the major component of the version (X)
-#   - MINOR_VERSION: the minor component of the version (Y)
-#   - PATCH_VERSION: the patch component of the version (Z)
+#   - MAJOR_VERSION: the major component of the version (X) or 0 if not defined.
+#   - MINOR_VERSION: the minor component of the version (Y) or 0 if not defined.
+#   - PATCH_VERSION: the patch component of the version (Z) or 0 if not defined.
 #
 # External requirements:
 #
-#   - Git (only if RELEASE_VERSION is not defined)
+#   - Git (if the file VERSION does not exist)
 #
 # Examples:
 #
-#   If RELEASE_VERSION is set to "1.2.3-rc4", VERSION_STRING will be set to
-#   "1.2.3-rc4". If RELEASE_VERSION is not defined and the current Git branch is
-#   "master" on commit "bc5b2c50e1fc8b1ce96607aa2f0d373900634389", with no
-#   non-committed changes to tracked files, VERSION_STRING will be set to
-#   "master-bc5b2c5".
+#   If the content of VERSION is "1.2.3", VERSION_STRING will be set to "1.2.3".
+#   If that file does not exist and the current Git branch is "master" on commit
+#   "bc5b2c50e1fc8b1ce96607aa2f0d373900634389", with no non-committed changes to
+#   tracked files, VERSION_STRING will be set to "master-bc5b2c5".
 #
-#   # Call set_version in main CMakeLists.txt to define VERSION_STRING variable:
+#   # Call set_version in the main CMakeLists.txt file to define VERSION_STRING:
 #   set_version()
 #
 #   # Generate header file substituting "@VERSION_STRING@" with the value of the
@@ -113,15 +103,15 @@ function(set_git_dirty_variable)
   endif()
 endfunction()
 
-# Set VERSION_STRING if the argument has the form "X.Y.Z[-rcW]".
+# Set VERSION_STRING if the argument has the form "X.Y.Z".
 macro(match_and_set_version_variables version)
-  if("${version}" MATCHES "^(0|[1-9][0-9]*)[.](0|[1-9][0-9]*)[.](0|[1-9][0-9]*)([-]rc[1-9][0-9]*)?$")
+  if("${version}" MATCHES "^(0|[1-9][0-9]*)[.](0|[1-9][0-9]*)[.](0|[1-9][0-9]*)$")
     set(VERSION_STRING ${version} PARENT_SCOPE)
     set(MAJOR_VERSION "${CMAKE_MATCH_1}" PARENT_SCOPE)
     set(MINOR_VERSION "${CMAKE_MATCH_2}" PARENT_SCOPE)
     set(PATCH_VERSION "${CMAKE_MATCH_3}" PARENT_SCOPE)
   else()
-    message(FATAL_ERROR "Version ${version} does not match format X.Y.Z[-rcW]")
+    message(FATAL_ERROR "Version ${version} does not match format X.Y.Z")
   endif()
 endmacro()
 
@@ -139,7 +129,8 @@ endmacro()
 
 # Set version string variable to release version or Git branch and commit.
 function(set_version)
-  if(RELEASE_VERSION)
+  if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/VERSION")
+    file(STRINGS "${CMAKE_CURRENT_LIST_DIR}/VERSION" RELEASE_VERSION)
     match_and_set_version_variables("${RELEASE_VERSION}")
   else()
     set_version_variables_from_git_branch_and_commit()
