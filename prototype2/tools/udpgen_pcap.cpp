57bcf2629c4e712d001eec80c1d4ad14a873ea3b
Morten Jagd Christensen <morten.christensen@esss.se>
2019-05-15 16:21:24 +0200

    change to CLI11 for udpgen_pcap (#224)
    * change to CLI11 for udpgen_pcap
    
    * update opt arg names used for udpgen_pcap in system tests
    
    * adding packet throttle

--- prototype2/tools/udpgen_pcap.cpp
+++ prototype2/tools/udpgen_pcap.cpp
@@ -1,31 +1,54 @@
 /** Copyright (C) 2016, 2017 European Spallation Source ERIC */
 
+#include <CLI/CLI.hpp>
 #include <arpa/inet.h>
 #include <cassert>
 #include <cinttypes>
-#include <tools/PcapArgs.h>
 #include <tools/ReaderPcap.h>
 #include <common/Socket.h>
 #include <string.h>
 #include <string>
 #include <unistd.h>
 // GCOVR_EXCL_START
+
+struct {
+  std::string FileName{""};
+  std::string IpAddress{"127.0.0.1"};
+  uint16_t UDPPort{9000};
+  uint64_t NumberOfPackets{0}; // 0 == all packets
+  uint64_t SpeedThrottle{0}; // 0 is fastest higher is slower
+  uint64_t PktThrottle{0}; // 0 is fastest
+  bool Loop{false}; // Keep looping the same file forever
+  // Not yet CLI settings
+  uint64_t PcapOffset{0};
+  uint32_t KernelTxBufferSize{1000000};
+} Settings;
+
+CLI::App app{"Wireshark file to UDP data generator"};
+
 int main(int argc, char *argv[]) {
-  PcapArgs opts(argc, argv);
+  app.add_option("-f, --file", Settings.FileName, "Multiblade H5 file with raw readouts");
+  app.add_option("-i, --ip", Settings.IpAddress, "Destination IP address");
+  app.add_option("-p, --port", Settings.UDPPort, "Destination UDP port");
+  app.add_option("-a, --packets", Settings.NumberOfPackets, "Number of packets to send");
+  app.add_option("-t, --throttle", Settings.SpeedThrottle, "Speed throttle (0 is fastest, larger is slower)");
+  app.add_option("-s, --pkt_throttle", Settings.PktThrottle, "Extra usleep() after n packets");
+  app.add_flag("-l, --loop", Settings.Loop, "Run forever");
+  CLI11_PARSE(app, argc, argv);
   char buffer[10000];
 
   Socket::Endpoint local("0.0.0.0", 0);
-  Socket::Endpoint remote(opts.dest_ip.c_str(), opts.port);
+  Socket::Endpoint remote(Settings.IpAddress.c_str(), Settings.UDPPort);
 
   UDPTransmitter DataSource(local, remote);
-  DataSource.setBufferSizes(1000000, 0);
+  DataSource.setBufferSizes(Settings.KernelTxBufferSize, 0);
   DataSource.printBufferSizes();
 
-  std::string pcapfile(opts.filename);
+  std::string pcapfile(Settings.FileName);
 
   uint64_t packets = 0;
   uint64_t totpackets = 0;
-  int pcappackets = 0;
+  uint64_t pcappackets = 0;
   do {
     int rdsize;
     ReaderPcap pcap(pcapfile);
@@ -36,25 +59,31 @@ int main(int argc, char *argv[]) {
       }
       pcappackets++;
 
-      if (pcappackets >= opts.pcapoffset) {
+      if (pcappackets >= Settings.PcapOffset) {
         DataSource.send(buffer, rdsize);
-        if (opts.throttle) {
-          usleep(opts.throttle);
+        if (Settings.SpeedThrottle) {
+          usleep(Settings.SpeedThrottle);
         }
         packets++;
         totpackets++;
-        if (packets >= opts.txPkt) {
+        if (Settings.PktThrottle) {
+          if (totpackets % Settings.PktThrottle == 0) {
+            usleep(10);
+          }
+        }
+        if (Settings.NumberOfPackets != 0 and packets >= Settings.NumberOfPackets) {
           printf("Sent %" PRIu64 " packets\n", totpackets);
           packets = 0;
           break;
         }
       }
     }
-    if (!opts.loop)
+    if (!Settings.Loop)
       pcap.printstats();
-  } while (opts.loop);
+    printf("Sent %" PRIu64 " packets\n", totpackets);
+  } while (Settings.Loop);
   // pcap.printstats();
-  printf("Sent %" PRIu64 " packets\n", totpackets);
+
   return 0;
 }
 // GCOVR_EXCL_STOP
