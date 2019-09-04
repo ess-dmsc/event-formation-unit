bee3f6c9c36eba287288f865e889d90fca037c83
Morten Jagd Christensen <morten.christensen@esss.se>
2019-09-04 16:39:36 +0200

    Update prototype2/tools/ReaderPcap.cpp
    Co-Authored-By: Jonas Nilsson <mjc.nilsson@gmail.com>

--- prototype2/tools/ReaderPcap.cpp
+++ prototype2/tools/ReaderPcap.cpp
@@ -20,7 +20,7 @@ const int IPPROTO_UDP = 17;
 const int ETHERTYPE_OFFSET = 12;
 const int ETHERNET_HEADER_SIZE = 14;
 const int IP_HEADR_OFFSET = 14;
-#define IP_HEADER_SIZE       20
+const int IP_HEADER_SIZE = 20;
 #define UDP_HEADER_OFFSET    34
 #define UDP_HEADER_SIZE       8
 #define UDP_DATA_OFFSET      42
