8334aa1ac17c85f607512e03a1240f0ccb5a2449
Morten Jagd Christensen <morten.christensen@esss.se>
2019-11-04 14:04:45 +0100

    wrong #include for std::memset

--- src/readout/Readout.cpp
+++ src/readout/Readout.cpp
@@ -1,7 +1,7 @@
 /** Copyright (C) 2016, 2017 European Spallation Source ERIC */
 
 //#include <common/gccintel.h> // UNUSED macros
-#include <algorithm>
+#include <cstring>
 #include <common/Trace.h>
 #include <readout/Readout.h>
 
