d0e11bba88ec882ab8c992a671dc42822d05a100
Morten Hilker-Skaaning <morten.hilkerskaaning@esss.se>
2020-04-28 12:55:23 +0200

    try fix gcc

--- src/common/BitMath.h
+++ src/common/BitMath.h
@@ -9,6 +9,8 @@
 
 #pragma once
 
+#include <cstdint>
+
 class BitMath {
 public:
   ///
@@ -24,7 +26,7 @@ public:
   inline static uint16_t reversebits16(uint16_t x) {
     uint32_t temp = reversebits32(x);
     return (temp >> 16);
-  };
+  }
 
   inline static uint32_t gray2bin32(uint32_t num) {
     num = num ^ (num >> 16);
@@ -33,9 +35,9 @@ public:
     num = num ^ (num >> 2);
     num = num ^ (num >> 1);
     return num;
-  };
+  }
 
-  inline static constexpr size_t NextPowerOfTwo(size_t n) {
+  inline static constexpr uint64_t NextPowerOfTwo(uint64_t n) {
     n--;
     n |= n >> 1;
     n |= n >> 2;
