a19533abfe1657b60de88f3e5ad573e0263cf7a2
Morten Jagd Christensen <morten.christensen@esss.se>
2020-10-02 17:55:33 +0200

    gst

--- src/modules/multigrid/reduction/test/ModulePipelineTest.cpp
+++ src/modules/multigrid/reduction/test/ModulePipelineTest.cpp
@@ -1,4 +1,11 @@
-/** Copyright (C) 2016, 2017 European Spallation Source ERIC */
+// Copyright (C) 2016 - 2020 European Spallation Source, ERIC. See LICENSE file
+//===----------------------------------------------------------------------===//
+///
+/// \file
+///
+/// \brief unit tests for ModulePipeline class
+/// \todo this is currently poorly understood and probably needs better tests
+//===----------------------------------------------------------------------===//
 
 #include <multigrid/reduction/ModulePipeline.h>
 #include <multigrid/geometry/PlaneMappings.h>
@@ -18,7 +25,7 @@ protected:
 TEST_F(ModulePipelineTest, Constructor) {
   ASSERT_EQ(pipeline.out_queue.size(), 0);
   ASSERT_EQ(pipeline.stats.time_seq_errors, 0);
-  ASSERT_EQ(pipeline.stats.invalid_planes, 0); // can never happen?
+  ASSERT_EQ(pipeline.stats.invalid_planes, 0); ///< \todo can never happen?
   ASSERT_EQ(pipeline.grid_clusterer.stats_cluster_count, 0);
   ASSERT_EQ(pipeline.wire_clusterer.stats_cluster_count, 0);
 }
@@ -33,7 +40,7 @@ TEST_F(ModulePipelineTest, Ingest) {
   ASSERT_EQ(pipeline.stats.wire_clusters, 1);
   ASSERT_EQ(pipeline.stats.grid_clusters, 1);
 
-  ASSERT_EQ(pipeline.stats.events_total, 0); ///< \todo why?
+  ASSERT_EQ(pipeline.stats.events_total, 0);
 
   ASSERT_EQ(pipeline.stats.time_seq_errors, 0);
   ASSERT_EQ(pipeline.stats.invalid_planes, 0);
