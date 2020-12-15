3ae65dd34d21e2392b1f1860fdd81ee50b185a00
Morten Hilker-Skaaning <morten.hilkerskaaning@esss.se>
2020-12-15 14:27:27 +0100

    asserts for testenv

--- src/modules/jalousie/geometry/DreamGeometry.h
+++ src/modules/jalousie/geometry/DreamGeometry.h
@@ -216,6 +216,7 @@ SlicePixel SlicePixelFromPixelId(uint32_t PixelId) {
 
 uint32_t PixelIdFromSlicePixel(SlicePixel Slice) {
   using namespace DreamGeometry;
+  TestEnvAssertMsg(Slice.IsValid(), "Bad SlicePixel");
   uint32_t PixelId = 1 + Slice.X + Slice.SectorIdx * (SliceWidth) +
                      Slice.Y * (SliceWidth * SectorCount) +
                      Slice.StripIdx * (SliceWidth * SectorCount * SliceHeight);
@@ -225,6 +226,7 @@ uint32_t PixelIdFromSlicePixel(SlicePixel Slice) {
 // The layout of the Sumo types along a SliceRow, indexd by X
 // 66666666666666666666555555555555555544444444444433333333
 SumoPixel SumoPixelFromSlicePixel(SlicePixel Slice) {
+  TestEnvAssertMsg(Slice.IsValid(), "Bad SlicePixel");
   struct SliceToSumoProperty {
     uint8_t SumoStartOffsetX;
     uint8_t SumoWidth;
@@ -246,11 +248,13 @@ SumoPixel SumoPixelFromSlicePixel(SlicePixel Slice) {
   Sumo.Y = Slice.Y;
   Sumo.Width = SliceToSumo.SumoWidth;
   Sumo.Sumo = SliceToSumo.SumoId;
+  TestEnvAssertMsg(Sumo.IsValid(), "Bad SumoPixel");
   return Sumo;
 }
 
 SlicePixel SlicePixelFromSumoPixel(SumoPixel Sumo, uint32_t SectorIdx,
                                    uint32_t StripIdx) {
+  TestEnvAssertMsg(Sumo.IsValid(), "Bad SumoPixel");
   static const uint8_t SumoStartOffsetX[4] = {0, 20, 36, 48};
   uint32_t SumoIdx = 6 - Sumo.Sumo; // Sumo6=0, Sumo3=3
   SlicePixel Slice;
@@ -258,20 +262,24 @@ SlicePixel SlicePixelFromSumoPixel(SumoPixel Sumo, uint32_t SectorIdx,
   Slice.Y = Sumo.Y;
   Slice.SectorIdx = SectorIdx;
   Slice.StripIdx = StripIdx;
+  TestEnvAssertMsg(Slice.IsValid(), "Bad SlicePixel");
   return Slice;
 }
 
 StripPlanePixel StripPlanePixelFromSumoPixel(SumoPixel Sumo) {
+  TestEnvAssertMsg(Sumo.IsValid(), "Bad SumoPixel");
   StripPlanePixel StripPlane;
   uint32_t CassetteCounterIdx = Sumo.Width - Sumo.X - 1;
   StripPlane.CassetteIdx = CassetteCounterIdx / 2;
   StripPlane.CounterIdx = CassetteCounterIdx % 2;
   StripPlane.WireIdx = DreamGeometry::SliceHeight - Sumo.Y - 1;
   StripPlane.Sumo = Sumo.Sumo;
+  TestEnvAssertMsg(StripPlane.IsValid(), "Bad StripPlanePixel");
   return StripPlane;
 }
 
 SumoPixel SumoPixelFromStripPlanePixel(StripPlanePixel StripPlane) {
+  TestEnvAssertMsg(StripPlane.IsValid(), "Bad StripPlanePixel");
   uint32_t CassetteCounterIdx =
       StripPlane.CassetteIdx * 2 + StripPlane.CounterIdx;
   SumoPixel Sumo;
@@ -279,6 +287,7 @@ SumoPixel SumoPixelFromStripPlanePixel(StripPlanePixel StripPlane) {
   Sumo.Width = DreamGeometry::SumoWidths[Sumo.Sumo];
   Sumo.X = Sumo.Width - CassetteCounterIdx - 1;
   Sumo.Y = DreamGeometry::SliceHeight - StripPlane.WireIdx - 1;
+  TestEnvAssertMsg(Sumo.IsValid(), "Bad SumoPixel");
   return Sumo;
 }
 
@@ -294,7 +303,7 @@ EndCapParams EndCapParamsFromPixelId(uint32_t PixelId) {
   EndCap.Wire = StripPlane.WireIdx + 1;
   EndCap.Cassette = StripPlane.CassetteIdx + 1;
   EndCap.Counter = StripPlane.CounterIdx + 1;
-
+  TestEnvAssertMsg(EndCap.IsValid(), "Bad EndCapParams");
   return EndCap;
 }
 
