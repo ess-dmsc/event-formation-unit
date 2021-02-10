b3ca0648d5eb89ba46c2dfdf5a8da766a5a03ba2
Morten Jagd Christensen <morten.christensen@esss.se>
2021-02-10 17:00:34 +0100

    added DREAM DataParser test

---
+++ src/modules/dream/test/DataParserTest.cpp
@@ -0,0 +1,97 @@
+// Copyright (C) 2021 European Spallation Source, see LICENSE file
+//===----------------------------------------------------------------------===//
+///
+/// \file
+///
+/// \brief unit test for DREAM DataParser
+///
+//===----------------------------------------------------------------------===//
+
+#include <dream/readout/DataParser.h>
+#include <dream/test/DataParserTestData.h>
+#include <test/TestBase.h>
+
+using namespace Jalousie;
+
+class DataParserTest : public TestBase {
+protected:
+  // From Counters.h
+  struct Counters Counters;
+  DataParser Parser{Counters};
+  void SetUp() override {
+    memset(&Counters, 0, sizeof(Counters));
+  }
+  void TearDown() override {}
+};
+
+TEST_F(DataParserTest, Constructor) {
+  ASSERT_EQ(Parser.Stats.Readouts, 0);
+  ASSERT_EQ(Parser.Stats.Headers, 0);
+  ASSERT_EQ(Parser.Stats.ErrorHeaders, 0);
+  ASSERT_EQ(Parser.Stats.ErrorBytes, 0);
+  ASSERT_EQ(Parser.Result.size(), 0);
+}
+
+TEST_F(DataParserTest, BadSize) {
+  auto Res = Parser.parse((char *)&ErrBadRingGoodFEN[0], 3);
+  ASSERT_EQ(Res, 0);
+  ASSERT_EQ(Parser.Stats.Readouts, 0);
+  ASSERT_EQ(Parser.Stats.Headers, 0);
+  ASSERT_EQ(Parser.Stats.ErrorHeaders, 1);
+  ASSERT_EQ(Parser.Stats.ErrorBytes, 3);
+  ASSERT_EQ(Parser.Result.size(), 0);
+}
+
+TEST_F(DataParserTest, HeaderSizeError) {
+  auto Res = Parser.parse((char *)&ErrSizeMismatch[0], ErrSizeMismatch.size());
+  ASSERT_EQ(Res, 0);
+  ASSERT_EQ(Parser.Stats.Readouts, 0);
+  ASSERT_EQ(Parser.Stats.Headers, 1);
+  ASSERT_EQ(Parser.Stats.ErrorHeaders, 1);
+  ASSERT_EQ(Parser.Stats.ErrorBytes, ErrSizeMismatch.size());
+  ASSERT_EQ(Parser.Result.size(), 0);
+}
+
+TEST_F(DataParserTest, BadRingGoodFEN) {
+  auto Res = Parser.parse((char *)&ErrBadRingGoodFEN[0], 4);
+  ASSERT_EQ(Res, 0);
+  ASSERT_EQ(Parser.Stats.Readouts, 0);
+  ASSERT_EQ(Parser.Stats.ErrorHeaders, 1);
+  ASSERT_EQ(Parser.Stats.ErrorBytes, 4);
+  ASSERT_EQ(Parser.Result.size(), 0);
+}
+
+TEST_F(DataParserTest, GoodRingBadFEN) {
+  auto Res = Parser.parse((char *)&ErrGoodRingBadFEN[0], 4);
+  ASSERT_EQ(Res, 0);
+  ASSERT_EQ(Parser.Stats.Readouts, 0);
+  ASSERT_EQ(Parser.Stats.ErrorHeaders, 1);
+  ASSERT_EQ(Parser.Stats.ErrorBytes, 4);
+  ASSERT_EQ(Parser.Result.size(), 0);
+}
+
+TEST_F(DataParserTest, DataSizeMismatch) {
+  auto Res =
+      Parser.parse((char *)&OkThreeDreamReadouts[0], 10);
+  ASSERT_EQ(Res, 0);
+  ASSERT_EQ(Parser.Stats.Readouts, 0);
+  ASSERT_EQ(Parser.Stats.ErrorHeaders, 1);
+  ASSERT_EQ(Parser.Stats.ErrorBytes, 10);
+  ASSERT_EQ(Parser.Result.size(), 0);
+}
+
+TEST_F(DataParserTest, ParseThree) {
+  auto Res =
+      Parser.parse((char *)&OkThreeDreamReadouts[0], OkThreeDreamReadouts.size());
+  ASSERT_EQ(Res, 3);
+  ASSERT_EQ(Parser.Stats.Readouts, 3);
+  ASSERT_EQ(Parser.Stats.Headers, 1);
+  ASSERT_EQ(Parser.Stats.ErrorHeaders, 0);
+  ASSERT_EQ(Parser.Stats.ErrorBytes, 0);
+  ASSERT_EQ(Parser.Result.size(), 1);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
