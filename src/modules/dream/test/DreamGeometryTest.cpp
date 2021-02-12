5bba7ddf89944ce2e37adb239e26234e1b4ecef1
Morten Jagd Christensen <morten.christensen@esss.se>
2021-02-12 14:06:52 +0100

    added ICD geometry test

--- src/modules/dream/test/DreamGeometryTest.cpp
+++ src/modules/dream/test/DreamGeometryTest.cpp
@@ -11,6 +11,15 @@ using namespace DreamGeometry;
 
 class DreamGeometryTest : public TestBase {
 protected:
+  uint32_t calcPixel(
+      uint8_t Sector, uint8_t Sumo, uint8_t Strip,
+      uint8_t Wire, uint8_t Cassette, uint8_t Counter) {
+    DreamGeometry::EndCapParams endcap = {Sector, Sumo, Strip, Wire, Cassette, Counter};
+
+    uint32_t Pixel{0};
+    PixelIdFromEndCapParams(endcap, Pixel);
+    return Pixel;
+  }
   void SetUp() override {}
   void TearDown() override {}
 };
@@ -1146,4 +1155,16 @@ TEST_F(DreamGeometryTest, EncodeDecodeAllPixels) {
 // this tests that test-only asserts assert
 TEST_F(DreamGeometryTest, SlicePixelFromPixelId_TestEnvInput) {
   ASSERT_DEATH({ SlicePixelFromPixelId(0); }, "Bad PixelId");
+}
+
+TEST_F(DreamGeometryTest, TestingICDDefinitions) {
+  //                  sec su  st wi  mo ctr
+  ASSERT_EQ(calcPixel( 1, 6,  1, 16, 10, 2), 1); // upper left (z = 0)
+  ASSERT_EQ(calcPixel( 1, 3,  1, 16,  1, 1), 56); // next sector 'upper left'
+  ASSERT_EQ(calcPixel(23, 3,  1, 16,  1, 1), 1288); // upper right
+
+  ASSERT_EQ(calcPixel( 1, 6,  2, 16, 10, 2), 1288*16+1); // (x,y,z) = (0, 16, 1)
+
+  ASSERT_EQ(calcPixel( 1, 6, 16,  1, 10, 2), 1288*255 + 1); // bottom left
+  ASSERT_EQ(calcPixel(23, 3, 16,  1,  1, 1), 1288*256); // bottom right
 }
