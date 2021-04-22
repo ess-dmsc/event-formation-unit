904c6598d5c8f45d9751dffe93a37db4ed3b89bf
Morten Jagd Christensen <mortenjc@jcaps.com>
2021-04-22 17:07:40 +0200

    corrections following PR review

--- src/modules/generators/DataFuzzer.h
+++ src/modules/generators/DataFuzzer.h
@@ -16,28 +16,26 @@
 class DataFuzzer {
 public:
 
-  DataFuzzer() {
-    std::seed_seq SeedSequence{1, 2, 3, 4, 5};
-    Generator = new std::mt19937(SeedSequence);
-  }
+  DataFuzzer() = default;
 
   /// \brief overwrite buffer with a number of random 8-bit values
   void fuzz8Bits(void * Buffer, uint16_t BufferLength, uint16_t Repeats);
 
 
   /// \brief generate random 8 bit values
-  uint8_t random8() { return IntDist(*Generator) & 0xFF; }
+  uint8_t random8() { return IntDist(Generator) & 0xFF; }
 
   /// \brief generate random 16 bit values
-  uint16_t random16() { return IntDist(*Generator) & 0xFFFF; }
+  uint16_t random16() { return IntDist(Generator) & 0xFFFF; }
 
   /// \brief Generate random ints in the interval [Begin ; End[
   uint16_t randomInterval(uint16_t Begin, uint16_t End) {
-    return Begin + RealDist(*Generator)*(End - Begin);
+    return Begin + RealDist(Generator)*(End - Begin);
   }
 
 private:
-  std::mt19937 * Generator;
+  std::seed_seq SeedSequence{1, 2, 3, 4, 5};
+  std::mt19937 Generator{SeedSequence};
   std::uniform_int_distribution<uint32_t> IntDist;
   std::uniform_real_distribution<double> RealDist{0.0, 1.0};
 };
