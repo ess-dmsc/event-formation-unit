1637531b74a0442bd4e40d8f1fd73af95a44395f
Morten Jagd Christensen <morten.christensen@esss.se>
2021-04-23 08:31:11 +0200

    Issue 391 monkey test (#396)
    * added DataFuzzer for LoKI monkey test
    
    * move DataFuzzer to generators
    
    * corrections following PR review
    
    * more clean up

--- src/modules/loki/test/ReadoutGenerator.cpp
+++ src/modules/loki/test/ReadoutGenerator.cpp
@@ -1,4 +1,11 @@
-/** Copyright (C) 2019 European Spallation Source ERIC */
+// Copyright (C) 2019 - 2021 European Spallation Source, ERIC. See LICENSE file
+//===----------------------------------------------------------------------===//
+///
+/// \file
+///
+/// \brief Generator of artificial LoKI readouts with variable number
+/// of sections and data elements per section.
+//===----------------------------------------------------------------------===//
 
 #include <cassert>
 #include <cstdint>
@@ -8,85 +15,57 @@
 
 using namespace Loki;
 
-/// in benchmark tests
 uint16_t ReadoutGenerator::lokiReadoutDataGen(bool Randomise, uint16_t DataSections, uint16_t DataElements, uint8_t Rings,
      uint8_t * Buffer, uint16_t MaxSize, uint32_t SeqNum) {
 
-  auto DataSize = sizeof(ReadoutParser::PacketHeaderV0) + DataSections * (4 + DataElements * 20);
+  uint16_t DataSize = HeaderSize + DataSections * (4 + DataElements * 20);
   if (DataSize > MaxSize) {
-    printf("Too much data for buffer. DataSize: %zu, MaxSize: %u\n", DataSize, MaxSize);
+    printf("Too much data for buffer. DataSize: %u, MaxSize: %u\n", DataSize, MaxSize);
     return 0;
   }
 
-  //printf("Write header (28 bytes)\n");
   memset(Buffer, 0, MaxSize);
   auto DP = (uint8_t *)Buffer;
-  //printf("Buffer pointer %p\n", (void *)Buffer);
   auto Header = (ReadoutParser::PacketHeaderV0 *)DP;
+
   Header->CookieAndType = 0x30535345;
   Header->Padding0 = 0;
   Header->Version = 0;
   //Header->OutputQueue = 0x00;
 
   Header->TotalLength = DataSize;
-  if (Randomise) {
-    if (random32() < 400'000'000) { // 1 in 10-ish
-      Header->TotalLength = random16() & 0x2000;
-    }
-  }
-
   Header->SeqNum = SeqNum;
-  if (Randomise) {
-    if (random8() > 240) // 1 in 10-ish
-      Header->SeqNum = random32();
-  }
 
   uint8_t RingCount{0};
 
-  DP += sizeof(ReadoutParser::PacketHeaderV0);
+  DP += HeaderSize;
   for (auto Section = 0; Section < DataSections; Section++) {
     auto DataHeader = (ReadoutParser::DataHeader *)DP;
-    if (Randomise) {
-      DataHeader->RingId = random32() & 0xF;
-      DataHeader->FENId = random32() & 0xF;
-    } else {
-      DataHeader->RingId = RingCount % Rings;
-      DataHeader->FENId = 0x00;
-    }
-    DataHeader->DataLength = sizeof(ReadoutParser::DataHeader) +
-       DataElements * sizeof(DataParser::LokiReadout);
+    DataHeader->RingId = RingCount % Rings;
+    DataHeader->FENId = 0x00;
+    DataHeader->DataLength = DataHeaderSize +
+       DataElements * LokiDataSize;
     assert(DataHeader->DataLength == 4 + 20 * DataElements);
     RingCount++;
-    //printf("  Data Header %u @ %p (4 bytes)\n", Section, (void *)DP);
-    DP += sizeof(ReadoutParser::DataHeader);
+    DP += DataHeaderSize;
+
     for (auto Element = 0; Element < DataElements; Element++) {
       auto DataBlock = (DataParser::LokiReadout *)DP;
-      if (Randomise) {
-        DataBlock->TimeLow = random32();
-        DataBlock->TubeId = random8();
-        DataBlock->AmpA = random16();
-        DataBlock->AmpB = random16();
-        DataBlock->AmpC = random16();
-        DataBlock->AmpD = random16();
-      } else {
-        DataBlock->TimeLow = 100;
-        DataBlock->TubeId = Element % 8;
-        DataBlock->AmpA = DataElements - Element;
-        DataBlock->AmpB = Element + 1;
-        DataBlock->AmpC = Element + 1;
-        DataBlock->AmpD = Element + 1;
-      }
-      //printf("    Data Element %u @ %p (20 bytes)\n", Element, (void *)DP);
-      assert(sizeof(DataParser::LokiReadout) == 20);
-      DP += sizeof(DataParser::LokiReadout);
+      DataBlock->TimeLow = 100;
+      DataBlock->TubeId = Element % 8;
+      DataBlock->AmpA = DataElements - Element;
+      DataBlock->AmpB = Element + 1;
+      DataBlock->AmpC = Element + 1;
+      DataBlock->AmpD = Element + 1;
+      DP += LokiDataSize;
     }
   }
-  // for (uint16_t i = 0; i < DataSize; i++) {
-  //   if (i % 4 == 0) {
-  //     printf("\n");
-  //   }
-  //   printf("%02x ", Buffer[i]);
-  // }
-  // printf("\n");
+
+  // if doing fuzzing, fuzz up to one field in header & up to 20 fields in data
+  if (Randomise) {
+    Fuzzer.fuzz8Bits(Buffer, HeaderSize, 1);
+    Fuzzer.fuzz8Bits(Buffer + HeaderSize, DataSize - HeaderSize, 20);
+  }
+
   return DataSize;
 }
--- src/modules/loki/test/ReadoutGenerator.h
+++ src/modules/loki/test/ReadoutGenerator.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2019 European Spallation Source, ERIC. See LICENSE file */
+// Copyright (C) 2019 - 2021 European Spallation Source, ERIC. See LICENSE file
 //===----------------------------------------------------------------------===//
 ///
 /// \file
@@ -9,34 +9,24 @@
 
 #pragma once
 
+#include <modules/generators/DataFuzzer.h>
 #include <loki/readout/DataParser.h>
-#include <random>
 
 class ReadoutGenerator {
 public:
 
-  ReadoutGenerator() {
-    std::random_device rd;
-    gen = new std::mt19937(rd());
-  }
-
-  uint32_t random32() {
-    return dist(*gen);
-  }
-
-  uint16_t random16() {
-    return dist(*gen) & 0xFFFF;
-  }
-
-  uint8_t random8() {
-    return dist(*gen) & 0xFF;
-  }
+  ReadoutGenerator() {}
 
 /// \brief Fill out specified buffer with LoKI readouts
   uint16_t lokiReadoutDataGen(bool Randomise, uint16_t DataSections, uint16_t DataElements, uint8_t Rings,
        uint8_t * Buffer, uint16_t MaxSize, uint32_t SeqNum);
 
 private:
-  std::mt19937 * gen;
-  std::uniform_int_distribution<uint32_t> dist;
+  static_assert(sizeof(Loki::DataParser::LokiReadout) == 20, "Loki data format mismatch");
+
+  const uint16_t HeaderSize = sizeof(ReadoutParser::PacketHeaderV0);
+  const uint16_t DataHeaderSize = sizeof(ReadoutParser::DataHeader);
+  const uint16_t LokiDataSize = sizeof(Loki::DataParser::LokiReadout);
+
+  DataFuzzer Fuzzer;
 };
