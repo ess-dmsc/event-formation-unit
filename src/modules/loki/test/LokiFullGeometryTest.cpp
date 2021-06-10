245ea556238652ab2f9d5beb769bf1771a036e45
Morten Jagd Christensen <mortenjc@jcaps.com>
2021-06-10 10:47:54 +0200

    more data

--- src/modules/loki/test/LokiFullGeometryTest.cpp
+++ src/modules/loki/test/LokiFullGeometryTest.cpp
@@ -8,6 +8,7 @@
 //===----------------------------------------------------------------------===//
 
 #include <algorithm>
+#include <LokiFullGeometryTestData.h>
 #include <logical_geometry/ESSGeometry.h>
 #include <loki/geometry/PanelGeometry.h>
 #include <memory>
@@ -16,21 +17,13 @@
 
 using namespace Loki;
 
-struct NexusGeometry {
-  uint16_t Bank;
-  uint16_t Tube;
-  uint16_t Straw;
-  uint16_t Pos;
-  uint16_t Pixel;
-};
-
 class FullGeometryTest : public TestBase {
 protected:
   const bool Vertical{true};
   const bool Horizontal{false};
   const uint32_t StrawOffset0{0};
   const uint16_t TZ4{4};      ///< # tubes in z-direction
-  ESSGeometry Geometry{512, 224, 1, 1};
+  ESSGeometry Geometry{512, 6272, 1, 1};
 
   std::vector<PanelGeometry *> Banks;
 
@@ -60,27 +53,21 @@ TEST_F(FullGeometryTest, BasicValidation) {
 }
 
 TEST_F(FullGeometryTest, FirstFewLines) {
-  std::vector<struct NexusGeometry> NGData {
-    {0, 1, 1,   1,    1},
-    {0, 1, 1,   2,    2},
-    {0, 1, 1, 511,  511},
-    {0, 1, 1, 512,  512},
-    {0, 1, 2,   1,  513},
-    {0, 1, 2,   2,  514},
-    {0, 1, 2, 511, 1023},
-    {0, 1, 2, 512, 1024},
-    {0, 1, 3,   1, 1025},
-    {0, 1, 3,   2, 1026}
-  };
 
   for (auto & NG : NGData) {
-    uint32_t TubeGroup = (NG.Tube - 1) / 4;
-    uint8_t LocalTube = (NG.Tube - 1) % 8;
-    uint32_t GlobalStraw = Banks[NG.Bank]->getGlobalStrawId(TubeGroup, LocalTube, NG.Straw - 1);
-    uint32_t Pixel = Geometry.pixel2D(NG.Pos - 1, GlobalStraw);
-    // printf("Panel %u, Tube %u, Tube Group %u, LocTube %u, Straw %u, GblStraw %u\n",
-    //   NG.Bank, NG.Tube, TubeGroup, LocalTube, NG.Straw, GlobalStraw);
-    ASSERT_EQ(NG.Pixel, Pixel);
+    // helper values
+    uint8_t NBTubes = Banks[NG.Bank]->getMaxGroup() * 2; // In non-Z direction
+    uint8_t Column = NG.Tube / NBTubes;
+    uint8_t Row = NG.Tube % 2;
+
+    uint32_t TubeGroup = (NG.Tube % NBTubes) / 2;
+    uint8_t LocalTube = Row * 4 + Column;
+    uint32_t GlobalStraw = Banks[NG.Bank]->getGlobalStrawId(TubeGroup, LocalTube, NG.Straw % 7);
+    uint32_t Pixel = Geometry.pixel2D(NG.Pos, GlobalStraw);
+
+    // printf("tube %u, tubegroup %u, loctube %u, locstraw %u, col %u, row %u, gblstraw %u, pixel %u\n",
+    //   NG.Tube, TubeGroup, LocalTube, NG.Straw % 7, Column, Row, GlobalStraw, Pixel);
+    ASSERT_EQ(NG.Pixel + 1, Pixel);
   }
 }
 
