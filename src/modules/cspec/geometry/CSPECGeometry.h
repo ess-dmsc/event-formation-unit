6a18934e3c664af91c70ce9646e56300ae7ac276
JennyLouise <jenny.walker@live.co.uk>
2022-01-28 09:19:48 +0100

    Playing with conditionals coverage

--- src/modules/cspec/geometry/CSPECGeometry.h
+++ src/modules/cspec/geometry/CSPECGeometry.h
@@ -33,6 +33,8 @@ public:
                       uint8_t Channel, uint16_t XOffset, bool Rotated) override;
   uint16_t yCoord(uint8_t HybridID, uint8_t VMMID, uint8_t Channel,
                   uint16_t YOffset, bool Rotated, bool Short) override;
+  
+protected:
   bool validGridMapping(uint8_t HybridID, uint8_t VMMID, uint8_t Channel,
                         bool Short) override;
   bool validWireMapping(uint8_t HybridID, uint8_t VMMID,
