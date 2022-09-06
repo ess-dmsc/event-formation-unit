5aa77273156f981f7d0261de23e25e2e00103fa0
JennyLouise <jenny.walker@live.co.uk>
2022-09-06 11:05:31 +0200

    Added MultiHitMatcher2D

---
+++ src/common/reduction/matching/MultiHitMatcher2D.cpp
@@ -0,0 +1,102 @@
+/* Copyright (C) 2022 European Spallation Source, ERIC. See LICENSE file */
+//===----------------------------------------------------------------------===//
+///
+/// \file MultiHitMatcher2D.h
+/// \brief MultiHitMatcher2D class implementation
+///
+//===----------------------------------------------------------------------===//
+
+#include <common/debug/Trace.h>
+#include <common/reduction/matching/MultiHitMatcher2D.h>
+// #include <cmath>
+// #include <algorithm>
+
+// #undef TRC_LEVEL
+// #define TRC_LEVEL TRC_L_DEB
+
+void MultiHitMatcher2D::set_minimum_time_gap(uint64_t minimum_time_gap) {
+  minimum_time_gap_ = minimum_time_gap;
+}
+
+void MultiHitMatcher2D::match(bool flush) {
+  unmatched_clusters_.sort([](const Cluster &c1, const Cluster &c2) {
+    return c1.time_start() < c2.time_start();
+  });
+
+  XTRACE(CLUSTER, DEB, "match(): unmatched clusters %u",
+         unmatched_clusters_.size());
+
+  Event evt{PlaneA, PlaneB};
+  while (!unmatched_clusters_.empty()) {
+
+    auto cluster = unmatched_clusters_.begin();
+
+    if (!flush && !ready_to_be_matched(*cluster)) {
+      XTRACE(CLUSTER, DEB, "not ready to be matched");
+      break;
+    }
+
+    if (!evt.empty() && (evt.time_gap(*cluster) > minimum_time_gap_)) {
+      XTRACE(CLUSTER, DEB, "time gap too large");
+      if ((evt.ClusterA.coord_span() < maximum_coord_span_) and (evt.ClusterB.coord_span() < maximum_coord_span_)){
+        stash_event(evt);
+        evt.clear();
+      }
+      else{ //split clusters by coord gaps and attempt to match based on ADC values
+        split_and_stash_event(evt);
+      }
+    }
+
+    evt.merge(*cluster);
+
+    unmatched_clusters_.pop_front();
+  }
+
+  /// If anything remains
+  if (!evt.empty()) {
+    if (flush) {
+      stash_event(evt);
+    } else {
+      requeue_clusters(evt);
+    }
+  }
+}
+
+std::string MultiHitMatcher2D::config(const std::string &prepend) const {
+  std::stringstream ss;
+  ss << AbstractMatcher::config(prepend);
+  ss << prepend << fmt::format("minimum_time_gap: {}\n", minimum_time_gap_);
+  return ss.str();
+}
+
+void MultiHitMatcher2D::split_and_stash_event(Event evt){
+  Cluster new_cluster;
+  std::vector<Cluster> new_clusters_a;
+  std::vector<Cluster> new_clusters_b;
+
+  sort_by_increasing_coordinate(evt.ClusterA.hits);
+  sort_by_increasing_coordinate(evt.ClusterB.hits);
+  uint last_coord = 0;
+
+
+  for(Hit hit : evt.ClusterA.hits){
+    if ((!new_cluster.empty()) && (hit.coordinate - last_coord > minimum_coord_gap_)){
+      new_clusters_a.push_back(new_cluster);
+      new_cluster.clear();
+      new_cluster.insert(hit);
+    }
+    else{
+      last_coord = hit.coordinate;
+      new_cluster.insert(hit);
+    }
+  }
+  if (!new_cluster.empty()){
+    new_clusters_a.push_back(new_cluster);
+    new_cluster.clear();
+  }
+
+  for(Cluster cluster : new_clusters_a){
+    XTRACE(DATA, DEB, "New cluster created, starts at coord %u, ends at coord %u", cluster.coord_start(), cluster.coord_end());
+  }
+  
+}
---
+++ src/common/reduction/matching/MultiHitMatcher2D.h
@@ -0,0 +1,49 @@
+/* Copyright (C) 2022 European Spallation Source, ERIC. See LICENSE file */
+//===----------------------------------------------------------------------===//
+///
+/// \file MultiHitMatcher2D.h
+/// \brief MultiHitMatcher2D class definition
+///
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <common/reduction/matching/AbstractMatcher.h>
+
+/// \class MultiHitMatcher2D MultiHitMatcher2D.h
+/// \brief Matcher implementation that joins clusters into events
+///         if time gaps between them are sufficiently small, and
+///         handles multiple hits in a time window by splitting
+///         clusters by coordinate gaps and using relationship
+///         between ADC values in different planes to match 
+///         resulting clusters
+
+class MultiHitMatcher2D : public AbstractMatcher {
+
+  public:
+  /// Inherits constructor
+  using AbstractMatcher::AbstractMatcher;
+  /// \brief sets the minimum time gap criterion
+  /// \param minimum_time_gap minimum time gap between subsequent clusters for
+  /// them
+  ///         to be disambiguated into separate events. If time gap is smaller,
+  ///         the clusters are merged into one event.
+  void set_minimum_time_gap(uint64_t minimum_time_gap);
+
+  /// \brief Match queued up clusters into events.
+  ///         Clusters that either overlap in time or have time gaps that are
+  ///         smaller than the minimum time gap are joined into events.
+  /// \param flush if all queued clusters should be matched regardless of
+  ///        latency considerations.
+  void match(bool flush) override;
+
+  /// \brief print configuration of GapMatcher
+  std::string config(const std::string &prepend) const override;
+
+private:
+  void split_and_stash_event(Event evt);
+
+  uint64_t minimum_time_gap_{0};
+  uint16_t minimum_coord_gap_{10};
+  uint16_t maximum_coord_span_{10};
+};
