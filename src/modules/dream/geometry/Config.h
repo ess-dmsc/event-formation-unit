e81c2da455283fa1fa8bb9788fe964e964872a67
Morten Jagd Christensen <mortenjc@jcaps.com>
2022-09-19 14:18:09 +0200

    initial work on DREAM ring/fen configuration

--- src/modules/dream/geometry/Config.h
+++ src/modules/dream/geometry/Config.h
@@ -19,11 +19,37 @@
 namespace Dream {
 class Config {
 public:
+  static constexpr int MaxRing{11};
+  static constexpr int MaxFEN{11};
+
+  enum ModuleType {BwEndCap, FwEndCap, Mantle, HR, SANS};
+  std::map<std::string, ModuleType> ModuleTypeMap = {
+    {"BwEndCap", BwEndCap},
+    {"FwEndCap", FwEndCap},
+    {"Mantle",   Mantle},
+    {"HR",       HR},
+    {"SANS",     SANS}
+  };
+
+  struct ModuleParms {
+    bool Initialized{false};
+    ModuleType Type;
+    bool WireIsZ;
+    int Wires;
+    int Strips;
+    int Rotation;
+  };
+
   Config();
 
   Config(std::string ConfigFile);
 
+  /// \brief log errormessage and throw runtime exception
+  void errorExit(std::string errmsg);
+
   uint32_t MaxPulseTimeNS{5 * 71'428'571}; // 5 * 1/14 * 10^9
+
+  ModuleParms RMConfig[MaxRing + 1][MaxFEN + 1];
 
 private:
 };
