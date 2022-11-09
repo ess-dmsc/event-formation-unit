3a6dc0434a69df2de3000a888133d64e9c1eaedc
Morten Jagd Christensen <mortenjc@jcaps.com>
2022-11-09 11:46:40 +0100

    more unit-ish tests

--- src/common/detector/EFUArgs.cpp
+++ src/common/detector/EFUArgs.cpp
@@ -182,7 +182,7 @@ EFUArgs::Status EFUArgs::parseArgs(const int argc, char *argv[]) {
     CLIParser.parse(argc, argv);
   } catch (const CLI::ParseError &e) {
     LOG(INIT, Sev::Error, "Invalid CLI argument(s) - error code {}", e.get_exit_code());
-    return Status::EXIT;
+    return Status::ERREXIT;
   }
 
   if ((*HelpOption)) {
