eba17c69e17c451e0fa43f6a4e2e91a25ec631e5
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-01-09 15:55:37 +0100

    script documentation

--- cppcheck_exclude_tests.sh
+++ cppcheck_exclude_tests.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 DIR=$1
-#mydir=$(pwd)
 
+# find all test files (assuming they end in Test.cpp/h/hpp
+# then make command line arguments for cppcheck of the form
+# -ipath/to/testfile.cpp -ipath/to/other/testfile.cpp ...
+
+# Used in the cppcheck call for cppcheck target and for Jenkins builds
+
+# empty list is fine
 if [[ $DIR == "" ]] ; then
   exit 0
 fi
 
 files=$(find $DIR -type f -regex '.*Test\.[ch]p*')
-#files=$(find $DIR -type d -name test| sed 's/^\.\.\///')
 
-EXCL=""
+excl=""
 for file in $files
 do
-    EXCL=$EXCL" -i$file"
+    excl=$excl" -i$file"
 done
 
-echo $EXCL
+echo $excl
