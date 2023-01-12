7c64896d0e0d9633d5d1c93e3432297baab9fde8
JennyLouise <jenny.walker@live.co.uk>
2023-01-12 10:26:30 +0100

    NMX

--- src/modules/nmx/geometry/Geometry.h
+++ src/modules/nmx/geometry/Geometry.h
@@ -22,6 +22,18 @@ namespace Nmx {
 
 class Geometry {
 public:
+  ///
+  ///\brief calculate coordinate value, in either plane
+  ///
+  ///\param Channel integer between 0 and 63 identifying the channel
+  ///         a readout has come from.
+  ///\param AsicId 0 or 1, identifying the asic a readout is from
+  ///\param Offset offset from 0
+  ///\param ReversedChannels boolean determining if channel order
+  ///         is reversed, ie. top half of a panel in X plane,
+  ///         or right side of a panel in Y plane.
+  ///\return uint16_t 
+  ///
   virtual uint16_t coord(uint8_t Channel, uint8_t AsicId, uint16_t Offset,
                          bool ReversedChannels) = 0;
   static constexpr uint16_t InvalidCoord = 65535;
