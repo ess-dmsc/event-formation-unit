ac3e972d04a2c1bfa18b11fe9eef7d9e5ced6f4a
JennyLouise <jenny.walker@live.co.uk>
2023-05-16 14:33:31 +0200

    Improved parser test coverage

--- src/modules/timepix3/test/Timepix3ParserTest.cpp
+++ src/modules/timepix3/test/Timepix3ParserTest.cpp
@@ -10,32 +10,68 @@
 
 using namespace Timepix3;
 
+
+std::vector<uint8_t> SinglePixelReadout{
+    // Single readout
+    0x91, 0xc6, 0x30, 0x80,
+    0x8b, 0xa8, 0x3a, 0xbf
+};
+
+std::vector<uint8_t> TooShort{
+    0x00, 0x01
+};
+
+std::vector<uint8_t> SingleTDCReadout{
+    // Single readout
+    0xc0, 0x42, 0x9f, 0xdd,
+    0xa4, 0x7e, 0x8b, 0x6f
+};
+
+std::vector<uint8_t> SingleEVRReadout{
+  0x01, 0x45, 0x53, 0x53,
+  0x2a, 0xc0, 0x08, 0x00,
+  0xcc, 0xa1, 0x3f, 0x64,
+  0xdd, 0xc9, 0x9a, 0x3b,
+  0xcb, 0xa1, 0x3f, 0x64,
+  0xdd, 0xc9, 0x9a, 0x3b
+};
+
 class Timepix3ParserTest : public TestBase {
 protected:
   struct Counters counters;
 
-  DataParser *timepix3;
+  DataParser *Timepix3Parser;
 
   void SetUp() override {
-     timepix3 = new DataParser(counters);
+     Timepix3Parser = new DataParser(counters);
    }
   void TearDown() override {}
 };
 
 // Test cases below
 TEST_F(Timepix3ParserTest, Constructor) {
-  DataParser Timepix3(counters);
+  DataParser Timepix3Parser(counters);
 }
 
-// TEST_F(Timepix3ParserTest, SingleGoodReadout) {
-//   auto Res = timepix3->Timepix3Parser.parse((char *)SingleGoodReadout.data(), SingleGoodReadout.size());
-//   ASSERT_EQ(Res, 1);
-//   ASSERT_EQ(counters.PixelReadouts, 1);
+TEST_F(Timepix3ParserTest, SinglePixelReadout){
+  auto Res = Timepix3Parser->parse((char *)SinglePixelReadout.data(), SinglePixelReadout.size());
+  EXPECT_EQ(Res, 1);
+}
 
-//   timepix3->processReadouts();
+TEST_F(Timepix3ParserTest, SingleTDCReadout){
+  auto Res = Timepix3Parser->parse((char *)SingleTDCReadout.data(), SingleTDCReadout.size());
+  EXPECT_EQ(Res, 1);
+}
 
-//   // ASSERT_EQ(counters.Events, 1);
-// }
+TEST_F(Timepix3ParserTest, TooShort){
+  auto Res = Timepix3Parser->parse((char *)TooShort.data(), TooShort.size());
+  EXPECT_EQ(Res, 0);
+}
+
+TEST_F(Timepix3ParserTest, SingleEVRReadout){
+  auto Res = Timepix3Parser->parse((char *)SingleEVRReadout.data(), SingleEVRReadout.size());
+  EXPECT_EQ(Res, 1);
+}
 
 int main(int argc, char **argv) {
 
