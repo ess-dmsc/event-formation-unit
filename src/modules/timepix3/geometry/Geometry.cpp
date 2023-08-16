9c5f42807c4f90edb7f970f14cb98d7db9d334a6
Tibor Bukovics <tibor.bukovics@ess.eu>
2023-08-16 14:00:21 +0000

    Some comments about calculation and formating

--- src/modules/timepix3/geometry/Geometry.cpp
+++ src/modules/timepix3/geometry/Geometry.cpp
@@ -12,32 +12,41 @@
 // #undef TRC_LEVEL
 // #define TRC_LEVEL TRC_L_DEB
 
-namespace Timepix3 {
+namespace Timepix3
+{
 
-uint32_t Geometry::calcPixel(DataParser::Timepix3PixelReadout &Data) {
-  XTRACE(DATA, DEB, "calculating pixel");
-  uint16_t X = calcX(Data);
-  uint16_t Y = calcY(Data);
+  uint32_t Geometry::calcPixel(DataParser::Timepix3PixelReadout &Data)
+  {
+    XTRACE(DATA, DEB, "calculating pixel");
+    uint16_t X = calcX(Data);
+    uint16_t Y = calcY(Data);
 
-  XTRACE(DATA, DEB, "using ESS Geom to calculate pixel id from X %u, Y %u", X,
-         Y);
-  return ESSGeom->pixel2D(X, Y);
-}
+    XTRACE(DATA, DEB, "using ESS Geom to calculate pixel id from X %u, Y %u", X,
+           Y);
+    return ESSGeom->pixel2D(X, Y);
+  }
 
-uint32_t Geometry::calcX(DataParser::Timepix3PixelReadout &Data) {
-  uint16_t Col = Data.Dcol + Data.Pix / 4;
-  return Col;
-}
+  // Calculation and naming (Col and Row) is taken over from CFEL-CMI pymepix
+  // https://github.com/CFEL-CMI/pymepix/blob/develop/pymepix/processing/logic/packet_processor.py
+  uint32_t Geometry::calcX(DataParser::Timepix3PixelReadout &Data)
+  {
+    uint16_t Col = Data.Dcol + Data.Pix / 4;
+    return Col;
+  }
 
-uint32_t Geometry::calcY(DataParser::Timepix3PixelReadout &Data) {
-  uint16_t Row = Data.Spix + (Data.Pix & 0x3);
-  return Row;
-}
+  // Calculation and naming (Col and Row) is taken over from CFEL-CMI pymepix
+  // https://github.com/CFEL-CMI/pymepix/blob/develop/pymepix/processing/logic/packet_processor.py
+  uint32_t Geometry::calcY(DataParser::Timepix3PixelReadout &Data)
+  {
+    uint16_t Row = Data.Spix + (Data.Pix & 0x3);
+    return Row;
+  }
 
-///\todo implement this
-bool Geometry::validateData(DataParser::Timepix3PixelReadout &Data) {
-  XTRACE(DATA, DEB, "validate data, dcol = %u", Data.Dcol);
-  return true;
-}
+  ///\todo implement this
+  bool Geometry::validateData(DataParser::Timepix3PixelReadout &Data)
+  {
+    XTRACE(DATA, DEB, "validate data, dcol = %u", Data.Dcol);
+    return true;
+  }
 
 } // namespace Timepix3
--- src/modules/timepix3/readout/DataParser.cpp
+++ src/modules/timepix3/readout/DataParser.cpp
@@ -14,166 +14,191 @@
 // #undef TRC_LEVEL
 // #define TRC_LEVEL TRC_L_DEB
 
-namespace Timepix3 {
+namespace Timepix3
+{
 
-int DataParser::parse(const char *Buffer, unsigned int Size) {
-  XTRACE(DATA, DEB, "parsing data, size is %u", Size);
-  PixelResult.clear();
-  unsigned int ParsedReadouts = 0;
+  int DataParser::parse(const char *Buffer, unsigned int Size)
+  {
+    XTRACE(DATA, DEB, "parsing data, size is %u", Size);
+    PixelResult.clear();
+    unsigned int ParsedReadouts = 0;
 
-  unsigned int BytesLeft = Size;
-  char *DataPtr = (char *)Buffer;
+    unsigned int BytesLeft = Size;
+    char *DataPtr = (char *)Buffer;
 
-  // packets in timepix3 datastream are either from the camera or the EVR system
-  // if from the EVR system, they will be 24 bits, and will contain pulse time
-  // information. If they are 24 bits but not type = 1, then it is a camera
-  // packet
-  if (Size == 24) {
-    XTRACE(DATA, DEB, "size is 24, could be EVR timestamp");
-    EVRTimeReadout *Data = (EVRTimeReadout *)((char *)DataPtr);
-    if (Data->Type == 1) {
-      XTRACE(DATA, DEB,
-             "Processed readout, packet type = %u, counter = %u, pulsetime "
-             "seconds = %u, "
-             "pulsetime nanoseconds = %u, previous pulsetime seconds = %u, "
-             "previous pulsetime nanoseconds = %u",
-             1, Data->Counter, Data->PulseTimeSeconds,
-             Data->PulseTimeNanoSeconds, Data->PrevPulseTimeSeconds,
-             Data->PrevPulseTimeNanoSeconds);
-      Stats.EVRTimestampReadouts++;
-      LastEVRTime =
-          Data->PulseTimeSeconds * 1000000000 + Data->PulseTimeNanoSeconds;
-      return 1;
-    }
-    XTRACE(DATA, DEB,
-           "Not type = 1, not an EVR timestamp, processing as normal");
-  }
-
-  while (BytesLeft) {
-    uint64_t DataBytes;
-
-    if (BytesLeft < sizeof(DataBytes)) {
-      // TODO add some error handling here
-      XTRACE(DATA, DEB, "not enough bytes left, %u", BytesLeft);
-      break;
-    }
-    // Copy 8 bytes into DataBytes variable
-    memcpy(&DataBytes, DataPtr, sizeof(DataBytes));
-
-    // regardless of readout type, the type variable is always in the same place
-    // we read it here
-    uint8_t ReadoutType = (DataBytes & TYPE_MASK) >> TYPE_OFFS;
-
-    // pixel readout, identifies where a pixel on the camera was activated
-    if (ReadoutType == 11) {
-      Timepix3PixelReadout Data;
-
-      // mask and offset values are defined in DataParser.h
-      Data.Dcol = (DataBytes & PIXEL_DCOL_MASK) >> PIXEL_DCOL_OFFSET;
-      Data.Spix = (DataBytes & PIXEL_SPIX_MASK) >> PIXEL_SPIX_OFFSET;
-      Data.Pix = (DataBytes & PIXEL_PIX_MASK) >> PIXEL_PIX_OFFSET;
-      Data.ToA = (DataBytes & PIXEL_TOA_MASK) >> PIXEL_TOA_OFFSET;
-      Data.ToT = ((DataBytes & PIXEL_TOT_MASK) >> PIXEL_TOT_OFFSET) * 25;
-      Data.FToA = (DataBytes & PIXEL_FTOA_MASK) >> PIXEL_FTOA_OFFSET;
-      Data.SpidrTime = DataBytes & PIXEL_SPTIME_MASK;
-
-      XTRACE(DATA, DEB,
-             "Processed readout, ReadoutType = %u, Dcol = %u, Spix = %u, Pix = "
-             "%u, SpidrTime = %u, ToA = %u, FToA = %u, ToT = %u",
-             ReadoutType, Data.Dcol, Data.Spix, Data.Pix, Data.SpidrTime,
-             Data.ToA, Data.FToA, Data.ToT);
-
-      // toa formula is based on information from the timepix3 manual provided
-      // with the camera
-      uint64_t toa = uint64_t(409600 * uint64_t(Data.SpidrTime) +
-                              25 * uint64_t(Data.ToA) - 1.5625 * Data.FToA);
-      XTRACE(DATA, DEB,
-             "ToA in nanoseconds: %u, previous TDC timestamp in nanoseconds: "
-             "%u, difference: %u",
-             toa, LastTDCTime,
-             int(409600 * Data.SpidrTime + 25 * Data.ToA - 1.5625 * Data.FToA) -
-                 LastTDCTime);
-      ParsedReadouts++;
-      Stats.PixelReadouts++;
-
-      // LastTDCTime is the latest seen time from a TDC readout, and this
-      // counter is used to see how strictly in order the pixel readouts are in
-      // relation to this. Pixel readouts from before the last TDC time belong
-      // to the previous pulse, and may need to be treated differently
-      if (toa < LastTDCTime) {
-        XTRACE(DATA, DEB, "Pixel readout from before TDC");
-        Stats.PixelReadoutFromBeforeTDC++;
+    // packets in timepix3 datastream are either from the camera or the EVR system
+    // if from the EVR system, they will be 24 bits, and will contain pulse time
+    // information. If they are 24 bits but not type = 1, then it is a camera
+    // packet
+    if (Size == 24)
+    {
+      XTRACE(DATA, DEB, "size is 24, could be EVR timestamp");
+      EVRTimeReadout *Data = (EVRTimeReadout *)((char *)DataPtr);
+      if (Data->Type == 1)
+      {
+        XTRACE(DATA, DEB,
+               "Processed readout, packet type = %u, counter = %u, pulsetime "
+               "seconds = %u, "
+               "pulsetime nanoseconds = %u, previous pulsetime seconds = %u, "
+               "previous pulsetime nanoseconds = %u",
+               1, Data->Counter, Data->PulseTimeSeconds,
+               Data->PulseTimeNanoSeconds, Data->PrevPulseTimeSeconds,
+               Data->PrevPulseTimeNanoSeconds);
+        Stats.EVRTimestampReadouts++;
+        LastEVRTime =
+            Data->PulseTimeSeconds * 1000000000 + Data->PulseTimeNanoSeconds;
+        return 1;
       }
-
-      PixelResult.push_back(Data);
-
-      // TDC readout type, indicating when the camera received a TDC pulse. In
-      // the ESS setup, this should correspond to an EVR pulse, indicating the
-      // start of a new pulse.
-    } else if (ReadoutType == 6) {
-      Timepix3TDCReadout Data;
-
-      // mask and offset values are defined in DataParser.h
-      Data.Type = (DataBytes & TDC_TYPE_MASK) >> TDC_TYPE_OFFSET;
-      Data.TriggerCounter =
-          (DataBytes & TDC_TRIGGERCOUNTER_MASK) >> TDC_TRIGGERCOUNTER_OFFSET;
-      Data.Timestamp = (DataBytes & TDC_TIMESTAMP_MASK) >> TDC_TIMESTAMP_OFFSET;
-      Data.Stamp = (DataBytes & TDC_STAMP_MASK) >> TDC_STAMP_OFFSET;
-
       XTRACE(DATA, DEB,
-             "Processed readout, ReadoutType = %u, trigger_counter = %u, "
-             "timestamp = %u, stamp = %u",
-             ReadoutType, Data.TriggerCounter, Data.Timestamp, Data.Stamp);
-      ParsedReadouts++;
-      Stats.TDCReadouts++;
-
-      // Time calculations based on information from the timepix manual
-      LastTDCTime = 3.125 * Data.Timestamp + 0.26 * Data.Stamp;
-
-      // TDC readouts can belong to one of two channels, and can either indicate
-      // the rising or the falling edge of the signal. The camera setup will
-      // determine which of these are sent.
-      if (Data.Type == 15) {
-        Stats.TDC1RisingReadouts++;
-      } else if (Data.Type == 10) {
-        Stats.TDC1FallingReadouts++;
-      } else if (Data.Type == 14) {
-        Stats.TDC2RisingReadouts++;
-      } else if (Data.Type == 11) {
-        Stats.TDC2FallingReadouts++;
-      } else {
-        // this should never happen - if it does something has gone wrong with
-        // the data format or parsing
-        Stats.UnknownTDCReadouts++;
-      }
-
-      // global timestamps are a readout allowing for longer periods of time to
-      // be recorded before "overflowing" to 0. This readout type isn't expected
-      // to be used in production, currently.
-    } else if (ReadoutType == 4) {
-      Timepix3GlobalTimeReadout Data;
-
-      // mask and offset values are defined in DataParser.h
-      Data.Timestamp =
-          (DataBytes & GLOBAL_TIMESTAMP_MASK) >> GLOBAL_TIMESTAMP_OFFSET;
-      Data.Stamp = (DataBytes & GLOBAL_STAMP_MASK) >> GLOBAL_STAMP_OFFSET;
-
-      XTRACE(DATA, DEB,
-             "Processed readout, ReadoutType = %u, Timestamp = %u, Stamp = %u",
-             ReadoutType, Data.Timestamp, Data.Stamp);
-      ParsedReadouts++;
-      Stats.GlobalTimestampReadouts++;
-
-    } else {
-      // we sometimes see packet type 7 here, which accompanies a lot of control
-      // signals
-      XTRACE(DATA, WAR, "Unknown packet type: %u", ReadoutType);
-      Stats.UndefinedReadouts++;
+             "Not type = 1, not an EVR timestamp, processing as normal");
     }
-    BytesLeft -= sizeof(DataBytes);
-    DataPtr += sizeof(DataBytes);
-  }
 
-  return ParsedReadouts;
-}
+    while (BytesLeft)
+    {
+      uint64_t DataBytes;
+
+      if (BytesLeft < sizeof(DataBytes))
+      {
+        // TODO add some error handling here
+        XTRACE(DATA, DEB, "not enough bytes left, %u", BytesLeft);
+        break;
+      }
+      // Copy 8 bytes into DataBytes variable
+      memcpy(&DataBytes, DataPtr, sizeof(DataBytes));
+
+      // regardless of readout type, the type variable is always in the same place
+      // we read it here
+      uint8_t ReadoutType = (DataBytes & TYPE_MASK) >> TYPE_OFFS;
+
+      // pixel readout, identifies where a pixel on the camera was activated
+      if (ReadoutType == 11)
+      {
+        Timepix3PixelReadout Data;
+
+        // mask and offset values are defined in DataParser.h
+        Data.Dcol = (DataBytes & PIXEL_DCOL_MASK) >> PIXEL_DCOL_OFFSET;
+        Data.Spix = (DataBytes & PIXEL_SPIX_MASK) >> PIXEL_SPIX_OFFSET;
+        Data.Pix = (DataBytes & PIXEL_PIX_MASK) >> PIXEL_PIX_OFFSET;
+        Data.ToA = (DataBytes & PIXEL_TOA_MASK) >> PIXEL_TOA_OFFSET;
+
+        // TOT has a unit of 25ns and we convert according to that.
+        Data.ToT = ((DataBytes & PIXEL_TOT_MASK) >> PIXEL_TOT_OFFSET) * 25;
+        Data.FToA = (DataBytes & PIXEL_FTOA_MASK) >> PIXEL_FTOA_OFFSET;
+        Data.SpidrTime = DataBytes & PIXEL_SPTIME_MASK;
+
+        XTRACE(DATA, DEB,
+               "Processed readout, ReadoutType = %u, Dcol = %u, Spix = %u, Pix = "
+               "%u, SpidrTime = %u, ToA = %u, FToA = %u, ToT = %u",
+               ReadoutType, Data.Dcol, Data.Spix, Data.Pix, Data.SpidrTime,
+               Data.ToA, Data.FToA, Data.ToT);
+
+        // toa formula is based on information from the timepix3 manual provided
+        // with the camera
+        uint64_t toa = uint64_t(409600 * uint64_t(Data.SpidrTime) +
+                                25 * uint64_t(Data.ToA) - 1.5625 * Data.FToA);
+        XTRACE(DATA, DEB,
+               "ToA in nanoseconds: %u, previous TDC timestamp in nanoseconds: "
+               "%u, difference: %u",
+               toa, LastTDCTime,
+               //  These are converion according to the Timepix manual
+               int(409600 * Data.SpidrTime + 25 * Data.ToA - 1.5625 * Data.FToA) -
+                   LastTDCTime);
+        ParsedReadouts++;
+        Stats.PixelReadouts++;
+
+        // LastTDCTime is the latest seen time from a TDC readout, and this
+        // counter is used to see how strictly in order the pixel readouts are in
+        // relation to this. Pixel readouts from before the last TDC time belong
+        // to the previous pulse, and may need to be treated differently
+        if (toa < LastTDCTime)
+        {
+          XTRACE(DATA, DEB, "Pixel readout from before TDC");
+          Stats.PixelReadoutFromBeforeTDC++;
+        }
+
+        PixelResult.push_back(Data);
+
+        // TDC readout type, indicating when the camera received a TDC pulse. In
+        // the ESS setup, this should correspond to an EVR pulse, indicating the
+        // start of a new pulse.
+      }
+      else if (ReadoutType == 6)
+      {
+        Timepix3TDCReadout Data;
+
+        // mask and offset values are defined in DataParser.h
+        Data.Type = (DataBytes & TDC_TYPE_MASK) >> TDC_TYPE_OFFSET;
+        Data.TriggerCounter =
+            (DataBytes & TDC_TRIGGERCOUNTER_MASK) >> TDC_TRIGGERCOUNTER_OFFSET;
+        Data.Timestamp = (DataBytes & TDC_TIMESTAMP_MASK) >> TDC_TIMESTAMP_OFFSET;
+        Data.Stamp = (DataBytes & TDC_STAMP_MASK) >> TDC_STAMP_OFFSET;
+
+        XTRACE(DATA, DEB,
+               "Processed readout, ReadoutType = %u, trigger_counter = %u, "
+               "timestamp = %u, stamp = %u",
+               ReadoutType, Data.TriggerCounter, Data.Timestamp, Data.Stamp);
+        ParsedReadouts++;
+        Stats.TDCReadouts++;
+
+        // Time calculations based on information from the timepix manual
+        LastTDCTime = 3.125 * Data.Timestamp + 0.26 * Data.Stamp;
+
+        // TDC readouts can belong to one of two channels, and can either indicate
+        // the rising or the falling edge of the signal. The camera setup will
+        // determine which of these are sent.
+        if (Data.Type == 15)
+        {
+          Stats.TDC1RisingReadouts++;
+        }
+        else if (Data.Type == 10)
+        {
+          Stats.TDC1FallingReadouts++;
+        }
+        else if (Data.Type == 14)
+        {
+          Stats.TDC2RisingReadouts++;
+        }
+        else if (Data.Type == 11)
+        {
+          Stats.TDC2FallingReadouts++;
+        }
+        else
+        {
+          // this should never happen - if it does something has gone wrong with
+          // the data format or parsing
+          Stats.UnknownTDCReadouts++;
+        }
+
+        // global timestamps are a readout allowing for longer periods of time to
+        // be recorded before "overflowing" to 0. This readout type isn't expected
+        // to be used in production, currently.
+      }
+      else if (ReadoutType == 4)
+      {
+        Timepix3GlobalTimeReadout Data;
+
+        // mask and offset values are defined in DataParser.h
+        Data.Timestamp =
+            (DataBytes & GLOBAL_TIMESTAMP_MASK) >> GLOBAL_TIMESTAMP_OFFSET;
+        Data.Stamp = (DataBytes & GLOBAL_STAMP_MASK) >> GLOBAL_STAMP_OFFSET;
+
+        XTRACE(DATA, DEB,
+               "Processed readout, ReadoutType = %u, Timestamp = %u, Stamp = %u",
+               ReadoutType, Data.Timestamp, Data.Stamp);
+        ParsedReadouts++;
+        Stats.GlobalTimestampReadouts++;
+      }
+      else
+      {
+        // we sometimes see packet type 7 here, which accompanies a lot of control
+        // signals
+        XTRACE(DATA, WAR, "Unknown packet type: %u", ReadoutType);
+        Stats.UndefinedReadouts++;
+      }
+      BytesLeft -= sizeof(DataBytes);
+      DataPtr += sizeof(DataBytes);
+    }
+
+    return ParsedReadouts;
+  }
 } // namespace Timepix3
