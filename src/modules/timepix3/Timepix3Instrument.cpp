24c1d21774210e45cb12189cc3eb33473cc6c814
Tibor Bukovics <tibor.bukovics@ess.eu>
2023-08-17 14:06:21 +0000

    Add comment on TOF calculation

--- src/modules/timepix3/Timepix3Instrument.cpp
+++ src/modules/timepix3/Timepix3Instrument.cpp
@@ -51,6 +51,11 @@ uint32_t Timepix3Instrument::calcPixel(DataParser::Timepix3PixelReadout &Data) {
   return pixel;
 }
 
+// ToDo: This calculates time of flight according to internal clock status
+// which may not relates to the EVR pulse time. This later drives to a wrong 
+// pairing in the KAFKA event (see generateEvent(). We should reset this value when
+// the TDC packet arrived like:
+// TOF - TDC time.
 uint64_t
 Timepix3Instrument::calcTimeOfFlight(DataParser::Timepix3PixelReadout &Data) {
   XTRACE(DATA, DEB, "Calculating TOF");
