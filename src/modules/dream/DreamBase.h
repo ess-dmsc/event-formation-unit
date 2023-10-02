eed49a0e4775cda70ee3076106d007df7495c08e
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-10-02 11:42:45 +0200

    update copyright header

--- src/modules/dream/DreamBase.h
+++ src/modules/dream/DreamBase.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2021 European Spallation Source, see LICENSE file
+// Copyright (C) 2021 - 2023 European Spallation Source, see LICENSE file
 //===----------------------------------------------------------------------===//
 ///
 /// \file
