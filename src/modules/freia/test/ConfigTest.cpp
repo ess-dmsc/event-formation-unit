cf18d629ab8f15354272ee42323f7a140ea41a85
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-10-11 08:42:41 +0200

    more elegant way of unit testing based on nlohmann json

--- src/modules/freia/test/ConfigTest.cpp
+++ src/modules/freia/test/ConfigTest.cpp
@@ -1,18 +1,13 @@
-// Copyright (C) 2016 - 2020 European Spallation Source, ERIC. See LICENSE file
+// Copyright (C) 2016 - 2023 European Spallation Source, ERIC. See LICENSE file
 //===----------------------------------------------------------------------===//
 ///
 /// \file
 //===----------------------------------------------------------------------===//
 
-#include <common/testutils/SaveBuffer.h>
+#include <common/JsonFile.h>
 #include <common/testutils/TestBase.h>
 #include <freia/geometry/Config.h>
 
-auto j2 = R"(
-{
-  "DoesNothing" : 0
-}
-)"_json;
 
 auto NoDetector = R"(
 {
@@ -20,70 +15,18 @@ auto NoDetector = R"(
 }
 )"_json;
 
-auto InvalidDetector = R"(
-{
-  "Detector": "Freias",
-  "WireChOffset" : 16
-}
-)"_json;
 
-auto InvalidRing = R"(
-{
-  "Detector": "Freia",
-
-  "WireChOffset" : 16,
-
-  "Config" : [
-    { "Ring" :  0, "FEN": 1, "Hybrid" :  0, "HybridId" : "E5533333222222221111111100000000"},
-    { "Ring" :  0, "FEN": 1, "Hybrid" :  1, "HybridId" : "E5533333222222221111111100000001"},
-    { "Ring" :  0, "FEN": 2, "Hybrid" :  0, "HybridId" : "E5533333222222221111111100000002"},
-    { "Ring" :  0, "FEN": 2, "Hybrid" :  1, "HybridId" : "E5533333222222221111111100000003"},
-    { "Ring" : 12, "FEN": 1, "Hybrid" :  0, "HybridId" : "E5533333222222221111111100000004"},
-    { "Ring" :  1, "FEN": 1, "Hybrid" :  1, "HybridId" : "E5533333222222221111111100000005"}
-  ]
-}
-)"_json;
-
-std::string InvalidConfig = R"(
-{
-  "Detector": "Freia",
-
-  "WireChOffset" : 16,
-
-  "Config" : [
-    { "Ring" :  0, "FEN": 1, "Hybrid" :  0, "HybridId" : "E5533333222222221111111100000000"},
-    { "Ring" :  0, "FEN": 1, "Hybrid" :  1, "HybridId" : "E5533333222222221111111100000001"},
-    { "Ring" :  0, "FEN": 2, "Hybrid" :  0, "HybridId" : "E5533333222222221111111100000002"},
-    { "Ring" :  0, "FEN": 2, "Hybrid" :  1, "HybridId" : "E5533333222222221111111100000003"},
-    { "Rinx" :  1, "FEN": 1, "Hybrid" :  0, "HybridId" : "E5533333222222221111111100000004"},
-    { "Ring" :  1, "FEN": 1, "Hybrid" :  1, "HybridId" : "E5533333222222221111111100000005"}
-  ],
-
-  "MaxPulseTimeNS" : 357000000
-}
-)";
-
-auto DuplicateEntry = R"(
-{
-  "Detector": "Freia",
-
-  "WireChOffset" : 16,
-
-  "Config" : [
-    { "Ring" :  0, "FEN": 0, "Hybrid" :  0, "HybridId" : "E5533333222222221111111100000000"},
-    { "Ring" :  0, "FEN": 0, "Hybrid" :  1, "HybridId" : "E5533333222222221111111100000001"},
-    { "Ring" :  0, "FEN": 1, "Hybrid" :  0, "HybridId" : "E5533333222222221111111100000002"},
-    { "Ring" :  0, "FEN": 0, "Hybrid" :  0, "HybridId" : "E5533333222222221111111100000003"}
-  ]
-}
-)"_json;
 
 using namespace Freia;
 
 class FreiaConfigTest : public TestBase {
 protected:
   Config config{"Freia", "config.json"};
-  void SetUp() override { config.root = j2; }
+
+  void SetUp() override {
+    config.root = from_json_file(FREIA_FULL);
+  }
+
   void TearDown() override {}
 };
 
@@ -98,27 +41,29 @@ TEST_F(FreiaConfigTest, UninitialisedHybrids) {
 }
 
 TEST_F(FreiaConfigTest, NoDetector) {
-  config.root = NoDetector;
+  json_change_key(config.root, "Detector", "NoMoreDetector");
   ASSERT_ANY_THROW(config.applyVMM3Config());
 }
 
 TEST_F(FreiaConfigTest, InvalidDetector) {
-  config.root = InvalidDetector;
+  config.root["Detector"] = "Frigg";
   ASSERT_ANY_THROW(config.applyVMM3Config());
 }
 
 TEST_F(FreiaConfigTest, InvalidRing) {
-  config.root = InvalidRing;
+  config.root["Config"][4]["Ring"] = 12;
   ASSERT_ANY_THROW(config.applyVMM3Config());
 }
 
 TEST_F(FreiaConfigTest, InvalidConfig) {
-  config.root = InvalidConfig;
+  json_change_key(config.root["Config"][4], "Ring", "Ringo");
   ASSERT_ANY_THROW(config.applyConfig());
 }
 
 TEST_F(FreiaConfigTest, Duplicate) {
-  config.root = DuplicateEntry;
+  config.root["Config"][3]["Ring"] = config.root["Config"][0]["Ring"];
+  config.root["Config"][3]["FEN"] = config.root["Config"][0]["FEN"];
+  config.root["Config"][3]["Hybrid"] = config.root["Config"][0]["Hybrid"];
   ASSERT_ANY_THROW(config.applyVMM3Config());
 }
 
@@ -140,6 +85,16 @@ std::vector<RingCfg> ReferenceConfig{
     {9, 0, 0},  {9, 0, 1},  {9, 1, 0}, {9, 1, 1}, {10, 0, 0}, {10, 0, 1},
     {10, 1, 0}, {10, 1, 1},
 };
+
+TEST_F(FreiaConfigTest, BadVersion) {
+  config.root["Version"] = 2;
+  ASSERT_ANY_THROW(config.applyConfig());
+}
+
+TEST_F(FreiaConfigTest, BadThresholdArraySize) {
+  config.root["Config"][0]["Thresholds"][0].push_back(42);
+  ASSERT_ANY_THROW(config.applyConfig());
+}
 
 TEST_F(FreiaConfigTest, FullInstrument) {
   config = Config("Freia", FREIA_FULL);
