9a9dec200ba986f032abaedf2366ec16372c02a2
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-10-13 13:05:29 +0200

    VMM3Config tests

--- src/common/readout/vmm3/test/VMM3ConfigTest.cpp
+++ src/common/readout/vmm3/test/VMM3ConfigTest.cpp
@@ -18,13 +18,27 @@ class VMM3 : public VMM3Config {
 
 class VMM3ConfigTest : public TestBase {
 protected:
+  VMM3 testvmm3;
 };
 
 
-TEST_F(VMM3ConfigTest, NotJson) {
-  VMM3 testvmm3;
+TEST_F(VMM3ConfigTest, ConfigFileNotJson) {
   testvmm3.FileName="/etc/passwd"; // not a jason file
   EXPECT_ANY_THROW(testvmm3.loadAndApplyConfig());
+}
+
+
+TEST_F(VMM3ConfigTest, CalibFileNotJson) {
+  EXPECT_ANY_THROW(testvmm3.loadAndApplyCalibration("/etc/passwd"));
+}
+
+
+TEST_F(VMM3ConfigTest, ConfigFileDuplicateHybrid) {
+  testvmm3.ExpectedName = "Freia";
+  testvmm3.root = from_json_file(VMM_CONFIG_FILE);
+
+  testvmm3.root["Config"][3]["HybridId"] = testvmm3.root["Config"][0]["HybridId"];
+  EXPECT_ANY_THROW(testvmm3.applyVMM3Config());
 }
 
 
