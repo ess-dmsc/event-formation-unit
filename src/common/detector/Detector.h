2a3bf7d83e34b8998863757677955e79b2a62489
Afonso Mukai <afonso.mukai@ess.eu>
2024-02-05 16:52:25 +0100

    Update KafkaBufferSize
    Tested manually running the freia EFU, most of the buffer was not being used.

--- src/common/detector/Detector.h
+++ src/common/detector/Detector.h
@@ -134,7 +134,7 @@ public:
   /// \todo figure out the right size  of EthernetBufferMaxEntries
   static const int EthernetBufferMaxEntries{2000};
   static const int EthernetBufferSize{9000}; /// bytes
-  static const int KafkaBufferSize{124000};  /// entries ~ 1MB
+  static const int KafkaBufferSize{10000};  /// entries ~ 1MB
 
   /// Shared between input_thread and processing_thread
   memory_sequential_consistent::CircularFifo<unsigned int,
