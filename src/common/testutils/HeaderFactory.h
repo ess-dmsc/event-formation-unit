91558353bd1cb1906bdfb7b78509ef89a4e7075d
Tibor Bukovics <tibor.bukovics@ess.eu>
2024-02-08 16:58:02 +0000

    updates

--- src/common/testutils/HeaderFactory.h
+++ src/common/testutils/HeaderFactory.h
@@ -23,7 +23,7 @@ public:
   Parser::PacketHeader *createHeader(const Parser::HeaderVersion &version) {
     if (version == Parser::HeaderVersion::V0) {
       Parser::PacketHeaderV0 *header = new Parser::PacketHeaderV0();
-      memset(header, 0, sizeof(Parser::PacketHeaderV0));
+      // memset(header, 0, sizeof(Parser::PacketHeaderV0));
 
       myHeaderPtr =
           std::make_unique<Parser::PacketHeader>(Parser::PacketHeader(header));
@@ -31,7 +31,7 @@ public:
 
     } else if (version == Parser::HeaderVersion::V1) {
       Parser::PacketHeaderV1 *header = new Parser::PacketHeaderV1();
-      memset(header, 0, sizeof(Parser::PacketHeaderV1));
+      // memset(header, 0, sizeof(Parser::PacketHeaderV1));
 
       myHeaderPtr =
           std::make_unique<Parser::PacketHeader>(Parser::PacketHeader(header));
