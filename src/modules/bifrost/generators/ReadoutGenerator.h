30b6e946ac11d279a7b399d363bee458cf558339
Tibor Bukovics <tibor.bukovics@ess.eu>
2024-08-07 12:52:14 +0000

    add new main for biforst gen to calculate packets for file

--- src/modules/bifrost/generators/ReadoutGenerator.h
+++ src/modules/bifrost/generators/ReadoutGenerator.h
@@ -14,23 +14,28 @@
 #include <generators/essudpgen/ReadoutGeneratorBase.h>
 #include <modules/caen/readout/DataParser.h>
 
-/**
- * @brief The BifrostDatReader class is responsible for reading data from a DAT
- * file and generating data.
- */
+///
+/// \brief The BifrostDatReader class is responsible for reading data from a DAT
+/// file and generating data.
+///
 class ReadoutGenerator : public ReadoutGeneratorBase {
 public:
   ReadoutGenerator();
 
+  ///
+  /// \brief This is a main function of the generator which replace the base
+  /// class main function with extra functionality.
+  ///
+  void main();
+
 private:
+  struct BiforstGeneratorSettings {
+    std::string FilePath{"none"}; ///< The path to the DAT file
+  } bifrostSettings;
 
-struct BiforstGeneratorSettings {
-  std::string FilePath{"none"}; ///< The path to the DAT file
-} bifrostSettings;
-
-  /**
-   * @brief The dat_data_t struct represents the data structure for DAT records.
-   */
+  ///
+  /// \brief The dat_data_t struct represents the data structure for DAT records.
+  ///
   struct dat_data_t {
     uint32_t timehi;  ///< High 32 bits of the timestamp
     uint32_t timelow; ///< Low 32 bits of the timestamp
@@ -42,19 +47,19 @@ struct BiforstGeneratorSettings {
   } __attribute__((__packed__));
   static_assert(sizeof(struct dat_data_t) == 16, "wrong packing");
 
-  /**
-   * @brief Reads a CDTReadout struct from the DAT file.
-   * @param reaout The reference to the dat_data_t struct to store the readout
-   * data.
-   * @return The number of bytes read, 0 if the line is ignored, or -1 upon
-   * error/end.
-   */
-  int readReadout(struct dat_data_t &reaout);
+  ///
+  /// \brief Reads a CDTReadout struct from the DAT file.
+  /// \param readout The reference to the dat_data_t struct to store the readout
+  /// data.
+  /// \return The number of bytes read, 0 if the line is ignored, or -1 upon
+  /// error/end.
+  ///
+  int readReadout(struct dat_data_t &readout);
 
-  /**
-   * @brief Implementation of base class abstract function.
-   * Generates data using the BifrostDatReader to read the DAT file.
-   */
+  ///
+  /// \brief Implementation of base class abstract function.
+  /// Generates data using the BifrostDatReader to read the DAT file.
+  ///
   void generateData() override;
 
   int FileDescriptor{-1}; ///< The file descriptor of the DAT file.
