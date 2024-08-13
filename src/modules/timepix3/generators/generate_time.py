7cef6dcd284cf534c7a9fc8f6fad30201076adc1
Tibor Bukovics <tibor.bukovics@ess.eu>
2024-08-13 09:22:25 +0000

    Add tpx tof time generator

--- src/modules/timepix3/generators/generate_time.py
+++ src/modules/timepix3/generators/generate_time.py
@@ -8,9 +8,13 @@ import bitstruct
 import struct
 import numpy as np
 
+PIXEL_MAX_TIMESTAMP_NS = 26843545600
+UDP_MAX_SIZE = 8952
+
+
 def gen_tdc_readout(counter, tdc_ns):
     # Define the tdc format of the payload
-    format = 'u4u4u12u35u4u5'
+    format = "u4u4u12u35u4u5"
 
     tdc_header = 0x6
     tdc_type = 15
@@ -18,19 +22,24 @@ def gen_tdc_readout(counter, tdc_ns):
     timestamp = int(tdc_ns / 3.125)
     stamp = 12
 
-    tdc_packet = bitstruct.pack(format, tdc_header, tdc_type, counter, timestamp, stamp, 0)
+    tdc_packet = bitstruct.pack(
+        format, tdc_header, tdc_type, counter, timestamp, stamp, 0
+    )
 
     tdc_packet = tdc_packet[::-1]
 
     return tdc_packet
 
+
 def gen_pixel_cluster(tdc_ns, delay_to_tdc, cluster_size):
     # Define the pixel format of the payload
-    format = 'u4u16u14u10u4u16'
+    format = "u4u16u14u10u4u16"
 
     pixel_header = 11
 
-    pixel_time_ns = tdc_ns + delay_to_tdc
+    pixel_time_ns = (tdc_ns + delay_to_tdc) - (
+        PIXEL_MAX_TIMESTAMP_NS * int(tdc_ns / PIXEL_MAX_TIMESTAMP_NS)
+    )
 
     spdr = int(pixel_time_ns / 409600)
     spdr_remainder = pixel_time_ns % 409600
@@ -38,53 +47,68 @@ def gen_pixel_cluster(tdc_ns, delay_to_tdc, cluster_size):
     toa_remainder = spdr_remainder % 25
     ftoa = int(toa_remainder / 1.5625)
 
-    payload = b''
-    
-    for i in range(cluster_size):
+    payload = b""
 
-        tot = 50
+    pixel_cluster_in_packet = UDP_MAX_SIZE // (cluster_size * 8)
 
-        pix_addr = 100 + i
+    for clusters in range(0, pixel_cluster_in_packet):
+        pix_addr = np.random.randint(100, 12000)
 
-        payload = bitstruct.pack(format, pixel_header, pix_addr, toa + i, tot, ftoa, spdr)[::-1]
+        for i in range(cluster_size):
+
+            tot = np.random.randint(1, 255)
+
+            pixel_readout = bitstruct.pack(
+                format, pixel_header, pix_addr + i, toa, tot, 0, spdr
+            )[::-1]
+
+            payload += pixel_readout
 
     return payload
 
+
 def print_pixel_readout(data):
-    format = 'u4u16u14u10u4u16'
-    packed = struct.pack('>Q', data)
-    unpacked = struct.unpack('<Q', packed)[0]
-    swaped_bytes = struct.pack('<Q', unpacked)
+    format = "u4u16u14u10u4u16"
+    packed = struct.pack(">Q", data)
+    unpacked = struct.unpack("<Q", packed)[0]
+    swaped_bytes = struct.pack("<Q", unpacked)
     pix_header, pix_addr, toa, tot, ftoa, spdr = bitstruct.unpack(format, swaped_bytes)
-    print(pix_header, pix_addr, toa, tot, ftoa, spdr, sep='\t')
+    print(pix_header, pix_addr, toa, tot, ftoa, spdr, sep="\t")
+
 
 def print_tdc_readout(data):
-    format = 'u4u4u12u35u4u5'
-    packed = struct.pack('>Q', data)
-    unpacked = struct.unpack('<Q', packed)[0]
-    swaped_bytes = struct.pack('<Q', unpacked)
-    tdc_header, tdc_type, counter, timestamp, stamp, _ = bitstruct.unpack(format, swaped_bytes)
-    print(tdc_header, tdc_type, counter, timestamp, stamp, sep='\t')
+    format = "u4u4u12u35u4u5"
+    packed = struct.pack(">Q", data)
+    unpacked = struct.unpack("<Q", packed)[0]
+    swaped_bytes = struct.pack("<Q", unpacked)
+    tdc_header, tdc_type, counter, timestamp, stamp, _ = bitstruct.unpack(
+        format, swaped_bytes
+    )
+    print(tdc_header, tdc_type, counter, timestamp, stamp, sep="\t")
+
 
 def main():
     # Generate TDC readout payload
     counter = 1
     tdc_ns = 0
-    delay_to_tdc = 50000
-    cluster_size = 8
+    us_to_ns = 1000
+    cluster_size = 1
 
     data = b""
 
-    for i in range(1000):
-        data += gen_tdc_readout(counter, tdc_ns)
-        data += gen_pixel_cluster(tdc_ns, delay_to_tdc, cluster_size)
+    with open("tpx/file", "wb") as file:
+        file.write(data)
 
-        tdc_ns += int(1e9 / 14)
+    for i in range(1):
+        data += gen_tdc_readout(counter + i, tdc_ns)
+        data += gen_pixel_cluster(tdc_ns, 40000 * us_to_ns, cluster_size)
+
+        tdc_ns += int(1e9 / 10)
         if tdc_ns > 107374182400:
             tdc_ns = 0
 
-    with open('file', 'wb') as file:
-        file.write(data)
+        with open("tpx/file", "ab") as file:
+            file.write(data)
 
     # with open('file', 'rb') as file:
     #     raw_data = file.read()
@@ -98,7 +122,7 @@ def main():
     #         print_tdc_readout(data_array[i])
     #     else:
     #         print_pixel_readout(data_array[i])
-    
+
 
 if __name__ == "__main__":
     main()
