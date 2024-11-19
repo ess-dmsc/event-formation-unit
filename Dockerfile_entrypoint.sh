6b1617ad8ba169399324c5af8bc079ce9f54a47f
Jekabs Karklins <58165815+jekabs-karklins@users.noreply.github.com>
2024-11-19 14:31:55 +0100

    ECDC-4350 EFU docker container (#786)
    gitla ci pipeline and docker file to product efu docker image as well
    ---------
    
    Co-authored-by: Jekabs Karklins <jekabskarklins@ess.eu>

---
+++ Dockerfile_entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+CMD="$BINARY_PATH -b $BROKER_ADDR -t $BROKER_TOPIC --nohwcheck"
+
+if [ -n "$GRAPHITE" ]; then
+    CMD="$CMD -g $GRAPHITE"
+fi
+
+if [ -z "$FILE" ]; then
+    echo "Please specify a config file"
+    exit 1
+elif [ ! -r "./config/$FILE" ]; then
+    echo "File $FILE does not exist or is not readable"
+    exit 1
+else
+    CMD="$CMD -f ./config/$FILE"
+fi
+
+
+if [ -n "$CALIBRATION" ]; then
+    CMD="$CMD -c ./config/$CALIBRATION"
+fi
+echo "Running $CMD"
+
+exec $CMD
