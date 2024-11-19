f33e9e9ee3f88d4bc81f667bd89cb21c1ddbf193
Jekabs Karklins <jekabskarklins@ess.eu>
2024-11-19 10:25:10 +0100

    Refactor Dockerfile_entrypoint.sh to handle config file errors

--- Dockerfile_entrypoint.sh
+++ Dockerfile_entrypoint.sh
@@ -6,9 +6,16 @@ if [ -n "$GRAPHITE" ]; then
     CMD="$CMD -g $GRAPHITE"
 fi
 
-if [ -n "$FILE" ]; then
+if [ -z "$FILE" ]; then
+    echo "Please specify a config file"
+    exit 1
+elif [ ! -r "./config/$FILE" ]; then
+    echo "File $FILE does not exist or is not readable"
+    exit 1
+else
     CMD="$CMD -f ./config/$FILE"
 fi
+
 
 if [ -n "$CALIBRATION" ]; then
     CMD="$CMD -c ./config/$CALIBRATION"
