665970ac2c223143c0866bb49d31d4bdf25bcf7e
Morten Jagd Christensen <mortenjc@jcaps.com>
2024-12-05 15:30:03 +0100

    cleanup

--- src/modules/freia/test/AMORChannelMappingTest.cpp
+++ src/modules/freia/test/AMORChannelMappingTest.cpp
@@ -12,7 +12,8 @@ using namespace Freia;
 class AMORChannelMappingTest : public TestBase {
 protected:
   AMORGeometry Geom;
-  uint16_t Cassette1{1};
+  uint16_t Cassette0Xoffset{0};
+  uint16_t Cassette0Yoffset{0};
   uint16_t VMMX{0};
   uint16_t VMMY{1};
   void SetUp() override {}
@@ -38,14 +39,14 @@ TEST_F(AMORChannelMappingTest, Coordinates) {
 }
 
 TEST_F(AMORChannelMappingTest, XCoordErrors) {
-  ASSERT_EQ(Geom.xCoord(0, VMMY, 0), Geom.InvalidCoord);  // bad VMM
-  ASSERT_EQ(Geom.xCoord(0, VMMX, 64), Geom.InvalidCoord); // bad Channel
+  ASSERT_EQ(Geom.xCoord(Cassette0Xoffset, VMMY, 0), Geom.InvalidCoord);  // bad VMM
+  ASSERT_EQ(Geom.xCoord(Cassette0Xoffset, VMMX, 64), Geom.InvalidCoord); // bad Channel
 }
 
 TEST_F(AMORChannelMappingTest, YCoordErrors) {
-  ASSERT_EQ(Geom.yCoord(1, VMMX, 32), Geom.InvalidCoord); // bad VMM
-  ASSERT_EQ(Geom.yCoord(1, VMMY, 15), Geom.InvalidCoord); // bad Channel
-  ASSERT_EQ(Geom.yCoord(1, VMMY, 48), Geom.InvalidCoord); // bad Channel
+  ASSERT_EQ(Geom.yCoord(Cassette0Yoffset, VMMX, 32), Geom.InvalidCoord); // bad VMM
+  ASSERT_EQ(Geom.yCoord(Cassette0Yoffset, VMMY, 15), Geom.InvalidCoord); // bad Channel
+  ASSERT_EQ(Geom.yCoord(Cassette0Yoffset, VMMY, 48), Geom.InvalidCoord); // bad Channel
 }
 
 int main(int argc, char **argv) {
--- src/modules/freia/test/FreiaChannelMappingTest.cpp
+++ src/modules/freia/test/FreiaChannelMappingTest.cpp
@@ -12,7 +12,8 @@ using namespace Freia;
 class FreiaChannelMappingTest : public TestBase {
 protected:
   FreiaGeometry Geom;
-  uint16_t Cassette0{0};
+  uint16_t Cassette0Xoffset{0};
+  uint16_t Cassette0Yoffset{0};
   uint16_t VMMX{1};
   uint16_t VMMY{0};
   void SetUp() override {}
@@ -21,22 +22,22 @@ protected:
 
 TEST_F(FreiaChannelMappingTest, Coordinates) {
   for (unsigned int i = 0; i < 64; i++) {
-    ASSERT_EQ(Geom.xCoord(0, VMMX, i), i);
+    ASSERT_EQ(Geom.xCoord(Cassette0Xoffset, VMMX, i), i);
   }
   for (unsigned int i = 16; i < 47; i++) {
-    ASSERT_EQ(Geom.yCoord(Cassette0, VMMY, i), i - 16);
+    ASSERT_EQ(Geom.yCoord(Cassette0Yoffset, VMMY, i), i - 16);
   }
 }
 
 TEST_F(FreiaChannelMappingTest, XCoordErrors) {
-  ASSERT_EQ(Geom.xCoord(0, VMMY, 0), Geom.InvalidCoord);  // bad VMM
-  ASSERT_EQ(Geom.xCoord(0, VMMX, 64), Geom.InvalidCoord); // bad Channel
+  ASSERT_EQ(Geom.xCoord(Cassette0Xoffset, VMMY, 0), Geom.InvalidCoord);  // bad VMM
+  ASSERT_EQ(Geom.xCoord(Cassette0Xoffset, VMMX, 64), Geom.InvalidCoord); // bad Channel
 }
 
 TEST_F(FreiaChannelMappingTest, YCoordErrors) {
-  ASSERT_EQ(Geom.yCoord(1, VMMX, 32), Geom.InvalidCoord); // bad VMM
-  ASSERT_EQ(Geom.yCoord(1, VMMY, 15), Geom.InvalidCoord); // bad Channel
-  ASSERT_EQ(Geom.yCoord(1, VMMY, 48), Geom.InvalidCoord); // bad Channel
+  ASSERT_EQ(Geom.yCoord(Cassette0Yoffset, VMMX, 32), Geom.InvalidCoord); // bad VMM
+  ASSERT_EQ(Geom.yCoord(Cassette0Yoffset, VMMY, 15), Geom.InvalidCoord); // bad Channel
+  ASSERT_EQ(Geom.yCoord(Cassette0Yoffset, VMMY, 48), Geom.InvalidCoord); // bad Channel
 }
 
 int main(int argc, char **argv) {
