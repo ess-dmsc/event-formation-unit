64362e21641d8082e2cef4a5ec7fc5db462d05f1
Tibor Bukovics <tibor.bukovics@ess.eu>
2025-01-03 17:18:49 +0000

    optimize cbm parser memory access.

--- src/modules/cbm/geometry/Parser.cpp
+++ src/modules/cbm/geometry/Parser.cpp
@@ -45,56 +45,58 @@ void Parser::parse(ESSReadout::Parser::PacketDataV0 &PacketData) {
     return;
   }
 
-  Parser::CbmReadout *DataPtr = (struct CbmReadout *)Buffer;
+  Parser::CbmReadout *Readout = (struct CbmReadout *)Buffer;
   for (unsigned int i = 0; i < Size / DataLength; i++) {
+
     Stats.Readouts++;
-    Parser::CbmReadout Readout = DataPtr[i];
-    if (Readout.FiberId > MaxFiberId) {
-      XTRACE(DATA, WAR, "Invalid FiberId %d (Max is %d)", Readout.FiberId,
+    if (Readout->FiberId > MaxFiberId) {
+      XTRACE(DATA, WAR, "Invalid FiberId %d (Max is %d)", Readout->FiberId,
              MaxFiberId);
       Stats.ErrorFiber++;
       continue;
     }
 
-    if (Readout.FENId > MaxFENId) {
-      XTRACE(DATA, WAR, "Invalid FENId %d (valid: 0 - %d)", Readout.FENId,
+    if (Readout->FENId > MaxFENId) {
+      XTRACE(DATA, WAR, "Invalid FENId %d (valid: 0 - %d)", Readout->FENId,
              MaxFENId);
       Stats.ErrorFEN++;
       continue;
     }
 
-    if (Readout.Type > CbmType::MAX || Readout.Type < CbmType::MIN) {
-      XTRACE(DATA, WAR, "Invalid data type %d (valid: %d - %d)", Readout.Type,
+    if (Readout->Type > CbmType::MAX || Readout->Type < CbmType::MIN) {
+      XTRACE(DATA, WAR, "Invalid data type %d (valid: %d - %d)", Readout->Type,
              CbmType::MIN, CbmType::MAX);
       Stats.ErrorType++;
       continue;
     }
 
-    if (Readout.DataLength != DataLength) {
+    if (Readout->DataLength != DataLength) {
       XTRACE(DATA, WAR, "Invalid header length %d - must be %d bytes",
-             Readout.DataLength, DataLength);
+             Readout->DataLength, DataLength);
       Stats.ErrorDataLength++;
       continue;
     }
 
     // Check for invalid ADC values only for TTL readouts
-    if (Readout.Type == CbmType::TTL && Readout.ADC == 0) {
+    if (Readout->Type == CbmType::TTL && Readout->ADC == 0) {
       XTRACE(DATA, WAR, "Invalid ADC (0)");
       Stats.ErrorADC++;
-    } else if (Readout.Type != CbmType::TTL && Readout.ADC != 0) {
+    } else if (Readout->Type != CbmType::TTL && Readout->ADC != 0) {
       XTRACE(DATA, WAR, "Invalid ADC unsued for this type should be 0");
       Stats.ErrorADC++;
       continue;
     }
 
-    if (Readout.TimeLow > ESSReadout::MaxFracTimeCount) {
-      XTRACE(DATA, WAR, "Invalid TimeLO %u (max is %u)", Readout.TimeLow,
+    if (Readout->TimeLow > ESSReadout::MaxFracTimeCount) {
+      XTRACE(DATA, WAR, "Invalid TimeLO %u (max is %u)", Readout->TimeLow,
              ESSReadout::MaxFracTimeCount);
       Stats.ErrorTimeFrac++;
       continue;
     }
 
-    Result.push_back(Readout);
+    Result.push_back(*Readout);
+
+    Readout++;
   }
 
   return;
