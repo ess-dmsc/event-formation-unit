db14bd53a7de09fa947da604d9e76e7beca118c8
Michael Christiansen <michael.christiansen@ess.eu>
2025-05-14 13:12:48 +0200

    changed to used linear distribution for calculating rToA for IBM version

--- src/modules/cbm/generators/udpgen.cpp
+++ src/modules/cbm/generators/udpgen.cpp
@@ -9,6 +9,7 @@
 // GCOVR_EXCL_START
 
 #include <modules/cbm/generators/ReadoutGenerator.h>
+#include <generators/functiongenerators/LinearGenerator.h>
 #include <modules/cbm/geometry/Parser.h>
 
 int main(int argc, char *argv[]) {
@@ -23,7 +24,7 @@ int main(int argc, char *argv[]) {
   if (CbmGen.cbmSettings.monitorType == cbm::CbmType::TTL) {
     distribution = DistributionGenerator::Factory(CbmGen.Settings.Frequency);
   } else if (CbmGen.cbmSettings.monitorType == cbm::CbmType::IBM) {
-    distribution = std::make_unique<DistributionGenerator>( 1000.0 / CbmGen.Settings.Frequency);
+    distribution = LinearGenerator::Factory(CbmGen.Settings.Frequency);
   } else {
     throw std::runtime_error("Unsupported monitor type");
   }
